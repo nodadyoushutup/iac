87d3078b7945ad8acb4e403de50630bb1343a54c
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-23 01:51:42 +0000

    [WIP] Spacelift

--- spacelift/component.tf
+++ spacelift/component.tf
@@ -1,15 +1,23 @@
-module "component" {
-  source  = "spacelift.io/nodadyoushutup/component/spacelift"
-  for_each = { for component in local.config.component : component => component }
-  
-  component = each.value
-
-  depends_on = [
-    spacelift_module.spacelift_stack,
-    spacelift_module.spacelift_component,
-    spacelift_module.proxmox_virtual_machine,
-    spacelift_module.fortigate_vip,
-    spacelift_module.fortigate_policy,
-    spacelift_module.fortigate_port_forward,
-  ]
+data "spacelift_module" "stack" {
+  module_id = "stack"
 }
+
+output "stack" {
+  value = spacelift_module.stack
+}
+
+# module "component" {
+#   source  = "spacelift.io/nodadyoushutup/component/spacelift"
+#   for_each = { for component in local.config.component : component => component }
+  
+#   component = each.value
+
+#   depends_on = [
+#     spacelift_module.spacelift_stack,
+#     spacelift_module.spacelift_component,
+#     spacelift_module.proxmox_virtual_machine,
+#     spacelift_module.fortigate_vip,
+#     spacelift_module.fortigate_policy,
+#     spacelift_module.fortigate_port_forward,
+#   ]
+# }
