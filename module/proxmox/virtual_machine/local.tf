5e7c302a18ce121dd2ee7d2b394007db94d79814
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-31 08:46:37 +0000

    [WIP] Stacks

--- module/proxmox/virtual_machine/local.tf
+++ module/proxmox/virtual_machine/local.tf
@@ -4,3 +4,5 @@ locals {
   config = try(yamldecode(file("/mnt/workspace/config.yaml")), yamldecode(file("/mnt/workspace/config.yml")))
   virtual_machine = try(yamldecode(file("/mnt/workspace/virtual_machine.yaml")), yamldecode(file("/mnt/workspace/virtual_machine.yml")))
 }
+
+# TODO: Migrate contents from virtual_machine.yaml to the main config yaml
