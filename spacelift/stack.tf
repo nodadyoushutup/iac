81022a8d36b9e6db8264151d07322aca6520a37e
nodadyoushutup <admin@nodadyoushutup.com>
2024-09-12 19:38:21 +0000

    [WIP] Spacelift

--- spacelift/stack.tf
+++ spacelift/stack.tf
@@ -59,48 +59,48 @@ module "stack" {
     source  = "spacelift.io/nodadyoushutup/stack/spacelift"
 
     ## REQUIRED ##
-    name        = "database"
-    repository  = "iac"
-    branch      = "main"
+    name = "database"
 
-    ## UNIQUE ##
-    project_root        = "database"
-    description         = "Database infrastructure"
-    labels              = ["infra", "database"]
+    # ## UNIQUE ##
+    # project_root = "database"
+    # description = "Database infrastructure"
+    # labels = ["infra", "database"]
 
-    ## OPTIONAL (No Global)
-    import_state = null
-    import_state_file = null
+    # ## OPTIONAL (No Global)
+    # import_state = null
+    # import_state_file = null
 
-    ## OPTIONAL ##
-    space_id = "root"
-    administrative = false
-    autodeploy = true
-    terraform_version = "1.5.7"
-    additional_project_globs = null
-    autoretry = false
-    enable_local_preview = false
-    enable_well_known_secret_masking = false
-    github_action_deploy = true
-    manage_state = true
-    protect_from_deletion = false
-    terraform_smart_sanitization = false
-    terraform_workflow_tool = "TERRAFORM_FOSS"
+    # ## OPTIONAL ##
+    # repository = "iac"
+    # branch = "main"
+    # space_id = "root"
+    # administrative = false
+    # autodeploy = true
+    # terraform_version = "1.5.7"
+    # additional_project_globs = null
+    # autoretry = false
+    # enable_local_preview = false
+    # enable_well_known_secret_masking = false
+    # github_action_deploy = true
+    # manage_state = true
+    # protect_from_deletion = false
+    # terraform_smart_sanitization = false
+    # terraform_workflow_tool = "TERRAFORM_FOSS"
 
-    ## HOOKS ##
-    before = {
-        apply = null
-        destroy = null
-        init = null
-        perform = null
-        plan = null
-    }
-    after = {
-        apply = null
-        destroy = null
-        init = null
-        perform = null
-        plan = null
-        run = null
-    }
+    # ## HOOKS ##
+    # before = {
+    #     apply = null
+    #     destroy = null
+    #     init = null
+    #     perform = null
+    #     plan = null
+    # }
+    # after = {
+    #     apply = null
+    #     destroy = null
+    #     init = null
+    #     perform = null
+    #     plan = null
+    #     run = null
+    # }
 }
