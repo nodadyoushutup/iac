a13a311b6f87d00b9c0395d04f3c0b15e534a138
nodadyoushutup <admin@nodadyoushutup.com>
2024-09-12 20:51:03 +0000

    [WIP] Spacelift

--- module/spacelift/stack/main.tf
+++ module/spacelift/stack/main.tf
@@ -23,14 +23,16 @@ resource "spacelift_stack" "stack" {
     ),
     []
   )
-  project_root = try(
-    coalesce(
-      try(var.project_root, null), 
-      try(local.stack.project_root, null), 
-      try(local.config.global.stack.project_root, null)
-    ),
-    null
-  )
+  # project_root = try(
+  #   coalesce(
+  #     try(var.project_root, null), 
+  #     try(local.stack.project_root, null), 
+  #     try(local.config.global.stack.project_root, null)
+  #   ),
+  #   null
+  # )
+
+  project_root = local.stack.project_root
 
   ## OPTIONAL (NO GLOBAL)
   import_state = var.import_state
