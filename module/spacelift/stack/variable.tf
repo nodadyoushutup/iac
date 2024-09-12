1c1b46b409bce65a7e765eed70d2b56ad46061da
nodadyoushutup <admin@nodadyoushutup.com>
2024-09-12 18:57:41 +0000

    [WIP] Spacelift

--- module/spacelift/stack/variable.tf
+++ module/spacelift/stack/variable.tf
@@ -13,7 +13,7 @@ variable "repository" {
 variable "branch" {
   description = "Git branch to use for the stack."
   type= string
-  default= "main"
+  default= local.config.global.branch
 }
 
 #OPTIONAL
