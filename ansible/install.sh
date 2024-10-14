fb77d1bb553b3c366544020f18807045f5d86bba
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-14 22:22:22 +0000

    first commit

---
+++ ansible/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "####################################################"
+echo "Spacelift runner Ansible installation script starting"
+
+echo "Creating Python virtual environment"
+python3 -m venv venv
+
+echo "Activating Python virtual environment"
+source venv/bin/activate
+
+echo "Installing PIP"
+python3 -m ensurepip --upgrade
+
+echo "Installing Ansible"
+pip install ansible
+
+echo "Setting private key permissions"
+ANSIBLE_CONFIG=${ANSIBLE_CONFIG:-"/mnt/workspace/source/ansible/ansible.cfg"}
+export ANSIBLE_PRIVATE_KEY_FILE=$(awk -F ' *=*' '/^private_key_file/ {print $2}' "$ANSIBLE_CONFIG" | tr -d '\r')
+chmod 600 $ANSIBLE_PRIVATE_KEY_FILE
+echo "####################################################"
---
+++ terraform/spacelift/data.tf
@@ -0,0 +1,7 @@
+data "spacelift_stack" "spacelift" {
+  stack_id = "spacelift"
+}
+
+data "spacelift_context" "config" {
+  context_id = "config"
+}
