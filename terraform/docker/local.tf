7873b1ceb89e897364821dabe8a806fa0449179d
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-14 23:54:58 +0000

    [WIP] Spacelift

--- terraform/docker/local.tf
+++
@@ -1,5 +0,0 @@
-# TODO: Make custom config path (using host environment variable)
-locals {
-  config = try(yamldecode(file("/mnt/workspace/config.yaml")), yamldecode(file("/mnt/workspace/config.yml")), {})
-  virtual_machine = try(yamldecode(file("/mnt/workspace/virtual_machine.yaml")), yamldecode(file("/mnt/workspace/virtual_machine.yml")), {})
-}
--- terraform/spacelift/data.tf
+++
@@ -1,7 +0,0 @@
-data "spacelift_stack" "spacelift" {
-  stack_id = "spacelift"
-}
-
-data "spacelift_context" "config" {
-  context_id = "config"
-}
