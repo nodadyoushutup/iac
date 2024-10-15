8f20be8f3b29f50536366369027df1469b16d872
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-15 22:09:19 +0000

    [WIP] Spacelift

--- terraform/spacelift/data.tf
+++ terraform/spacelift/data.tf
@@ -1,7 +1,7 @@
-data "spacelift_stack" "spacelift" {
-  stack_id = "spacelift"
-}
+# data "spacelift_stack" "spacelift" {
+#   stack_id = "spacelift"
+# }
 
-data "spacelift_context" "config" {
-  context_id = "config"
-}
+# data "spacelift_context" "config" {
+#   context_id = "config"
+# }
