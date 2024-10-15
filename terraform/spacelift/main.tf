fab1c20370372fa052100b43af09b6d1e490cfe7
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-15 22:57:55 +0000

    [WIP] Spacelift

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -10,7 +10,7 @@
 
 ### DOCKER ###
 resource "spacelift_stack" "docker_infra_stack" {
-    depends_on = [spacelift_context.ansible_hook]
+    # depends_on = [spacelift_context.ansible_hook]
     administrative = true
     autodeploy = true
     branch = "main"
@@ -19,29 +19,36 @@ resource "spacelift_stack" "docker_infra_stack" {
     project_root = "terraform/docker"
     repository = "iac"
     terraform_version = "1.5.7"
-    labels = ["ansible", "docker", "administrative", "p1"]
+    labels = ["terraform", "infra", "docker", "administrative", "p1", "p1a"]
 }
 
 resource "spacelift_stack" "docker_init_stack" {
-    depends_on = [spacelift_context.ansible_hook]
+    depends_on = [spacelift_stack.ansibldocker_infra_stacke_hook]
     administrative = true
     autodeploy = true
     branch = "main"
-    description = "Docker applications"
+    description = "Docker initialization"
     name = "docker_init"
     project_root = "ansible"
     repository = "iac"
     terraform_version = "1.5.7"
-    labels = ["ansible", "docker", "administrative", "p1"]
+    labels = ["ansible", "init", "docker", "administrative", "p1", "p1b"]
     ansible {
         playbook = "docker_init.yaml"
     }
 }
 
-resource "spacelift_context_attachment" "docker_context_attachment_config" {
+resource "spacelift_context_attachment" "docker_infra_context_attachment_config" {
     depends_on = [spacelift_stack.docker_infra_stack]
     context_id = data.spacelift_context.config.id
     stack_id   = spacelift_stack.docker_infra_stack.id
+    priority   = 0
+}
+
+resource "spacelift_context_attachment" "docker_init_context_attachment_config" {
+    depends_on = [spacelift_stack.docker_init_stack]
+    context_id = data.spacelift_context.config.id
+    stack_id   = spacelift_stack.docker_init_stack.id
     priority   = 0
 }
 
