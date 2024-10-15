ef1f5073f9812ecbfce0826e12b8fe76ab4adaa5
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-15 00:02:45 +0000

    [WIP] Spacelift

--- terraform/test/local.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  config = try(yamldecode(file("/mnt/workspace/config.yaml")), yamldecode(file("/mnt/workspace/config.yml")), {})
-  virtual_machine = try(yamldecode(file("/mnt/workspace/virtual_machine.yaml")), yamldecode(file("/mnt/workspace/virtual_machine.yml")), {})
-}
