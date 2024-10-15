d3af8abe625c41b23d74621c0bb4762cb6df0087
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-15 22:54:48 +0000

    [WIP] Spacelift

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -1,37 +1,53 @@
 ### ANSIBLE ###
-resource "spacelift_context" "ansible_hook" {
-    description = "Ansible hook"
-    name        = "ansible_hook"
-    before_init = [
-        "/mnt/workspace/source/ansible/install.sh",
-        "source venv/bin/activate"
-    ]
-}
+# resource "spacelift_context" "ansible_hook" {
+#     description = "Ansible hook"
+#     name        = "ansible_hook"
+#     before_init = [
+#         "/mnt/workspace/source/ansible/install.sh",
+#         "source venv/bin/activate"
+#     ]
+# }
 
 ### DOCKER ###
-resource "spacelift_stack" "docker_stack" {
+resource "spacelift_stack" "docker_infra_stack" {
     depends_on = [spacelift_context.ansible_hook]
     administrative = true
     autodeploy = true
     branch = "main"
     description = "Docker applications"
-    name = "docker"
+    name = "docker_infra"
     project_root = "terraform/docker"
     repository = "iac"
     terraform_version = "1.5.7"
     labels = ["ansible", "docker", "administrative", "p1"]
 }
 
+resource "spacelift_stack" "docker_init_stack" {
+    depends_on = [spacelift_context.ansible_hook]
+    administrative = true
+    autodeploy = true
+    branch = "main"
+    description = "Docker applications"
+    name = "docker_init"
+    project_root = "ansible"
+    repository = "iac"
+    terraform_version = "1.5.7"
+    labels = ["ansible", "docker", "administrative", "p1"]
+    ansible {
+        playbook = "docker_init.yaml"
+    }
+}
+
 resource "spacelift_context_attachment" "docker_context_attachment_config" {
-    depends_on = [spacelift_stack.docker_stack]
+    depends_on = [spacelift_stack.docker_infra_stack]
     context_id = data.spacelift_context.config.id
-    stack_id   = spacelift_stack.docker_stack.id
+    stack_id   = spacelift_stack.docker_infra_stack.id
     priority   = 0
 }
 
-resource "spacelift_context_attachment" "docker_context_attachment_ansible_hook" {
-    depends_on = [spacelift_stack.docker_stack]
-    context_id = spacelift_context.ansible_hook.id
-    stack_id   = spacelift_stack.docker_stack.id
-    priority   = 0
-}
+# resource "spacelift_context_attachment" "docker_context_attachment_ansible_hook" {
+#     depends_on = [spacelift_stack.docker_infra_stack]
+#     context_id = spacelift_context.ansible_hook.id
+#     stack_id   = spacelift_stack.docker_infra_stack.id
+#     priority   = 0
+# }
