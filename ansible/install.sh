353d7dc113df6ad4f3481ea8dce2c158d58871d9
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-15 22:48:09 +0000

    [WIP] Docker

--- ansible/install.sh
+++ ansible/install.sh
@@ -18,4 +18,5 @@ pip install ansible
 echo "Setting private key permissions"
 export ANSIBLE_PRIVATE_KEY_FILE=$(awk -F ' *=*' '/^private_key_file/ {print $2}' "$ANSIBLE_CONFIG" | tr -d '\r')
 chmod 600 $ANSIBLE_PRIVATE_KEY_FILE
+echo $ANSIBLE_CONFIG
 echo "####################################################"
