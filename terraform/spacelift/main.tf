f9c8b4d6f18b6673f27c03fc32ee06aca2c56287
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 22:25:08 +0000

    [WIP] Spacelift

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -3,13 +3,11 @@
 #     byte_length = 8
 # }
 
-
 # data "external" "private_key_validation" {
 #     program = ["bash", "${path.module}/private_key_validation.sh", local.config.path.private_key]
 #     query = {trigger = random_id.trigger.hex}
 #     depends_on = [random_id.trigger]
 # }
-
 
 
 # ### CONTEXT ###
