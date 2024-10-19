5e7f3d4128c3d437c482f6e6b1e4928d0476bfe8
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 19:28:49 +0000

    [WIP] Spacelift

--- terraform/spacelift/local.tf
+++ terraform/spacelift/local.tf
@@ -1,11 +1,12 @@
 locals {
   config_path = try(
-    var.CONFIG != null && var.CONFIG != "" ? var.CONFIG: "/mnt/workspace/config.yaml",
-    # fileexists("/mnt/workspace/config.yaml") ? "/mnt/workspace/config.yaml" : null,
-    # fileexists("/mnt/workspace/config.yml") ? "/mnt/workspace/config.yml" : null,
-    # fileexists("/mnt/workspace/source/config/config.yaml") ? "/mnt/workspace/source/config/config.yaml" : null,
-    # fileexists("/mnt/workspace/source/config/config.yml") ? "/mnt/workspace/source/config/config.yml" : null,
-    # "/mnt/workspace/config.yaml"
+    var.CONFIG != null && var.CONFIG != "" ? var.CONFIG: try(
+      fileexists("/mnt/workspace/config.yaml") ? "/mnt/workspace/config.yaml" : null,
+      fileexists("/mnt/workspace/config.yml") ? "/mnt/workspace/config.yml" : null,
+      fileexists("/mnt/workspace/source/config/config.yaml") ? "/mnt/workspace/source/config/config.yaml" : null,
+      fileexists("/mnt/workspace/source/config/config.yml") ? "/mnt/workspace/source/config/config.yml" : null,
+      "/mnt/workspace/config.yaml"
+    )
   )
   config = try(yamldecode(file(local.config_path)), {})
 }
