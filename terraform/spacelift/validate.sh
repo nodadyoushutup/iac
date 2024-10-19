faf31f8dbe4ad0a1487071070b4fbb83deed5327
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 23:32:15 +0000

    [WIP] Spacelift

--- terraform/spacelift/validate.sh
+++ terraform/spacelift/validate.sh
@@ -4,7 +4,7 @@ PRIVATE_KEY_PATH="$1"
 chmod 600 $PRIVATE_KEY_PATH
 OUTPUT=$(ssh-keygen -l -f "$PRIVATE_KEY_PATH" 2>&1)
 
-la -la /mnt/workspace
+ls -la /mnt/workspace
 
 echo $YAML_FILE
 echo $PRIVATE_KEY_PATH
