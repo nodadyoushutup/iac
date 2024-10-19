02cd6c2e15420dddb5b8c053cdae6bc8a611937d
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 23:57:41 +0000

    [WIP] Spacelift

--- terraform/spacelift/private_key_validation.sh
+++ terraform/spacelift/private_key_validation.sh
@@ -3,12 +3,11 @@
 PRIVATE_KEY_PATH="$1"
 chmod 600 "$PRIVATE_KEY_PATH"
 
-# Run ssh-keygen -y to check if the private key is valid by generating the public key
-OUTPUT=$(ssh-keygen -y -f "$PRIVATE_KEY_PATH" 2>&1)
+FIRST_LINE=$(head -n 1 "$PRIVATE_KEY_PATH")
+LAST_LINE=$(tail -n 1 "$PRIVATE_KEY_PATH")
 
-# Check if ssh-keygen failed
-if echo "$OUTPUT" | grep -q "load failed"; then
-  echo "{\"valid\": \"false\"}"  # Invalid private key
+if echo "$FIRST_LINE" | grep -q "BEGIN OPENSSH PRIVATE KEY" && echo "$LAST_LINE" | grep -q "END OPENSSH PRIVATE KEY"; then
+  echo "{\"valid\": \"true\"}"
 else
-  echo "{\"valid\": \"true\"}"   # Valid private key
+  echo "{\"valid\": \"false\"}"
 fi
