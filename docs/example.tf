b0170bf43fb5aaf6e13875c9a1b48a6faa019e14
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 23:15:38 +0000

    [WIP] Spacelift

---
+++ docs/example.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "run_private_key_validation" {
+  provisioner "local-exec" {
+    command = "${path.module}/validate.sh ${local.config.path.private_key}"
+  }
+
+  triggers = {
+    run = random_id.trigger.hex
+  }
+}
+
+output "script_ran" {
+  value = "Script has been executed without JSON output."
+}
