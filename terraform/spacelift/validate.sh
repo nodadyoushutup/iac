5e2970ce8348f95b7403c7f05f479fbc3ce6f943
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 23:13:01 +0000

    [WIP] Spacelift

--- terraform/spacelift/validate.sh
+++ terraform/spacelift/validate.sh
@@ -2,25 +2,17 @@
 # Path to the YAML file (from the environment variable)
 YAML_FILE="$1"
 
-
-
 # Extract the value of path.private_key using grep and awk
 PRIVATE_KEY_PATH=$(grep 'private_key' "$YAML_FILE" | awk -F '*:*' '{print $2}' | sed 's/"//g' | tr -d ' ')
 
 # Run ssh-keygen and capture both stdout and stderr into a variable
 OUTPUT=$(ssh-keygen -l -f "$PRIVATE_KEY_PATH" 2>&1)
 
-ls -la /mnt/workspace
-echo $OUTPUT
-
 # Check if the output contains the phrase "not a public key file"
 if echo "$OUTPUT" | grep -q "not a public key file"; then
-  echo "+++++++++++++++++++++++++++++"
-  echo $OUTPUT
+  echo "{\"valid\": \"false\"}" 
 elif echo "$OUTPUT" | grep -q "No such file or directory"; then
-  echo "+++++++++++++++++++++++++++++"
-  echo $OUTPUT
+  echo "{\"valid\": \"false\"}" 
 else
-  echo "+++++++++++++++++++++++++++++"
-  echo $OUTPUT
+  echo "{\"valid\": \"true\"}" 
 fi
