7b191d51d3b2996dee5e5bdb4b98a32bfd907f52
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 23:21:00 +0000

    [WIP] Spacelift

--- terraform/spacelift/validate.sh
+++ terraform/spacelift/validate.sh
@@ -3,7 +3,7 @@
 YAML_FILE="$1"
 
 # Extract the value of path.private_key using grep and awk
-PRIVATE_KEY_PATH=$(grep 'private_key' "$YAML_FILE" | awk -F '*:*' '{print $2}' | sed 's/"//g' | tr -d ' ')
+PRIVATE_KEY_PATH=$(grep 'private_key' "$YAML_FILE" | awk -F ':' '{print $2}' | sed 's/"//g' | tr -d ' ')
 
 # Run ssh-keygen and capture both stdout and stderr into a variable
 OUTPUT=$(ssh-keygen -l -f "$PRIVATE_KEY_PATH" 2>&1)
