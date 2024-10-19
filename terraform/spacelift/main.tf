bd6c41fcce4a26b1a759cfc0d1689e499e623ceb
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 23:02:00 +0000

    [WIP] Spacelift

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -89,6 +89,21 @@ output "valid_check" {
 
 
 
+resource "null_resource" "run_private_key_validation" {
+  provisioner "local-exec" {
+    command = "${path.module}/private_key_validation.sh ${local.config.path.private_key}"
+  }
+
+  triggers = {
+    run = random_id.trigger.hex
+  }
+}
+
+output "script_ran" {
+  value = "Script has been executed without JSON output."
+}
+
+
 # ### DOCKER ###
 # resource "spacelift_stack" "docker_infra_stack" {
 #     depends_on = [spacelift_context.ansible_hook_context]
