2d922ca606caa489c1955063c3d20e8b4baf4ebc
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 19:29:25 +0000

    [WIP] Spacelift

--- terraform/spacelift/local.tf
+++ terraform/spacelift/local.tf
@@ -1,11 +1,11 @@
 locals {
   config_path = try(
-    var.CONFIG != null && var.CONFIG != "" ? var.CONFIG: try(
+    var.CONFIG != null && var.CONFIG != "" ? var.CONFIG : try(
       fileexists("/mnt/workspace/config.yaml") ? "/mnt/workspace/config.yaml" : null,
       fileexists("/mnt/workspace/config.yml") ? "/mnt/workspace/config.yml" : null,
       fileexists("/mnt/workspace/source/config/config.yaml") ? "/mnt/workspace/source/config/config.yaml" : null,
       fileexists("/mnt/workspace/source/config/config.yml") ? "/mnt/workspace/source/config/config.yml" : null,
-      "/mnt/workspace/config.yaml"
+      "/mnt/workspace/config2.yaml"
     )
   )
   config = try(yamldecode(file(local.config_path)), {})
