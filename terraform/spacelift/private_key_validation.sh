f92c88bc020698a622dd6fac4ae1bea6d8c644f5
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 22:53:36 +0000

    [WIP] Spacelift

--- terraform/spacelift/private_key_validation.sh
+++ terraform/spacelift/private_key_validation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Path to the YAML file (from the environment variable)
-YAML_FILE="${TF_VAR_CONFIG}"
+YAML_FILE="$1"
 
 # Extract the value of path.private_key using grep and awk
 PRIVATE_KEY_PATH=$(grep 'private_key' "$YAML_FILE" | awk -F ': *' '{print $2}' | sed 's/"//g' | tr -d ' ')
