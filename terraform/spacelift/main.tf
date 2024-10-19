d8ee3886135b8601458ee169dd7934674ce9dd4f
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 22:03:28 +0000

    [WIP] Spacelift

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -81,7 +81,7 @@ resource "spacelift_environment_variable" "env_environment_variable" {
     ]
     context_id  = spacelift_context.config_context.id
     name        = "TF_VAR_ENV" 
-    value       = data.external.private_key_validation.result["valid"] == "true" ? local.env + 1 : 0
+    value       = data.external.private_key_validation.result["valid"] == "true" ? local.env + 1 : local.env
     write_only  = false 
     description = "Flag for valid environment initialization"
 }
