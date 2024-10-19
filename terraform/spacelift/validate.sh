2482aef0fe20644e2585acc07baf897a0d75e2c1
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 23:24:31 +0000

    [WIP] Spacelift

--- terraform/spacelift/validate.sh
+++ terraform/spacelift/validate.sh
@@ -7,12 +7,11 @@ PRIVATE_KEY_PATH=$(grep 'private_key' "$YAML_FILE" | awk -F ':' '{print $2}' | s
 
 # Run ssh-keygen and capture both stdout and stderr into a variable
 OUTPUT=$(ssh-keygen -l -f "$PRIVATE_KEY_PATH" 2>&1)
+echo $PRIVATE_KEY_PATH
+echo $OUTPUT
 
-# Check if the output contains the phrase "not a public key file"
 if echo "$OUTPUT" | grep -q "(RSA)"; then
-  echo $OUTPUT
   echo "{\"valid\": \"true\"}" 
 else
-  echo $OUTPUT
   echo "{\"valid\": \"false\"}" 
 fi
