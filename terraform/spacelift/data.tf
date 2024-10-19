72a661970bb757f261a7808f5be5bd7091e8260f
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 19:09:27 +0000

    [WIP] Spacelift

--- terraform/spacelift/data.tf
+++ terraform/spacelift/data.tf
@@ -2,11 +2,11 @@ data "spacelift_stack" "spacelift" {
   stack_id = "spacelift"
 }
 
-data "spacelift_context" "config" {
-  context_id = "config"
-}
+# data "spacelift_context" "config" {
+#   context_id = "config"
+# }
 
-data "spacelift_environment_variable" "private_key" {
-  context_id = "config"
-  name       = "PRIVATE_KEY"
-}
+# data "spacelift_environment_variable" "private_key" {
+#   context_id = "config"
+#   name       = "PRIVATE_KEY"
+# }
