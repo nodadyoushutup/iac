012c534f1076087c97f0fc12aca4f3d798a27936
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 23:42:16 +0000

    [WIP] Spacelift

--- terraform/spacelift/private_key_validation.sh
+++ terraform/spacelift/private_key_validation.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-# Path to the YAML file (from the environment variable)
-YAML_FILE="$1"
 
-# Extract the value of path.private_key using grep and awk
-PRIVATE_KEY_PATH=$(grep 'private_key' "$YAML_FILE" | awk -F '*:*' '{print $2}' | sed 's/"//g' | tr -d ' ')
+PRIVATE_KEY_PATH="$1"
+chmod 600 "$PRIVATE_KEY_PATH"
 
-# Run ssh-keygen and capture both stdout and stderr into a variable
-OUTPUT=$(ssh-keygen -l -f "$PRIVATE_KEY_PATH" 2>&1)
+# Run ssh-keygen -y to check if the private key is valid by generating the public key
+OUTPUT=$(ssh-keygen -y -f "$PRIVATE_KEY_PATH" 2>&1)
 
-# Check if the output contains the phrase "not a public key file"
-if echo "$OUTPUT" | grep -q "not a public key file"; then
-  echo "{\"valid\": \"false\"}" 
-elif echo "$OUTPUT" | grep -q "No such file or directory"; then
-  echo "{\"valid\": \"false\"}" 
+# Check if ssh-keygen failed
+if echo "$OUTPUT" | grep -q "load failed"; then
+  echo "{\"valid\": \"false\"}"  # Invalid private key
 else
-  echo "{\"valid\": \"true\"}" 
+  echo "{\"valid\": \"true\"}"   # Valid private key
 fi
