cadd1827020475bdbf561de33255548d0b4e4e61
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 17:35:58 +0000

    [WIP] Spacelift

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -1,5 +1,5 @@
 ### ANSIBLE ###
-resource "spacelift_context" "ansible_hook" {
+resource "spacelift_context" "ansible_hook_context" {
     description = "Ansible hook"
     name        = "ansible_hook"
     before_init = [
@@ -9,7 +9,7 @@ resource "spacelift_context" "ansible_hook" {
 
 ### DOCKER ###
 resource "spacelift_stack" "docker_infra_stack" {
-    # depends_on = [spacelift_context.ansible_hook]
+    depends_on = [spacelift_context.ansible_hook_context]
     administrative = true
     autodeploy = true
     branch = "main"
@@ -36,35 +36,35 @@ resource "spacelift_stack" "docker_init_stack" {
     }
 }
 
-resource "spacelift_context_attachment" "docker_infra_context_attachment_config" {
+resource "spacelift_context_attachment" "docker_infra_config_context_attachment" {
     depends_on = [spacelift_stack.docker_infra_stack]
     context_id = data.spacelift_context.config.id
     stack_id   = spacelift_stack.docker_infra_stack.id
     priority   = 0
 }
 
-resource "spacelift_context_attachment" "docker_init_context_attachment_config" {
+resource "spacelift_context_attachment" "docker_init_config_context_attachment" {
     depends_on = [spacelift_stack.docker_init_stack]
     context_id = data.spacelift_context.config.id
     stack_id   = spacelift_stack.docker_init_stack.id
     priority   = 0
 }
 
-resource "spacelift_context_attachment" "docker_init_context_attachment_ansible_hook" {
+resource "spacelift_context_attachment" "docker_init_ansible_hook_context_attachment" {
     depends_on = [spacelift_stack.docker_init_stack, spacelift_context.ansible_hook]
     context_id = spacelift_context.ansible_hook.id
     stack_id   = spacelift_stack.docker_init_stack.id
     priority   = 0
 }
 
-resource "spacelift_stack_dependency" "spacelift_docker_infra" {
+resource "spacelift_stack_dependency" "docker_infra_spacelift_stack_dependency" {
   count = local.config.dependency_deploy.infra ? 1 : 0
   depends_on = [data.spacelift_stack.spacelift, spacelift_stack.docker_infra_stack]
   stack_id            = spacelift_stack.docker_infra_stack.id
   depends_on_stack_id = data.spacelift_stack.spacelift.id
 }
 
-resource "spacelift_stack_dependency" "docker_infra_docker_init" {
+resource "spacelift_stack_dependency" "docker_init_docker_infra_stack_dependency" {
   count = local.config.dependency_deploy.init ? 1 : 0
   depends_on = [spacelift_stack.docker_infra_stack, spacelift_stack.docker_init_stack]
   stack_id            = spacelift_stack.docker_init_stack.id
@@ -87,23 +87,32 @@ resource "spacelift_stack" "prometheus_init_stack" {
     }
 }
 
-resource "spacelift_context_attachment" "prometheus_init_context_attachment_config" {
+resource "spacelift_context_attachment" "prometheus_init_config_context_attachment" {
     depends_on = [spacelift_stack.prometheus_init_stack]
     context_id = data.spacelift_context.config.id
     stack_id   = spacelift_stack.prometheus_init_stack.id
     priority   = 0
 }
 
-resource "spacelift_context_attachment" "prometheus_init_context_attachment_ansible_hook" {
+resource "spacelift_context_attachment" "prometheus_init_ansible_hook_context_attachment" {
     depends_on = [spacelift_stack.prometheus_init_stack, spacelift_context.ansible_hook]
     context_id = spacelift_context.ansible_hook.id
     stack_id   = spacelift_stack.prometheus_init_stack.id
     priority   = 0
 }
 
-resource "spacelift_stack_dependency" "docker_init_prometheus_init" {
+resource "spacelift_stack_dependency" "prometheus_init_docker_init_stack_dependency" {
     count = local.config.dependency_deploy.init ? 1 : 0
     depends_on = [spacelift_stack.docker_init_stack, spacelift_stack.prometheus_init_stack]
     stack_id            = spacelift_stack.prometheus_init_stack.id
     depends_on_stack_id = spacelift_stack.docker_init_stack.id
+}
+
+### TEST ###
+resource "spacelift_environment_variable" "test_env_var" {
+  context_id  = "config"
+  name        = "TEST_ENV_VAR"
+  value       = try(var.TEST_ENV_VAR, "/project/spacelift/kubeconfig")
+  write_only  = false
+  description = "Test environment variable"
 }
--- terraform/spacelift/variable.tf
+++ terraform/spacelift/variable.tf
@@ -3,3 +3,9 @@ variable "CONFIG" {
   description = "Terraform config path"
   default = null
 }
+
+variable "TEST_ENV_VAR" {
+  type = string
+  description = "test"
+  default = null
+}
