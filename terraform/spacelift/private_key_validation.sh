a81fac87121a5107db321b52265818c882cbe889
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 21:27:19 +0000

    [WIP] Spacelift

--- terraform/spacelift/private_key_validation.sh
+++ terraform/spacelift/private_key_validation.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # Path to the YAML file (from the environment variable)
 YAML_FILE="${TF_VAR_CONFIG}"
 
-# Extract the value of path.private_key using yq
-PRIVATE_KEY_PATH=$(yq e '.path.private_key' "$YAML_FILE")
-
+# Extract the value of path.private_key using grep and awk
+PRIVATE_KEY_PATH=$(grep 'private_key' $YAML_FILE | awk -F ': *' '{print $2}' | sed 's/"//g' | tr -d ' ')
+chmod 600 $PRIVATE_KEY_PATH
 # Check if the private key is valid
 if ssh-keygen -l -f "$PRIVATE_KEY_PATH" >/dev/null 2>&1; then
   echo "{\"valid\": \"true\"}"  # Return true as a string
