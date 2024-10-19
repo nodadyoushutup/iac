7f144977c70daba54f38a937884200c25c232f80
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 22:50:12 +0000

    [WIP] Spacelift

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -4,7 +4,7 @@ resource "random_id" "trigger" {
 }
 
 data "external" "private_key_validation" {
-    program = ["bash", "${path.module}/private_key_validation.sh", "/mnt/workspace/id_rsa"]
+    program = ["bash", "${path.module}/private_key_validation.sh", local.config.path.private_key]
     query = {trigger = random_id.trigger.hex}
     depends_on = [random_id.trigger]
 }
