179430e1c2938a38d37b328b5c263580f7dc3553
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-19 20:05:45 +0000

    [WIP] Spacelift

--- terraform/spacelift/local.tf
+++ terraform/spacelift/local.tf
@@ -2,5 +2,5 @@ locals {
   config_path = try(var.CONFIG != null && var.CONFIG != "" ? var.CONFIG : "/mnt/workspace/config.yaml")
   git_config_path = try(var.GIT_CONFIG != null && var.GIT_CONFIG != "" ? var.GIT_CONFIG : "/mnt/workspace/.gitconfig")
   config = try(yamldecode(file(local.git_config_path)), {})
-  config_base64 = try(filebase64(local.config_path), filebase64("/mnt/workspace/source/config/empty.yaml"))
+  config_base64 = try(filebase64(local.config_path), filebase64("/mnt/workspace/source/config/empty.yaml")) #
 }
