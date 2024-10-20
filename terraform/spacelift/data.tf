6bf2867208e47d984d83d0f21eea3fdeeaeb1c9a
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-20 00:16:51 +0000

    [WIP] Spacelift

--- terraform/spacelift/data.tf
+++ terraform/spacelift/data.tf
@@ -3,7 +3,7 @@ data "spacelift_stack" "spacelift" {
 }
 
 data "external" "private_key_validation" {
-    program = ["bash", "${path.module}/private_key_validation.sh", local.config.path.private_key]
+    program = ["bash", "${path.module}/validate_env.sh", local.config.path.private_key]
     query = {trigger = random_id.trigger.hex}
     depends_on = [random_id.trigger]
 }
--- terraform/spacelift/validate_env.sh
+++ terraform/spacelift/validate_env.sh
@@ -6,7 +6,14 @@ chmod 600 "$PRIVATE_KEY_PATH"
 FIRST_LINE=$(head -n 1 "$PRIVATE_KEY_PATH")
 LAST_LINE=$(tail -n 1 "$PRIVATE_KEY_PATH")
 
+PRIVATE_KEY_VALID="false"
+
 if echo "$FIRST_LINE" | grep -q "BEGIN OPENSSH PRIVATE KEY" && echo "$LAST_LINE" | grep -q "END OPENSSH PRIVATE KEY"; then
+  PRIVATE_KEY_VALID="true"
+fi
+
+# Check the value of PRIVATE_KEY_VALID and echo the result
+if [ "$PRIVATE_KEY_VALID" = "true" ]; then
   echo "{\"valid\": \"true\"}"
 else
   echo "{\"valid\": \"false\"}"
