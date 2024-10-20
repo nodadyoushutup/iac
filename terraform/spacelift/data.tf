88f836adf831ba46819df9e7e6c8efe13fcbd2b0
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-20 00:35:31 +0000

    [WIP] Spacelift

--- terraform/spacelift/data.tf
+++ terraform/spacelift/data.tf
@@ -2,8 +2,3 @@ data "spacelift_stack" "spacelift" {
   stack_id = "spacelift"
 }
 
-data "external" "validate_env" {
-    program = ["bash", "${path.module}/validate_env.sh", local.config.path.private_key]
-    query = {trigger = random_id.trigger.hex}
-    depends_on = [random_id.trigger]
-}
