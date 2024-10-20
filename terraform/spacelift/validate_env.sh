b44163c8de767c41c4415e35ff9eb42437030d23
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-20 00:30:11 +0000

    [WIP] Spacelift

--- terraform/spacelift/validate_env.sh
+++ terraform/spacelift/validate_env.sh
@@ -1,17 +1,37 @@
 #!/bin/sh
 
 PRIVATE_KEY_PATH="$1"
+GITCONFIG_PATH="$2"
+
+# Check for the private key validity
 PRIVATE_KEY_FIRST_LINE=$(head -n 1 "$PRIVATE_KEY_PATH")
 PRIVATE_KEY_LAST_LINE=$(tail -n 1 "$PRIVATE_KEY_PATH")
 PRIVATE_KEY_VALID="false"
 chmod 600 "$PRIVATE_KEY_PATH"
-if echo "$PRIVATE_KEY_FIRST_LINE" | grep -q "BEGIN OPENSSH PRIVATE KEY" && echo "$PRIVATE_KEY_LAST_LINE" | grep -q "END OPENSSH PRIVATE KEY"; then
-  PRIVATE_KEY_VALID="true"
+
+if [ -f "$PRIVATE_KEY_PATH" ]; then
+  if echo "$PRIVATE_KEY_FIRST_LINE" | grep -q "BEGIN OPENSSH PRIVATE KEY" && echo "$PRIVATE_KEY_LAST_LINE" | grep -q "END OPENSSH PRIVATE KEY"; then
+    PRIVATE_KEY_VALID="true"
+  fi
 fi
 
-# Check the value of PRIVATE_KEY_VALID and echo the result
+# Check for the gitconfig file validity (simple check for existence and basic format)
+if [ -f "$GITCONFIG_PATH" ]; then
+  if grep -A 1 "\[user\]" "$GITCONFIG_PATH" | grep -qE "^\s*name\s*=\s*\S+"; then
+    GITCONFIG_VALID="true"
+  fi
+fi
+
+# Check the value of PRIVATE_KEY_VALID and echo the result for the private key
 if [ "$PRIVATE_KEY_VALID" = "true" ]; then
-  echo "{\"valid\": \"true\"}"
+  echo "{\"private_key_valid\": \"true\"}"
 else
-  echo "{\"valid\": \"false\"}"
+  echo "{\"private_key_valid\": \"false\"}"
+fi
+
+# Check the value of GITCONFIG_VALID and echo the result for the gitconfig
+if [ "$GITCONFIG_VALID" = "true" ]; then
+  echo "{\"gitconfig_valid\": \"true\"}"
+else
+  echo "{\"gitconfig_valid\": \"false\"}"
 fi
