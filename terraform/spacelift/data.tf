e18993919105c94e9cf79f80917db0e22de2166a
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-20 00:11:05 +0000

    [WIP] Spacelift

--- terraform/spacelift/data.tf
+++ terraform/spacelift/data.tf
@@ -2,8 +2,8 @@ data "spacelift_stack" "spacelift" {
   stack_id = "spacelift"
 }
 
-data "external" "private_key_validation" {
-    program = ["bash", "${path.module}/private_key_validation.sh", local.config.path.private_key]
-    query = {trigger = random_id.trigger.hex}
-    depends_on = [random_id.trigger]
-}
+# data "external" "private_key_validation" {
+#     program = ["bash", "${path.module}/private_key_validation.sh", local.config.path.private_key]
+#     query = {trigger = random_id.trigger.hex}
+#     depends_on = [random_id.trigger]
+# }
