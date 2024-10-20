39239c4e88aa4e3cdb80c9da3a2ce8b9c0eb0e85
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-20 01:50:58 +0000

    [WIP] Spacelift

--- terraform/spacelift/local.tf
+++ terraform/spacelift/local.tf
@@ -1,4 +1,5 @@
 locals {
+  env = try(var.ENV != null && var.ENV != "" && var.ENV > 0 ? var.ENV : 0, 0)
   config_path = try(var.CONFIG != null && var.CONFIG != "" ? var.CONFIG : "/mnt/workspace/config.yaml")
   config = try(yamldecode(file(local.config_path)), {
     path = {
@@ -9,7 +10,6 @@ locals {
       docker_compose = "/mnt/workspace/source/config/docker-compose.yaml"
     }
   })
-  env = try(var.ENV != null && var.ENV != "" && var.ENV > 0 ? var.ENV : 0, 0)
   base64 = {
     config = try(filebase64(local.config_path), filebase64("/mnt/workspace/source/default/config.yaml"))
     private_key = try(filebase64(local.config.path.private_key), filebase64("/mnt/workspace/source/default/id_rsa"))
