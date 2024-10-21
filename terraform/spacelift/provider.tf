2cd730403b40e4dbb4d6cfd96f8b818534dac44c
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-21 23:07:40 +0000

    [WIP] Spacelift

--- terraform/spacelift/provider.tf
+++ terraform/spacelift/provider.tf
@@ -4,5 +4,23 @@ terraform {
       source  = "spacelift-io/spacelift"
       version = ">= 0.2.0"
     }
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = local.config.provider.proxmox.endpoint
+  insecure  = local.config.provider.proxmox.insecure
+  username = local.config.provider.proxmox.username
+  password = local.config.provider.proxmox.password
+  ssh {
+    private_key = file(local.config.path.private_key)
+    node {
+      name = local.config.provider.proxmox.ssh.node.name
+      address = local.config.provider.proxmox.ssh.node.address
+      port = local.config.provider.proxmox.ssh.node.port
+    }
   }
 }
