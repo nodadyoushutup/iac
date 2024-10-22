b524d89b2ab0a4d76e9fe930cafdbc81d9472f01
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-22 01:18:22 +0000

    [WIP] Docker

--- terraform/docker/local.tf
+++ terraform/docker/local.tf
@@ -1,7 +1,7 @@
 locals {
   config_path = try(var.CONFIG != null && var.CONFIG != "" ? var.CONFIG : "/mnt/workspace/config.yaml")
   config = try(yamldecode(file(local.config_path)), {})
-  cloud_config = try(file("/mnt/workspace/cloud_config.yaml"), "<<-EOF EOF")
+  cloud_config = try("<<-EOF ${file("/mnt/workspace/cloud_config.yaml")} EOF", "<<-EOF EOF")
 }
 
 output "cloud_config" {
