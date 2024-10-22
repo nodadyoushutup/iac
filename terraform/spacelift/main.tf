589bff4b401e19d6198faef31b96438f8f4e4407
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-22 02:51:46 +0000

    [WIP] Docker

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -73,15 +73,6 @@ resource "spacelift_environment_variable" "ansible_config_environment_variable" 
     description = "Ansible configuration path"
 }
 
-resource "spacelift_environment_variable" "ansible_config_environment_variable" { 
-    depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
-    context_id  = spacelift_context.config_context.id
-    name        = "ANSIBLE_CONFIG" 
-    value       = local.ansible_config_path
-    write_only  = false 
-    description = "Ansible configuration path"
-}
-
 resource "spacelift_environment_variable" "tf_log_environment_variable" { 
     depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
     context_id  = spacelift_context.config_context.id
