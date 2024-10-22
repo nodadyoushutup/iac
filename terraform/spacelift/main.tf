40606df1b8c8799423b2a8e98bf8a16f58510ccc
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-22 04:06:35 +0000

    [WIP] Spacelift

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -12,94 +12,94 @@ resource "spacelift_context_attachment" "spacelift_config_context_attachment" {
     priority   = 0
 }
 
-# resource "spacelift_context" "ansible_hook_context" {
-#     depends_on = [data.spacelift_stack.spacelift]
-#     description = "Ansible hook"
-#     name        = "ansible_hook"
-#     before_init = [
-#         "chmod 600 ${local.config.path.private_key}"
-#     ]
-# }
+resource "spacelift_context" "ansible_hook_context" {
+    depends_on = [data.spacelift_stack.spacelift]
+    description = "Ansible hook"
+    name        = "ansible_hook"
+    before_init = [
+        "chmod 600 ${local.config.path.private_key}"
+    ]
+}
 
-# ### MOUNTED FILE ###
-# resource "spacelift_mounted_file" "config_mounted_file" {
-#     depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
-#     context_id = spacelift_context.config_context.id
-#     relative_path = "config.yaml"
-#     content = local.base64.config
-#     write_only = false
-# }
+### MOUNTED FILE ###
+resource "spacelift_mounted_file" "config_mounted_file" {
+    depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
+    context_id = spacelift_context.config_context.id
+    relative_path = "config.yaml"
+    content = local.base64.config
+    write_only = false
+}
 
-# resource "spacelift_mounted_file" "private_keymounted_file" {
-#     depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
-#     context_id = spacelift_context.config_context.id
-#     relative_path = "id_rsa"
-#     content = local.base64.private_key
-#     write_only = true
-# }
+resource "spacelift_mounted_file" "private_keymounted_file" {
+    depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
+    context_id = spacelift_context.config_context.id
+    relative_path = "id_rsa"
+    content = local.base64.private_key
+    write_only = true
+}
 
-# resource "spacelift_mounted_file" "inventory_keymounted_file" {
-#     depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
-#     context_id = spacelift_context.config_context.id
-#     relative_path = "inventory"
-#     content = local.base64.inventory
-#     write_only = false
-# }
+resource "spacelift_mounted_file" "inventory_keymounted_file" {
+    depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
+    context_id = spacelift_context.config_context.id
+    relative_path = "inventory"
+    content = local.base64.inventory
+    write_only = false
+}
 
-# resource "spacelift_mounted_file" "env_keymounted_file" {
-#     depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
-#     context_id = spacelift_context.config_context.id
-#     relative_path = ".env"
-#     content = local.base64.env
-#     write_only = false
-# }
+resource "spacelift_mounted_file" "env_keymounted_file" {
+    depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
+    context_id = spacelift_context.config_context.id
+    relative_path = ".env"
+    content = local.base64.env
+    write_only = false
+}
 
-# ### ENVIRONMENT VARIABLES ###
-# resource "spacelift_environment_variable" "config_environment_variable" { 
-#     depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
-#     context_id  = spacelift_context.config_context.id
-#     name        = "TF_VAR_CONFIG" 
-#     value       = local.config_path
-#     write_only  = false 
-#     description = "Terraform configuration path"
-# }
+### ENVIRONMENT VARIABLES ###
+resource "spacelift_environment_variable" "config_environment_variable" { 
+    depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
+    context_id  = spacelift_context.config_context.id
+    name        = "TF_VAR_CONFIG" 
+    value       = local.config_path
+    write_only  = false 
+    description = "Terraform configuration path"
+}
 
-# resource "spacelift_environment_variable" "ansible_config_environment_variable" { 
-#     depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
-#     context_id  = spacelift_context.config_context.id
-#     name        = "ANSIBLE_CONFIG" 
-#     value       = local.ansible_config_path
-#     write_only  = false 
-#     description = "Ansible configuration path"
-# }
+resource "spacelift_environment_variable" "ansible_config_environment_variable" { 
+    depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
+    context_id  = spacelift_context.config_context.id
+    name        = "ANSIBLE_CONFIG" 
+    value       = local.ansible_config_path
+    write_only  = false 
+    description = "Ansible configuration path"
+}
 
-# resource "spacelift_environment_variable" "private_key_environment_variable" { 
-#     depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
-#     context_id  = spacelift_context.config_context.id
-#     name        = "PRIVATE_KEY" 
-#     value       = local.private_key
-#     write_only  = false 
-#     description = "SSH private Key"
-# }
+resource "spacelift_environment_variable" "private_key_environment_variable" { 
+    depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
+    context_id  = spacelift_context.config_context.id
+    name        = "PRIVATE_KEY" 
+    value       = local.private_key
+    write_only  = false 
+    description = "SSH private Key"
+}
 
-# resource "spacelift_environment_variable" "tf_log_environment_variable" { 
-#     depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
-#     context_id  = spacelift_context.config_context.id
-#     name        = "TF_LOG" 
-#     value       = "debug"
-#     write_only  = false 
-#     description = "Terraform log level"
-# }
+resource "spacelift_environment_variable" "tf_log_environment_variable" { 
+    depends_on = [spacelift_context_attachment.spacelift_config_context_attachment]
+    context_id  = spacelift_context.config_context.id
+    name        = "TF_LOG" 
+    value       = "debug"
+    write_only  = false 
+    description = "Terraform log level"
+}
 
 resource "spacelift_environment_variable" "env_environment_variable" { 
     depends_on = [
         spacelift_context.config_context,
-        # spacelift_mounted_file.config_mounted_file,
-        # spacelift_mounted_file.private_keymounted_file,
-        # spacelift_mounted_file.env_keymounted_file,
-        # spacelift_mounted_file.private_keymounted_file,
-        # spacelift_environment_variable.config_environment_variable,
-        # spacelift_environment_variable.tf_log_environment_variable
+        spacelift_mounted_file.config_mounted_file,
+        spacelift_mounted_file.private_keymounted_file,
+        spacelift_mounted_file.env_keymounted_file,
+        spacelift_mounted_file.private_keymounted_file,
+        spacelift_environment_variable.config_environment_variable,
+        spacelift_environment_variable.tf_log_environment_variable
     ]
     context_id  = spacelift_context.config_context.id
     name        = "TF_VAR_ENV" 
@@ -114,21 +114,8 @@ resource "random_id" "trigger" {
     byte_length = 8
 }
 
-data "external" "validate_env" {
-    program = [
-        "bash", 
-        "${path.module}/validate_env.sh", 
-        local.config.path.private_key, 
-        local.config.path.gitconfig, 
-        local.config.path.inventory,
-        local.config.path.env
-    ]
-    query = {trigger = random_id.trigger.hex}
-    depends_on = [random_id.trigger]
-}
-
 output "valid_check" {
-  value = data.external.validate_env.result["valid"]
+  value = data.external.validate_env.result["valid"] == "true" ? "Environment configuration is valid" : "Environment configuration is not valid"
 }
 
 
