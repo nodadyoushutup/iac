5e460542ba37e69440d21ca5bb47bfa4e3b12d3e
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-22 00:09:42 +0000

    [WIP] Docker

--- terraform/docker/local.tf
+++ terraform/docker/local.tf
@@ -1,15 +1,5 @@
 locals {
   config_path = try(var.CONFIG != null && var.CONFIG != "" ? var.CONFIG : "/mnt/workspace/config.yaml")
-  config = try(yamldecode(file(local.config_path)), {
-    path = {
-      private_key     = "/mnt/workspace/source/default/id_rsa"
-      inventory       = "/mnt/workspace/source/default/inventory"
-      env             = "/mnt/workspace/source/default/.env"
-      gitconfig       = "/mnt/workspace/source/config/.gitconfig"
-      ansible         = "/mnt/workspace/source/config/ansible.cfg"
-      prometheus      = "/mnt/workspace/source/config/prometheus.yaml"
-      docker_compose  = "/mnt/workspace/source/config/docker-compose.yaml"
-    }
-  })
+  config = try(yamldecode(file(local.config_path)), {})
   cloud_config = try(file("/mnt/workspace/cloud_config.yaml"), "<<-EOF EOF")
 }
