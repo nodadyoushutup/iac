049bcaed8f557668e76a6b3d9c28b285d35fcdbd
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-22 04:17:31 +0000

    [WIP] Spacelift

--- terraform/spacelift/output.tf
+++ terraform/spacelift/output.tf
@@ -1,3 +1,3 @@
-output "valid_check" {
+output "env_valid_check" {
   value = data.external.validate_env.result["valid"] == "true" ? "Environment configuration is valid" : "Environment configuration is not valid"
 }
