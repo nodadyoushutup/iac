7e578fdbdca8003a6b8574e7a683864162a7b105
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-22 01:42:38 +0000

    [WIP] Docker

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -3,7 +3,18 @@ resource "proxmox_virtual_environment_file" "docker_cloud_config" {
   datastore_id = try(local.config.virtual_machine.datastore_id.cloud_config)
   node_name    = try(local.config.provider.proxmox.ssh.node.name)
   source_raw {
-    data = local.cloud_config
+    data = <<-EOF
+    #cloud-config
+        users:
+          - default
+          - name: ubuntu
+            passwd: $6$rounds=4096$T8DMtimvQVZEyTGr$I3wHi8.0NxA938poCUOqtqcJQbAt335MfHF.lpS8Fdwfnt45vH5goXxPQ.RXBGef2yegKPero/PFYvKOmJWeS1
+            groups:
+              - sudo
+            shell: /bin/bash
+            ssh_authorized_keys: []
+            sudo: ALL=(ALL) NOPASSWD:ALL
+    EOF
     file_name = "docker-cloud-config.yaml"
   }
 }
