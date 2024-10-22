946f6d13b21829b1f1c8941d73860432405f4de3
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-22 04:34:48 +0000

    [WIP] Spacelift

--- terraform/spacelift/data.tf
+++ terraform/spacelift/data.tf
@@ -12,8 +12,8 @@ data "external" "validate_env" {
         "${path.module}/validate_env.sh", 
         local.config.path.private_key, 
         local.config.path.gitconfig, 
-        local.config.path.inventory,
-        local.config.path.env
+        local.config.path.ansible.inventory,
+        local.config.path.docker.env
     ]
     query = {trigger = random_id.trigger.hex}
     depends_on = [random_id.trigger]
