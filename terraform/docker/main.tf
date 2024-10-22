de918329473d9d276888f6d494baad9fcec4ec97
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-22 01:49:53 +0000

    [WIP] Docker

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -28,49 +28,49 @@ resource "proxmox_virtual_environment_file" "cloud_config" {
 }
 
 
-# resource "proxmox_virtual_environment_vm" "docker_vm" {
-#   depends_on = [proxmox_virtual_environment_file.cloud_config]
-#   name = "docker"
-#   vm_id = 101
-#   description = "docker"
-#   tags = ["terraform", "ubuntu", "docker"]
-#   node_name = try(local.config.provider.proxmox.ssh.node.name)
-#   agent {
-#     enabled = false
-#   }
-#   stop_on_destroy = true
-#   startup {
-#     order = 1
-#     up_delay = 0
-#     down_delay = 0
-#   }
-#   cpu {
-#     cores = 4
-#     type = "x86-64-v2-AES"
-#   }
-#   memory {
-#     dedicated = 4096
-#   }
-#   disk {
-#     datastore_id = try(local.config.virtual_machine.datastore_id.disk)
-#     file_id = "${local.config.virtual_machine.datastore_id.iso}:iso/${local.config.virtual_machine.file_id}"
-#     interface = "scsi0"
-#     size = 100
-#   }
-#   initialization {
-#     ip_config {
-#       ipv4 {
-#         address = try(local.config.virtual_machine.docker.address)
-#         gateway = try(local.config.virtual_machine.docker.gateway)
-#       }
-#     }
-#     user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
-#   }
-#   network_device {
-#     bridge = "vmbr0"
-#   }
-#   operating_system {
-#     type = "l26"
-#   }
-# }
+resource "proxmox_virtual_environment_vm" "docker_vm" {
+  depends_on = [proxmox_virtual_environment_file.cloud_config]
+  name = "docker"
+  vm_id = 101
+  description = "docker"
+  tags = ["terraform", "ubuntu", "docker"]
+  node_name = try(local.config.provider.proxmox.ssh.node.name)
+  agent {
+    enabled = false
+  }
+  stop_on_destroy = true
+  startup {
+    order = 1
+    up_delay = 0
+    down_delay = 0
+  }
+  cpu {
+    cores = 4
+    type = "x86-64-v2-AES"
+  }
+  memory {
+    dedicated = 4096
+  }
+  disk {
+    datastore_id = try(local.config.virtual_machine.datastore_id.disk)
+    file_id = "${local.config.virtual_machine.datastore_id.iso}:iso/${local.config.virtual_machine.file_id}"
+    interface = "scsi0"
+    size = 100
+  }
+  initialization {
+    ip_config {
+      ipv4 {
+        address = try(local.config.virtual_machine.docker.address)
+        gateway = try(local.config.virtual_machine.docker.gateway)
+      }
+    }
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+  }
+  network_device {
+    bridge = "vmbr0"
+  }
+  operating_system {
+    type = "l26"
+  }
+}
 
