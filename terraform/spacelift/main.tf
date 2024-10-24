3320924b1a1229a59739c67c244db8745728c269
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-24 23:49:24 +0000

    [WIP] Spacelift

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -166,7 +166,7 @@ resource "spacelift_stack" "docker_init_stack" {
     branch = "main"
     description = "Docker initialization"
     name = "docker_init"
-    project_root = "ansible/playbook"
+    project_root = "ansible"
     repository = "iac"
     labels = ["ansible", "init", "docker", "administrative", "p1", "p1b"]
     additional_project_globs = [
