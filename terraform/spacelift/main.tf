c5ac998b3c2fb0f2028ba73fa5c4748d98e711d6
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-26 19:01:39 +0000

    [WIP] Grafana

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -340,6 +340,17 @@ resource "spacelift_context_attachment" "grafana_init_config_context_attachment"
     priority   = 0
 }
 
+resource "spacelift_context_attachment" "grafana_config_config_context_attachment" {
+    count = local.env > 0 ? 1 : 0
+    depends_on = [
+        spacelift_stack.grafana_config_stack,
+        spacelift_context.config_context
+    ]
+    context_id = spacelift_context.config_context.id
+    stack_id   = spacelift_stack.grafana_config_stack[count.index].id
+    priority   = 0
+}
+
 resource "spacelift_context_attachment" "grafana_init_ansible_hook_context_attachment" {
     count = local.env > 0 ? 1 : 0
     depends_on = [
