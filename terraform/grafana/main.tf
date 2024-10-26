42dc7aae2527980db14d8dbdc2b2f231df1aac64
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-26 19:58:15 +0000

    [WIP] Grafana

--- terraform/grafana/main.tf
+++ terraform/grafana/main.tf
@@ -1,7 +1,7 @@
 resource "grafana_data_source" "prometheus" {
     name = "prometheus"
     type = "prometheus"
-    url  = "http://${local.config.provider.prometheus.hostname}"
+    url  = local.config.provider.prometheus.hostname
 
   # Uncomment and configure the following lines if needed
   # basic_auth_enabled  = true
