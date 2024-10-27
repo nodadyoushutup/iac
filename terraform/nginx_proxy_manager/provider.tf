fe96330179e92a52880d9bcb7a68f4a974618d84
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-27 22:34:53 +0000

    [WIP] NPM

--- terraform/nginx_proxy_manager/provider.tf
+++ terraform/nginx_proxy_manager/provider.tf
@@ -1,14 +1,13 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "Sander0542/nginxproxymanager"
-    }
-  }
-}
-
-provider "nginxproxymanager" {
-  host     = local.config.provider.nginx_proxy_manager.host
-  username = local.config.provider.nginx_proxy_manager.username
-  password = local.config.provider.nginx_proxy_manager.password
-}
-
+terraform { 
+  required_providers { 
+    nginxproxymanager = { 
+      source = "Sander0542/nginxproxymanager" 
+    } 
+  } 
+} 
+ 
+provider "nginxproxymanager" { 
+  host     = local.config.provider.nginx_proxy_manager.host 
+  username = local.config.provider.nginx_proxy_manager.username 
+  password = local.config.provider.nginx_proxy_manager.password 
+} 
