4a68e50d9d2fc5ce49bcc54f4a295053266b4c90
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-27 18:56:12 +0000

    [WIP] Spacelift

--- terraform/spacelift/nginx_proxy_manager_init.tf
+++ terraform/spacelift/nginx_proxy_manager_init.tf
@@ -1,4 +1,3 @@
-### NGINX PROXY MANAGER ###
 resource "spacelift_stack" "nginx_proxy_manager_init_stack" {
     count = local.env > 0 ? 1 : 0
     depends_on = [spacelift_stack.docker_init_stack]
@@ -10,15 +9,26 @@ resource "spacelift_stack" "nginx_proxy_manager_init_stack" {
     project_root = "ansible"
     repository = "iac"
     labels = ["ansible", "init", "collector", "administrative"]
-    # additional_project_globs = [
-    #     "role/docker_compose",
-    #     "role/config_load",
-    #     "role/vm_init",
-    #     "role/vm_ping"
-    # ]
+    additional_project_globs = [
+        "role/docker_compose",
+        "role/config_load",
+        "role/vm_init",
+        "role/vm_ping"
+    ]
     ansible {
         playbook = "nginx_proxy_manager_init.yaml"
     }
+}
+
+resource "spacelift_context_attachment" "nginx_proxy_manager_init_config_context_attachment" {
+    count = local.env > 0 ? 1 : 0
+    depends_on = [
+        spacelift_stack.nginx_proxy_manager_init_stack,
+        spacelift_context.config_context
+    ]
+    context_id = spacelift_context.config_context.id
+    stack_id   = spacelift_stack.nginx_proxy_manager_init_stack[count.index].id
+    priority   = 0
 }
 
 resource "spacelift_context_attachment" "nginx_proxy_manager_init_terraform_context_attachment" {
