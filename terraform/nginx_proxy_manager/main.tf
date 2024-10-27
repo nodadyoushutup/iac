c2cf5433a57e996c3e4ee7a76625a153221d553a
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-27 23:31:15 +0000

    [WIP] NPM

--- terraform/nginx_proxy_manager/main.tf
+++ terraform/nginx_proxy_manager/main.tf
@@ -5,17 +5,17 @@
 #   certificate_key = file("example.key")
 # }
 
-# resource "nginxproxymanager_proxy_host" "nodadyoushutup" {
-#   domain_names   = ["nodadyoushutup.com"]
-#   forward_host   = "192.168.1.100"
-#   forward_port   = 9055
-#   forward_scheme = "http"
-#   certificate_id = "2"
-#   ssl_forced = true
-#   hsts_enabled = false
-#   hsts_subdomains = false
-#   http2_support = false
-# }
+resource "nginxproxymanager_proxy_host" "nodadyoushutup" {
+  domain_names   = ["nodadyoushutup.com"]
+  forward_host   = "192.168.1.100"
+  forward_port   = 9055
+  forward_scheme = "http"
+  certificate_id = "2"
+  ssl_forced = true
+  hsts_enabled = false
+  hsts_subdomains = false
+  http2_support = false
+}
 
 # Fetch all certificates
 data "nginxproxymanager_certificates" "all" {}
