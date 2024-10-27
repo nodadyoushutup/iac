ab2a4fc5f1efda966c203bd3e5da17471d4205bf
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-27 22:56:10 +0000

    [WIP] NPM

--- terraform/nginx_proxy_manager/provider.tf
+++ terraform/nginx_proxy_manager/provider.tf
@@ -7,8 +7,8 @@ terraform {
 } 
  
 provider "nginxproxymanager" { 
-  host     = local.config.provider.nginx_proxy_manager.host 
-  username = local.config.provider.nginx_proxy_manager.username 
-  password = local.config.provider.nginx_proxy_manager.password 
+  host     = local.config.spacelift.provider.nginx_proxy_manager.host 
+  username = local.config.spacelift.provider.nginx_proxy_manager.username 
+  password = local.config.spacelift.provider.nginx_proxy_manager.password 
 } 
 
