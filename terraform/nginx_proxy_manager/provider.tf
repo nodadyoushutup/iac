53a04d2e08a285c6bbe0fa5647578d4c6c68e28f
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-27 23:06:13 +0000

    [WIP] NPM

--- terraform/nginx_proxy_manager/provider.tf
+++ terraform/nginx_proxy_manager/provider.tf
@@ -12,8 +12,43 @@ provider "nginxproxymanager" {
   password = local.config.spacelift.provider.nginx_proxy_manager.password 
 } 
 
-data "nginxproxymanager_users" "all" {}
+# Manage a proxy host
+resource "nginxproxymanager_proxy_host" "example" {
+  domain_names = ["example.com"]
 
-output "nginxproxymanager_users" {
-  value = data.nginxproxymanager_users.all.users
+  forward_scheme = "https"
+  forward_host   = "example2.com"
+  forward_port   = 443
+
+  caching_enabled         = true
+  allow_websocket_upgrade = true
+  block_exploits          = true
+
+  access_list_id = 0 # Publicly Accessible
+
+  location {
+    path           = "/admin"
+    forward_scheme = "https"
+    forward_host   = "example3.com"
+    forward_port   = 443
+
+    advanced_config = ""
+  }
+
+  location {
+    path           = "/contact"
+    forward_scheme = "http"
+    forward_host   = "example4.com"
+    forward_port   = 80
+
+    advanced_config = ""
+  }
+
+  certificate_id  = 0 # No Certificate
+  ssl_forced      = false
+  hsts_enabled    = false
+  hsts_subdomains = false
+  http2_support   = false
+
+  advanced_config = ""
 }
