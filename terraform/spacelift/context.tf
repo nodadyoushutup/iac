c75a68332b6496ed8058e466717be6b73189dfea
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-27 18:22:22 +0000

    [WIP] Spacelift

--- terraform/spacelift/context.tf
+++ terraform/spacelift/context.tf
@@ -1,7 +1,7 @@
 ### CONTEXT ###
 resource "spacelift_context" "terraform_context" {
     depends_on = [data.spacelift_stack.spacelift]
-    description = "Infrastructure as Code  Configuration"
+    description = "Terraform Configuration"
     name        = "terraform"
 }
 
@@ -13,6 +13,13 @@ resource "spacelift_context" "ansible_context" {
         "chmod 600 ${local.config.spacelift.path.private_key}"
     ]
 }
+
+resource "spacelift_context" "config_context" {
+    depends_on = [data.spacelift_stack.spacelift]
+    description = "IaC Configuration"
+    name        = "config"
+}
+
 
 resource "spacelift_context_attachment" "spacelift_terraform_context_attachment" {
     depends_on = [spacelift_context.terraform_context]
