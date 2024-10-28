cd1b2751bc1dc86bce752ab3710cf24aa8cbe1ff
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-28 01:38:08 +0000

    [WIP] NPM

--- terraform/nginx_proxy_manager/local.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  config_path = try(var.CONFIG != null && var.CONFIG != "" ? var.CONFIG : "/mnt/workspace/config.yaml")
-  config = try(yamldecode(file(local.config_path)), {})
-}
--- terraform/spacelift/nginx_proxy_manager_init.tf
+++ terraform/spacelift/nginx_proxy_manager_init.tf
@@ -12,7 +12,8 @@ resource "spacelift_stack" "nginx_proxy_manager_init_stack" {
     additional_project_globs = [
         "role/docker_compose",
         "role/config_load",
-        "role/nginx-proxy-manager",
+        "role/nginx_proxy_manager_admin",
+        "role/nginx_proxy_manager_host",
         "role/vm_init",
         "role/vm_ping"
     ]
