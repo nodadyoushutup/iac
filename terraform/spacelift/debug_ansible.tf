b32f50da82e743acc2f6cc9078c4a4349b95506f
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-03 22:46:32 +0000

    [WIP] Spacelift

--- terraform/spacelift/debug_ansible.tf
+++ terraform/spacelift/debug_ansible.tf
@@ -1,48 +1,48 @@
-resource "spacelift_stack" "debug_stack" {
+resource "spacelift_stack" "debug_ansible_stack" {
     count = local.env > 0 ? 1 : 0
     depends_on = [spacelift_stack.docker_infra_stack]
     administrative = true
     autodeploy = true
     branch = "main"
     description = "Docker initialization"
-    name = "debug"
-    project_root = "ansible/stack/debug"
+    name = "debug_ansible"
+    project_root = "ansible/stack/debug_ansible"
     repository = "iac"
-    labels = ["ansible", "init", "docker", "administrative", "p1", "p1b"]
+    labels = ["ansible", "debug"]
     ansible {
         playbook = "main.yaml"
     }
 }
 
-resource "spacelift_context_attachment" "debug_config_context_attachment" {
+resource "spacelift_context_attachment" "debug_ansible_config_context_attachment" {
     count = local.env > 0 ? 1 : 0
     depends_on = [
-        spacelift_stack.debug_stack,
+        spacelift_stack.debug_ansible_stack,
         spacelift_context.config_context
     ]
     context_id = spacelift_context.config_context.id
-    stack_id   = spacelift_stack.debug_stack[count.index].id
+    stack_id   = spacelift_stack.debug_ansible_stack[count.index].id
     priority   = 0
 }
 
-resource "spacelift_context_attachment" "debug_terraform_context_attachment" {
+resource "spacelift_context_attachment" "debug_ansible_terraform_context_attachment" {
     count = local.env > 0 ? 1 : 0
     depends_on = [
-        spacelift_stack.debug_stack,
+        spacelift_stack.debug_ansible_stack,
         spacelift_context.terraform_context
     ]
     context_id = spacelift_context.terraform_context.id
-    stack_id   = spacelift_stack.debug_stack[count.index].id
+    stack_id   = spacelift_stack.debug_ansible_stack[count.index].id
     priority   = 0
 }
 
-resource "spacelift_context_attachment" "debug_ansible_context_attachment" {
+resource "spacelift_context_attachment" "debug_ansible_ansible_context_attachment" {
     count = local.env > 0 ? 1 : 0
     depends_on = [
-        spacelift_stack.debug_stack, 
+        spacelift_stack.debug_ansible_stack, 
         spacelift_context.ansible_context,
     ]
     context_id = spacelift_context.ansible_context.id
-    stack_id   = spacelift_stack.debug_stack[count.index].id
+    stack_id   = spacelift_stack.debug_ansible_stack[count.index].id
     priority   = 0
 }
