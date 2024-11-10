70dded0593cb5250c1c5c782658bcbbdf4216c1a
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 23:00:13 +0000

    [WIP] Spacelift

--- terraform/env/main.tf
+++ terraform/env/main.tf
@@ -5,6 +5,7 @@ resource "spacelift_context" "env_context" {
 }
 
 resource "spacelift_environment_variable" "env_branch" { 
+    depends_on = [ spacelift_context.env_context ]
     context_id  = spacelift_context.env_context.id
     name        = "TF_VAR_ENV_BRANCH" 
     value       = var.ENV_BRANCH
@@ -13,9 +14,27 @@ resource "spacelift_environment_variable" "env_branch" {
 }
 
 resource "spacelift_environment_variable" "env_repository" { 
+    depends_on = [ spacelift_context.env_context ]
     context_id  = spacelift_context.env_context.id
     name        = "TF_VAR_ENV_REPOSITORY" 
     value       = var.ENV_REPOSITORY
     write_only  = false 
     description = "Environment repository"
+}
+
+resource "spacelift_stack" "module" {
+    count = var.ENV_BRANCH != null && var.ENV_REPOSITORY != null
+    depends_on = [ 
+        spacelift_environment_variable.env_branch,
+        spacelift_environment_variable.env_repository
+    ]
+    administrative = true
+    autodeploy = true
+    branch = var.ENV_BRANCH
+    description = "Modules"
+    name = "module"
+    project_root = "terraform/module"
+    repository = var.ENV_REPOSITORY
+    terraform_version = "1.5.7"
+    labels = ["module"]
 }
