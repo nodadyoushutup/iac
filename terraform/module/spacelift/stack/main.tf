7b400d9f7434815b426c33e6a1c8fcb176e90285
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 20:53:34 +0000

    [WIP] Spacelift

--- terraform/module/spacelift/stack/main.tf
+++ terraform/module/spacelift/stack/main.tf
@@ -5,43 +5,50 @@ resource "spacelift_stack" "stack" {
     repository     = var.repository
 
     # OPTIONAL
-    additional_project_globs = var.additional_project_globs
-    administrative = var.administrative
-    after_apply = var.after_apply
-    after_destroy = var.after_destroy
-    after_init = var.after_init
-    after_perform = var.after_perform
-    after_plan = var.after_plan
-    after_run = var.after_run
-    autodeploy = var.autodeploy
-    autoretry = var.autoretry
-    before_apply = var.before_apply
-    before_destroy = var.before_destroy
-    before_init = var.before_init
-    before_perform = var.before_perform
-    before_plan = var.before_plan
-    description = var.description
-    enable_local_preview = var.enable_local_preview
-    enable_well_known_secret_masking = var.enable_well_known_secret_masking
-    github_action_deploy = var.github_action_deploy
-    import_state = var.import_state
-    import_state_file = var.import_state_file
-    labels = var.labels
-    manage_state = var.manage_state
-    project_root = var.project_root
-    protect_from_deletion = var.protect_from_deletion
-    runner_image = var.runner_image
-    slug = var.slug
-    space_id = var.space_id
-    terraform_external_state_access = var.terraform_external_state_access
-    terraform_smart_sanitization = var.terraform_smart_sanitization
-    terraform_version = var.terraform_version
-    terraform_workflow_tool = var.terraform_workflow_tool
-    terraform_workspace = var.terraform_workspace
-    worker_pool_id = var.worker_pool_id
+    additional_project_globs           = var.additional_project_globs
+    administrative                     = var.administrative
+    after_apply                        = var.after_apply
+    after_destroy                      = var.after_destroy
+    after_init                         = var.after_init
+    after_perform                      = var.after_perform
+    after_plan                         = var.after_plan
+    after_run                          = var.after_run
+    autodeploy                         = var.autodeploy
+    autoretry                          = var.autoretry
+    before_apply                       = var.before_apply
+    before_destroy                     = var.before_destroy
+    before_init                        = var.before_init
+    before_perform                     = var.before_perform
+    before_plan                        = var.before_plan
+    description                        = var.description
+    enable_local_preview               = var.enable_local_preview
+    enable_well_known_secret_masking   = var.enable_well_known_secret_masking
+    github_action_deploy               = var.github_action_deploy
+    import_state                       = var.import_state
+    import_state_file                  = var.import_state_file
+    labels                             = var.labels
+    manage_state                       = var.manage_state
+    project_root                       = var.project_root
+    protect_from_deletion              = var.protect_from_deletion
+    runner_image                       = var.runner_image
+    slug                               = var.slug
+    space_id                           = var.space_id
+    terraform_external_state_access    = var.terraform_external_state_access != null && var.ansible == null ? var.terraform_external_state_access : null
+    terraform_smart_sanitization       = var.terraform_smart_sanitization != null && var.ansible == null ? var.terraform_smart_sanitization : null
+    terraform_version                  = var.terraform_version != null && var.ansible == null ? var.terraform_version : null
+    terraform_workflow_tool            = var.terraform_workflow_tool != null && var.ansible == null ? var.terraform_workflow_tool : null
+    terraform_workspace                = var.terraform_workspace != null && var.ansible == null ? var.terraform_workspace : null
+    worker_pool_id                     = var.worker_pool_id
 
     dynamic "ansible" {
-        for_each = var.ansible != null && var.terraform_version == null ? [var.ansible] : []
+        for_each = (
+                var.ansible != null 
+                && var.terraform_external_state_access == null 
+                && var.terraform_smart_sanitization == null 
+                && var.terraform_version == null 
+                && var.terraform_workflow_tool == null 
+                && var.terraform_workspace == null
+            ) ? [var.ansible] : []
         content {
             playbook = ansible.value.playbook
         }
@@ -51,7 +58,7 @@ resource "spacelift_stack" "stack" {
         for_each = var.raw_git != null ? [var.raw_git] : []
         content {
             namespace = raw_git.value.namespace
-            url = raw_git.value.url
+            url       = raw_git.value.url
         }
     }
 }
