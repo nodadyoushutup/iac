700008c8956a220455edf706342fa4c2dd68f499
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 18:08:08 +0000

    [WIP] Spacelift

--- terraform/module/spacelift/stack/main.tf
+++ terraform/module/spacelift/stack/main.tf
@@ -1,15 +1,21 @@
 resource "spacelift_stack" "stack" {
     # REQUIRED
-    branch = try(var.branch, local.config.spacelift.stack.branch)
-    name = "stack"
-    repository = "iac"
+    name = var.name
+    project_root = var.project_root
 
-    # OPTIONAL
-    administrative = true
-    autodeploy = true
-    description = "Spacelift stack"
-    project_root = "terraform"
-    space_id = "root"
-    terraform_version = "1.5.7"
-    labels = []
+    # UNIQUE
+    description = var.description
+    labels = var.labels
+    
+    # GENERAL
+    administrative = try(var.administrative, local.config.spacelift.stack.administrative, true)
+    autodeploy = try(var.autodeploy, local.config.spacelift.stack.autodeploy, true)
+    branch = try(var.branch, local.config.spacelift.stack.branch, "main")
+    repository = try(var.branch, local.config.spacelift.stack.repository, "iac")
+    space_id = try(var.space_id, local.config.spacelift.stack.space_id, "root")
+
+    # TERRAFORM
+    terraform_version = try(var.branch, local.config.spacelift.stack.repository, "1.5.7")
+
+    
 }
--- terraform/module/spacelift/stack/variable.tf
+++ terraform/module/spacelift/stack/variable.tf
@@ -4,8 +4,63 @@ variable "CONFIG" {
   default = null
 }
 
+# REQUIRED
+variable "name" {
+  type = string
+  description = "Name"
+}
+
+variable "project_root" {
+  type = string
+  description = "Project root"
+}
+
+# UNIQUE
+variable "description" {
+  type = string
+  description = "Description"
+  default = null
+}
+
+variable "labels" {
+  type = string
+  description = "Labels"
+  default = []
+}
+
+# GENERAL
+variable "administrative" {
+  type = bool
+  description = "Administrative"
+  default = null
+}
+
+variable "autodeploy" {
+  type = bool
+  description = "Autodeploy"
+  default = null
+}
+
 variable "branch" {
   type = string
   description = "Branch"
+  default = null
+}
+
+variable "repository" {
+  type = string
+  description = "Repository"
+  default = null
+}
+
+variable "space_id" {
+  type = string
+  description = "Space ID"
+  default = null
+}
+
+variable "terraform_version" {
+  type = string
+  description = "Terraform version"
   default = null
 }
