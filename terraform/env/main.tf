0f0b423829f0afc9388ffa359de886edd36af177
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 22:55:28 +0000

    [WIP] Spacelift

--- terraform/env/main.tf
+++ terraform/env/main.tf
@@ -3,3 +3,19 @@ resource "spacelift_context" "env_context" {
     name = "env"
     space_id = "root"
 }
+
+resource "spacelift_environment_variable" "env_branch" { 
+    context_id  = spacelift_context.env_context.id
+    name        = "TF_VAR_ENV_BRANCH" 
+    value       = var.ENV_BRANCH
+    write_only  = false 
+    description = "Environment branch"
+}
+
+resource "spacelift_environment_variable" "env_repository" { 
+    context_id  = spacelift_context.env_context.id
+    name        = "TF_VAR_ENV_REPOSITORY" 
+    value       = var.ENV_REPOSITORY
+    write_only  = false 
+    description = "Environment repository"
+}
---
+++ terraform/env/variable.tf
@@ -0,0 +1,11 @@
+variable "ENV_BRANCH" {
+  type = string
+  description = "Environment branch"
+  default = null
+}
+
+variable "ENV_REPOSITORY" {
+  type = string
+  description = "Environment repository"
+  default = null
+}
