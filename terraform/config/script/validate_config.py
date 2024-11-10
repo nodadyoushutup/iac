a1a19f29f3558bc2d5d99c36e1bed7339a1b7715
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 23:55:39 +0000

    [WIP] Spacelift

--- terraform/config/script/validate_config.py
+++ terraform/config/script/validate_config.py
@@ -1,1 +1,11 @@
-print({"valid": "true"})
+import json
+
+def main():
+    # Example result as JSON string
+    result = {"valid": "true"}
+
+    # Print the result as a JSON-encoded string
+    print(json.dumps(result))
+
+if __name__ == "__main__":
+    main()
