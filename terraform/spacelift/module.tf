2666509d80bd24ef98d03206c2fa63e1034a2154
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 22:01:48 +0000

    [WIP] Spacelift

--- terraform/spacelift/module.tf
+++ terraform/spacelift/module.tf
@@ -1,15 +1,13 @@
-# # module
-# module "test_stack" {
-#     source  = "spacelift.io/nodadyoushutup/stack/spacelift"
-#     name = "test"
-#     branch = "main"
-#     repository = "iac"
-#     project_root = "terraform/docker"
-#     raw_git = {
-#         namespace = "nodadyoushutup"
-#         url = "https://github.com/nodadyoushutup/iac"
-#     }
-#     ansible = {
-#         playbook = "main.yaml"
-#     }
-# }
+# module
+module "test_stack" {
+    source  = "spacelift.io/nodadyoushutup/stack/spacelift"
+    name = "test"
+    branch = "main"
+    repository = "iac"
+    project_root = "terraform/docker"
+    terraform_version = "1.5.7"
+    raw_git = {
+        namespace = "nodadyoushutup"
+        url = "https://github.com/nodadyoushutup/iac"
+    }
+}
