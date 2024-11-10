5d4da1cd1b88772f3435897d784639623d24bdf2
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 22:04:08 +0000

    [WIP] Spacelift

--- terraform/spacelift/module.tf
+++ terraform/spacelift/module.tf
@@ -1,13 +1,9 @@
 # module
 module "test_stack" {
-    source  = "spacelift.io/nodadyoushutup/stack/spacelift"
+    source  = "spacelift.io/${spacelift_account_name}/stack/spacelift"
     name = "test"
     branch = "main"
     repository = "iac"
     project_root = "terraform/docker"
     terraform_version = "1.5.7"
-    raw_git = {
-        namespace = "nodadyoushutup"
-        url = "https://github.com/nodadyoushutup/iac"
-    }
 }
