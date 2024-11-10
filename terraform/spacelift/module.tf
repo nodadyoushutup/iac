5f59443a8997d283566a753c1a134b88f18dd945
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 22:18:38 +0000

    [WIP] Spacelift

--- terraform/spacelift/module.tf
+++ terraform/spacelift/module.tf
@@ -1,6 +1,6 @@
 # module
 module "test_stack" {
-    source  = "spacelift.io/${var.spacelift_account_name}/stack/spacelift"
+    source  = "spacelift.io/nodadyoushutup/stack/spacelift"
     count = local.env > 0 ? 1 : 0
     
     branch = coalesce(try(local.config.spacelift.stack.branch, null), "main")
