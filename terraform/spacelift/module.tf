ac61c3fae17da6ae9d614c0a09f621272e3cf104
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 17:06:24 +0000

    [WIP] Spacelift

--- terraform/spacelift/module.tf
+++ terraform/spacelift/module.tf
@@ -1,5 +1,3 @@
-# module "infra" {
-#     depends_on = [ spacelift_module.spacelift_stack_terraform ]
-#     source  = "spacelift.io/nodadyoushutup/stack-terraform/spacelift"
-  
-# }
+module "infra" {
+    source  = "spacelift.io/nodadyoushutup/stack/spacelift"
+}
