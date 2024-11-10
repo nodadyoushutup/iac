ac3f7b50d2f9cc9ba5a42352f51c1e27482ffbfe
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 23:52:13 +0000

    [WIP] Spacelift

--- terraform/config/data.tf
+++ terraform/config/data.tf
@@ -1,3 +1,21 @@
 data "spacelift_stack" "config" {
   stack_id = "config"
 }
+
+resource "random_id" "validate_env_trigger" {
+    byte_length = 8
+}
+
+data "external" "validate_env" {
+    program = [
+        "python3",
+        "${path.module}/script/validate_config.py",
+        local.config.spacelift.private_key
+    ]
+    query = { trigger = random_id.validate_env_trigger.hex }
+    depends_on = [random_id.validate_env_trigger]
+}
+
+output "validate_env" {
+  value = data.external.validate_env
+}
