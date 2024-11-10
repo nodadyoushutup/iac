9a7bf553953aea85e4d865abee1bea0126bb5639
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 20:28:57 +0000

    [WIP] Spacelift

--- terraform/spacelift/module.tf
+++ terraform/spacelift/module.tf
@@ -1,7 +1,10 @@
-# # module
-# module "test_stack" {
-#     source  = "spacelift.io/nodadyoushutup/stack/spacelift"
-#     name = "test"
-#     branch = "main"
-#     repository = "iac"
-# }
+# module
+module "test_stack" {
+    source  = "spacelift.io/nodadyoushutup/stack/spacelift"
+    name = "test"
+    branch = "main"
+    repository = "iac"
+    github_enterprise = {
+        namespace = "nodadyoushutup"
+    }
+}
