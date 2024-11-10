8953004514d3bf868d4b1a0c862a757c4502283d
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 20:59:23 +0000

    [WIP] Spacelift

--- terraform/module/spacelift/stack/main.tf
+++ terraform/module/spacelift/stack/main.tf
@@ -33,22 +33,15 @@ resource "spacelift_stack" "stack" {
     runner_image                       = var.runner_image
     slug                               = var.slug
     space_id                           = var.space_id
-    terraform_external_state_access    = var.terraform_external_state_access != null && var.ansible == null ? var.terraform_external_state_access : null
-    terraform_smart_sanitization       = var.terraform_smart_sanitization != null && var.ansible == null ? var.terraform_smart_sanitization : null
+    terraform_external_state_access    = var.terraform_external_state_access
+    terraform_smart_sanitization       = var.terraform_smart_sanitization
     terraform_version                  = var.terraform_version != null && var.ansible == null ? var.terraform_version : null
-    terraform_workflow_tool            = var.terraform_workflow_tool != null && var.ansible == null ? var.terraform_workflow_tool : null
-    terraform_workspace                = var.terraform_workspace != null && var.ansible == null ? var.terraform_workspace : null
+    terraform_workflow_tool            = var.terraform_workflow_tool
+    terraform_workspace                = var.terraform_workspace
     worker_pool_id                     = var.worker_pool_id
 
     dynamic "ansible" {
-        for_each = (
-                var.ansible != null 
-                && var.terraform_external_state_access == null 
-                && var.terraform_smart_sanitization == null 
-                && var.terraform_version == null 
-                && var.terraform_workflow_tool == null 
-                && var.terraform_workspace == null
-            ) ? [var.ansible] : []
+        for_each = (var.ansible != null && var.terraform_version == null) ? [var.ansible] : []
         content {
             playbook = ansible.value.playbook
         }
