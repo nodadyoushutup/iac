d3debf5a622b90633f9825639e25eade20d1ffa0
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 20:46:29 +0000

    [WIP] Spacelift

--- terraform/module/spacelift/stack/main.tf
+++ terraform/module/spacelift/stack/main.tf
@@ -41,7 +41,7 @@ resource "spacelift_stack" "stack" {
     worker_pool_id = var.worker_pool_id
 
     dynamic "ansible" {
-        for_each = var.ansible != null ? [var.ansible] : []
+        for_each = var.ansible != null && var.terraform_version == null ? [var.ansible] : []
         content {
             playbook = ansible.value.playbook
         }
