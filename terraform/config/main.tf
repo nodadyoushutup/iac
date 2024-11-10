5e1daef8fb18c5904348e16b1022a8487e51a5ba
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 23:33:32 +0000

    [WIP] Spacelift

--- terraform/config/main.tf
+++ terraform/config/main.tf
@@ -1,87 +1,87 @@
-# main
-resource "spacelift_context" "config" {
-    description = "Configuration"
-    name = "config"
-    space_id = "root"
-}
+# # main
+# resource "spacelift_context" "config" {
+#     description = "Configuration"
+#     name = "config"
+#     space_id = "root"
+# }
 
-resource "spacelift_context_attachment" "config_config" {
-    depends_on = [spacelift_context.config]
-    context_id = spacelift_context.config.id
-    stack_id   = data.spacelift_stack.config.id
-    priority   = 0
-}
-
-resource "spacelift_environment_variable" "config" { 
-    depends_on = [spacelift_context_attachment.config_config]
-    context_id  = spacelift_context.config.id
-    name        = "TF_VAR_CONFIG" 
-    value       = var.CONFIG_PATH
-    write_only  = false 
-    description = "IaC configuration path"
-}
-
-resource "spacelift_environment_variable" "env_branch" { 
-    depends_on = [spacelift_context_attachment.config_config]
-    context_id  = spacelift_context.config.id
-    name        = "TF_VAR_ENV_BRANCH" 
-    value       = var.ENV_BRANCH
-    write_only  = false 
-    description = "Environment branch"
-}
-
-resource "spacelift_environment_variable" "env_repository" { 
-    depends_on = [spacelift_context_attachment.config_config]
-    context_id  = spacelift_context.config.id
-    name        = "TF_VAR_ENV_REPOSITORY" 
-    value       = var.ENV_REPOSITORY
-    write_only  = false 
-    description = "Environment repository"
-}
-
-resource "spacelift_environment_variable" "private_key" { 
-    depends_on = [spacelift_context_attachment.config_config]
-    context_id  = spacelift_context.config.id
-    name        = "TF_VAR_PRIVATE_KEY" 
-    value       = var.PRIVATE_KEY
-    write_only  = false 
-    description = "Private key"
-}
-
-
-
-
-
-# resource "spacelift_mounted_file" "config_mounted_file" {
-#     depends_on = [spacelift_context_attachment.config_config]
+# resource "spacelift_context_attachment" "config_config" {
+#     depends_on = [spacelift_context.config]
 #     context_id = spacelift_context.config.id
-#     relative_path = "config.yaml"
-#     content = local.base64.config
-#     write_only = false
+#     stack_id   = data.spacelift_stack.config.id
+#     priority   = 0
 # }
 
-# resource "spacelift_mounted_file" "private_keymounted_file" {
+# resource "spacelift_environment_variable" "config" { 
 #     depends_on = [spacelift_context_attachment.config_config]
-#     context_id = spacelift_context.config.id
-#     relative_path = "id_rsa"
-#     content = local.base64.private_key
-#     write_only = true
+#     context_id  = spacelift_context.config.id
+#     name        = "TF_VAR_CONFIG" 
+#     value       = var.CONFIG_PATH
+#     write_only  = false 
+#     description = "IaC configuration path"
 # }
 
-# resource "spacelift_stack" "module" {
-#     count = var.ENV_BRANCH != null && var.ENV_REPOSITORY != null ? 1 : 0
-#     depends_on = [
-#         spacelift_environment_variable.env_branch,
-#         spacelift_environment_variable.env_repository
-#    ]
-#     administrative = true
-#     autodeploy = true
-#     branch = var.ENV_BRANCH
-#     description = "Modules"
-#     name = "module"
-#     project_root = "terraform/module"
-#     repository = var.ENV_REPOSITORY
-#     terraform_version = "1.5.7"
-#     labels = ["module"]
+# resource "spacelift_environment_variable" "env_branch" { 
+#     depends_on = [spacelift_context_attachment.config_config]
+#     context_id  = spacelift_context.config.id
+#     name        = "TF_VAR_ENV_BRANCH" 
+#     value       = var.ENV_BRANCH
+#     write_only  = false 
+#     description = "Environment branch"
 # }
+
+# resource "spacelift_environment_variable" "env_repository" { 
+#     depends_on = [spacelift_context_attachment.config_config]
+#     context_id  = spacelift_context.config.id
+#     name        = "TF_VAR_ENV_REPOSITORY" 
+#     value       = var.ENV_REPOSITORY
+#     write_only  = false 
+#     description = "Environment repository"
+# }
+
+# resource "spacelift_environment_variable" "private_key" { 
+#     depends_on = [spacelift_context_attachment.config_config]
+#     context_id  = spacelift_context.config.id
+#     name        = "TF_VAR_PRIVATE_KEY" 
+#     value       = var.PRIVATE_KEY
+#     write_only  = false 
+#     description = "Private key"
+# }
+
+
+
+
+
+# # resource "spacelift_mounted_file" "config_mounted_file" {
+# #     depends_on = [spacelift_context_attachment.config_config]
+# #     context_id = spacelift_context.config.id
+# #     relative_path = "config.yaml"
+# #     content = local.base64.config
+# #     write_only = false
+# # }
+
+# # resource "spacelift_mounted_file" "private_keymounted_file" {
+# #     depends_on = [spacelift_context_attachment.config_config]
+# #     context_id = spacelift_context.config.id
+# #     relative_path = "id_rsa"
+# #     content = local.base64.private_key
+# #     write_only = true
+# # }
+
+# # resource "spacelift_stack" "module" {
+# #     count = var.ENV_BRANCH != null && var.ENV_REPOSITORY != null ? 1 : 0
+# #     depends_on = [
+# #         spacelift_environment_variable.env_branch,
+# #         spacelift_environment_variable.env_repository
+# #    ]
+# #     administrative = true
+# #     autodeploy = true
+# #     branch = var.ENV_BRANCH
+# #     description = "Modules"
+# #     name = "module"
+# #     project_root = "terraform/module"
+# #     repository = var.ENV_REPOSITORY
+# #     terraform_version = "1.5.7"
+# #     labels = ["module"]
+# # }
 
--- terraform/config/variable.tf
+++ terraform/config/variable.tf
@@ -1,23 +1,23 @@
-variable "CONFIG_PATH" {
-    type = string
-    description = "Configuration path"
-    default = "/mnt/workspace/config.yaml"
-}
+# variable "CONFIG_PATH" {
+#     type = string
+#     description = "Configuration path"
+#     default = "/mnt/workspace/config.yaml"
+# }
 
-variable "ENV_BRANCH" {
-    type = string
-    description = "Environment branch"
-    default = null
-}
+# variable "ENV_BRANCH" {
+#     type = string
+#     description = "Environment branch"
+#     default = null
+# }
 
-variable "ENV_REPOSITORY" {
-    type = string
-    description = "Environment repository"
-    default = null
-}
+# variable "ENV_REPOSITORY" {
+#     type = string
+#     description = "Environment repository"
+#     default = null
+# }
 
-variable "PRIVATE_KEY" {
-  type = string
-  description = "Private key"
-  default = "/mnt/workspace/ird_rsa"
-}
+# variable "PRIVATE_KEY" {
+#   type = string
+#   description = "Private key"
+#   default = "/mnt/workspace/ird_rsa"
+# }
