ba7320fe5b69b02ad29a00b84f9c06598bd61906
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-10 18:36:02 +0000

    [WIP] Spacelift

--- terraform/module/spacelift/stack/main.tf
+++ terraform/module/spacelift/stack/main.tf
@@ -1,19 +1,24 @@
-resource "spacelift_stack" "stack" {
-    # REQUIRED
-    name = var.name
-    project_root = var.project_root
+# resource "spacelift_stack" "stack" {
+#     # REQUIRED
+#     name = var.name
+#     project_root = var.project_root
 
-    # UNIQUE
-    description = var.description
-    labels = var.labels
+#     # UNIQUE
+#     description = var.description
+#     labels = var.labels
 
-    # GENERAL
-    administrative = try(var.administrative, local.config.spacelift.stack.administrative, true)
-    autodeploy = try(var.autodeploy, local.config.spacelift.stack.autodeploy, true)
-    branch = try(var.branch, local.config.spacelift.stack.branch, "main")
-    repository = try(var.repository, local.config.spacelift.stack.repository, "iac")
-    space_id = try(var.space_id, local.config.spacelift.stack.space_id, "root")
+#     # GENERAL
+#     administrative = try(var.administrative, local.config.spacelift.stack.administrative, true)
+#     autodeploy = try(var.autodeploy, local.config.spacelift.stack.autodeploy, true)
+#     branch = try(var.branch, local.config.spacelift.stack.branch, "main")
+#     repository = try(var.repository, local.config.spacelift.stack.repository, "iac")
+#     space_id = try(var.space_id, local.config.spacelift.stack.space_id, "root")
 
-    # TERRAFORM
-    terraform_version = try(var.terraform_version, local.config.spacelift.stack.terraform_version, "1.5.7")
+#     # TERRAFORM
+#     terraform_version = try(var.terraform_version, local.config.spacelift.stack.terraform_version, "1.5.7")
+# }
+
+
+output "test" {
+  value = try(var.administrative, local.config.spacelift.stack.administrative, true)
 }
