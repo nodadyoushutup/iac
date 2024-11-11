f7c7b08b0e0084e1297ee694873929ce8f10f102
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-11 06:59:40 +0000

    [WIP] Spacelift

--- terraform/config/output.tf
+++ terraform/config/output.tf
@@ -1,15 +1,15 @@
 output "_validate_results" {
-    value = data.external.validate_env.result["valid"] != "false" ? "All validation checks have passed" : "Some validation checks failed"
+    value = data.external.validate_env[count.index].result["valid"] != "false" ? "All validation checks have passed" : "Some validation checks failed"
 }
 
 output "validate_config_path" {
-    value = data.external.validate_env.result["config_path"]
+    value = data.external.validate_env[count.index].result["config_path"]
 }
 
 output "validate_yaml" {
-    value = data.external.validate_env.result["yaml"]
+    value = data.external.validate_env[count.index].result["yaml"]
 }
 
 output "validate_private_key" {
-    value = data.external.validate_env.result["private_key"]
+    value = data.external.validate_env[count.index].result["private_key"]
 }
