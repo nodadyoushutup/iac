3c49335455116c404ceae5d72c196335a17984ea
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-11 19:19:06 +0000

    [WIP] Spacelift

--- terraform/config/mounted_file.tf
+++ terraform/config/mounted_file.tf
@@ -1,9 +1,5 @@
 resource "spacelift_mounted_file" "config_mounted_file" {
-    count = var.PYVENV == 2 ? 1 : 0
-    depends_on = [
-        spacelift_context.config,
-        spacelift_environment_variable.pyvenv
-    ]
+    depends_on = [spacelift_context.config]
     context_id = spacelift_context.config.id
     relative_path = "config.yaml"
     content = local.base64.config
@@ -11,11 +7,7 @@ resource "spacelift_mounted_file" "config_mounted_file" {
 }
 
 resource "spacelift_mounted_file" "private_keymounted_file" {
-    count = var.PYVENV == 2 ? 1 : 0
-    depends_on = [
-        spacelift_context.config,
-        spacelift_environment_variable.pyvenv
-    ]
+    depends_on = [spacelift_context.config]
     context_id = spacelift_context.config.id
     relative_path = "id_rsa"
     content = local.base64.private_key
