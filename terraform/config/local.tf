2cad48a4f729238bb9b3e5df5253e9ab6d9221b4
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-11 21:28:07 +0000

    [WIP] Spacelift

--- terraform/config/local.tf
+++ terraform/config/local.tf
@@ -1,11 +1,20 @@
 locals {
     base64 = {
-        config = try(filebase64(var.PATH_CONFIG), filebase64("/mnt/workspace/source/config/default/config.yaml"))
-        private_key = try(filebase64(var.PATH_PRIVATE_KEY), filebase64("/mnt/workspace/source/config/default/id_rsa"))
+        config = try(
+            filebase64(var.PATH_CONFIG), 
+            filebase64("/mnt/workspace/source/config/default/config.yaml")
+        )
+        private_key = try(
+            filebase64(var.PATH_PRIVATE_KEY), 
+            filebase64("/mnt/workspace/source/config/default/id_rsa")
+        )
     }
     flag = {
         pyvenv = var.FLAG_PYVENV == 2 ? var.FLAG_PYVENV : var.FLAG_PYVENV + 1
-        config = try(var.FLAG_PYVENV == 2 && data.external.validate_env[count.index].result["valid"] == "true", null) ? var.FLAG_CONFIG + 1 : var.FLAG_CONFIG
+        config = try(
+            var.FLAG_PYVENV == 2 
+            && data.external.validate_env[0].result["valid"] == "true", null
+        ) ? var.FLAG_CONFIG + 1 : var.FLAG_CONFIG
     }
     git = {
         branch = coalesce(var.GIT_BRANCH, data.spacelift_stack.config.branch)
