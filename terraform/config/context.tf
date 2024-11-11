814d8a933dd9c3364fa6f8c0b2b8bae0252fd8c5
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-11 19:06:52 +0000

    [WIP] Spacelift

--- terraform/config/context.tf
+++ terraform/config/context.tf
@@ -1,6 +1,6 @@
 resource "spacelift_context" "pyvenv" {
-    description = "Runner Python virtual environment hooks"
     name = "pyvenv"
+    description = "Runner Python virtual environment hooks"
     space_id = data.spacelift_space.root.id
     before_init = [
         "python3 -m venv venv",
@@ -10,16 +10,14 @@ resource "spacelift_context" "pyvenv" {
 }
 
 resource "spacelift_context" "env_flag" {
-    description = "Environment flags"
     name = "env_flag"
+    description = "Environment flags"
     space_id = data.spacelift_space.root.id
 }
 
 
-# resource "spacelift_context" "config" {
-#     count = var.PYVENV == 2 ? 1 : 0
-#     depends_on = [ spacelift_context_attachment.env_flag_config ]
-#     description = "Configuration"
-#     name = "config"
-#     space_id = data.spacelift_space.root.id
-# }
+resource "spacelift_context" "config" {
+    name = "config"
+    description = "Configuration"
+    space_id = data.spacelift_space.root.id
+}
--- terraform/config/context_attachment.tf
+++ terraform/config/context_attachment.tf
@@ -1,7 +1,20 @@
-# resource "spacelift_context_attachment" "config_config" {
-#     count = var.ENV_FLAG_PYVENV == 2 ? 1 : 0
-#     depends_on = [spacelift_context.config]
-#     context_id = spacelift_context.config[count.index].id
-#     stack_id   = data.spacelift_stack.config.id
-#     priority   = 0
-# }
+resource "spacelift_context_attachment" "pyvenv_config" {
+    depends_on = [spacelift_context.config]
+    context_id = spacelift_context.pyvenv.id
+    stack_id   = data.spacelift_stack.config.id
+    priority   = 0
+}
+
+resource "spacelift_context_attachment" "config_config" {
+    depends_on = [spacelift_context.config]
+    context_id = spacelift_context.config.id
+    stack_id   = data.spacelift_stack.config.id
+    priority   = 0
+}
+
+resource "spacelift_context_attachment" "env_flag_config" {
+    depends_on = [spacelift_context.config]
+    context_id = spacelift_context.config.id
+    stack_id   = data.spacelift_stack.config.id
+    priority   = 0
+}
