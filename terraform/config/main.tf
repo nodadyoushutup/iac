d3facc886f813dde5d2ecec984ff76d1613a1757
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-11 06:41:16 +0000

    [WIP] Spacelift

--- terraform/config/main.tf
+++ terraform/config/main.tf
@@ -31,79 +31,86 @@ resource "spacelift_environment_variable" "pyvenv" {
 
 
 
-# # CONTEXT
-# resource "spacelift_context" "config" {
-#     count = var.PYVENV > 0 ? 1 : 0
-#     depends_on = [ spacelift_context_attachment.pyvenv_config ]
-#     description = "Configuration"
-#     name = "config"
-#     space_id = data.spacelift_space.root.id
-# }
+# CONTEXT
+resource "spacelift_context" "config" {
+    count = var.PYVENV == 2 ? 1 : 0
+    depends_on = [ spacelift_context_attachment.pyvenv_config ]
+    description = "Configuration"
+    name = "config"
+    space_id = data.spacelift_space.root.id
+}
 
 
-# # CONTEXT ATTACHMENT
-# resource "spacelift_context_attachment" "config_config" {
-#     depends_on = [spacelift_context.config]
-#     context_id = spacelift_context.config.id
-#     stack_id   = data.spacelift_stack.config.id
-#     priority   = 0
-# }
+# CONTEXT ATTACHMENT
+resource "spacelift_context_attachment" "config_config" {
+    count = var.PYVENV == 2 ? 1 : 0
+    depends_on = [spacelift_context.config]
+    context_id = spacelift_context.config.id
+    stack_id   = data.spacelift_stack.config.id
+    priority   = 0
+}
 
 
 
-# # ENVIRONMENT VARIABLE
-# resource "spacelift_environment_variable" "branch" { 
-#     depends_on = [spacelift_context_attachment.config_config]
-#     context_id  = spacelift_context.config.id
-#     name        = "TF_VAR_BRANCH" 
-#     value       = var.BRANCH
-#     write_only  = false 
-#     description = "Environment branch"
-# }
+# ENVIRONMENT VARIABLE
+resource "spacelift_environment_variable" "branch" { 
+    count = var.PYVENV == 2 ? 1 : 0
+    depends_on = [spacelift_context_attachment.config_config]
+    context_id  = spacelift_context.config.id
+    name        = "TF_VAR_BRANCH" 
+    value       = var.BRANCH
+    write_only  = false 
+    description = "Environment branch"
+}
 
-# resource "spacelift_environment_variable" "repository" { 
-#     depends_on = [spacelift_context_attachment.config_config]
-#     context_id  = spacelift_context.config.id
-#     name        = "TF_VAR_REPOSITORY" 
-#     value       = var.REPOSITORY
-#     write_only  = false 
-#     description = "Environment repository"
-# }
+resource "spacelift_environment_variable" "repository" { 
+    count = var.PYVENV == 2 ? 1 : 0
+    depends_on = [spacelift_context_attachment.config_config]
+    context_id  = spacelift_context.config.id
+    name        = "TF_VAR_REPOSITORY" 
+    value       = var.REPOSITORY
+    write_only  = false 
+    description = "Environment repository"
+}
 
-# resource "spacelift_environment_variable" "private_key" { 
-#     depends_on = [spacelift_context_attachment.config_config]
-#     context_id  = spacelift_context.config.id
-#     name        = "TF_VAR_PRIVATE_KEY" 
-#     value       = var.PRIVATE_KEY
-#     write_only  = false 
-#     description = "Private key"
-# }
+resource "spacelift_environment_variable" "private_key" { 
+    count = var.PYVENV == 2 ? 1 : 0
+    depends_on = [spacelift_context_attachment.config_config]
+    context_id  = spacelift_context.config.id
+    name        = "TF_VAR_PRIVATE_KEY" 
+    value       = var.PRIVATE_KEY
+    write_only  = false 
+    description = "Private key"
+}
 
-# resource "spacelift_environment_variable" "config_path" { 
-#     depends_on = [spacelift_context_attachment.config_config]
-#     context_id  = spacelift_context.config.id
-#     name        = "TF_VAR_CONFIG_PATH" 
-#     value       = var.CONFIG_PATH
-#     write_only  = false 
-#     description = "IaC configuration path"
-# }
+resource "spacelift_environment_variable" "config_path" { 
+    count = var.PYVENV == 2 ? 1 : 0
+    depends_on = [spacelift_context_attachment.config_config]
+    context_id  = spacelift_context.config.id
+    name        = "TF_VAR_CONFIG_PATH" 
+    value       = var.CONFIG_PATH
+    write_only  = false 
+    description = "IaC configuration path"
+}
 
-# # MOUNTED FILE
-# resource "spacelift_mounted_file" "config_mounted_file" {
-#     depends_on = [spacelift_context_attachment.config_config]
-#     context_id = spacelift_context.config.id
-#     relative_path = "config.yaml"
-#     content = local.base64.config
-#     write_only = false
-# }
+# MOUNTED FILE
+resource "spacelift_mounted_file" "config_mounted_file" {
+    count = var.PYVENV == 2 ? 1 : 0
+    depends_on = [spacelift_context_attachment.config_config]
+    context_id = spacelift_context.config.id
+    relative_path = "config.yaml"
+    content = local.base64.config
+    write_only = false
+}
 
-# resource "spacelift_mounted_file" "private_keymounted_file" {
-#     depends_on = [spacelift_context_attachment.config_config]
-#     context_id = spacelift_context.config.id
-#     relative_path = "id_rsa"
-#     content = local.base64.private_key
-#     write_only = true
-# }
+resource "spacelift_mounted_file" "private_keymounted_file" {
+    count = var.PYVENV == 2 ? 1 : 0
+    depends_on = [spacelift_context_attachment.config_config]
+    context_id = spacelift_context.config.id
+    relative_path = "id_rsa"
+    content = local.base64.private_key
+    write_only = true
+}
 
 # # resource "spacelift_stack" "module" {
 # #     count = var.BRANCH != null && var.REPOSITORY != null ? 1 : 0
