2d04cabb48da9d85a0a9d26a49c223bdc366ee9c
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-11 21:48:28 +0000

    [WIP] Spacelift

--- terraform/config/output.tf
+++ terraform/config/output.tf
@@ -1,15 +1,15 @@
-# output "_validate_results" {
-#   value = (var.PYVENV == 2 ? (data.external.validate_env[0].result["valid"] != "false" ? "All validation checks have passed" : "Some validation checks failed") : null)
-# }
+output "_validate_results" {
+  value = var.FLAG_PYVENV == 2 ? (data.external.validate_env[0].result["valid"] != "false" ? "All validation checks have passed" : "Some validation checks failed") : null
+}
 
-# output "validate_config_path" {
-#   value = var.PYVENV == 2 ? data.external.validate_env[0].result["config_path"] : null
-# }
+output "validate_config_path" {
+  value = var.FLAG_PYVENV == 2 ? data.external.validate_env[0].result["config_path"] : null
+}
 
-# output "validate_yaml" {
-#   value = var.PYVENV == 2 ? data.external.validate_env[0].result["yaml"] : null
-# }
+output "validate_yaml" {
+  value = var.FLAG_PYVENV == 2 ? data.external.validate_env[0].result["yaml"] : null
+}
 
-# output "validate_private_key" {
-#   value = var.PYVENV == 2 ? data.external.validate_env[0].result["private_key"] : null
-# }
+output "validate_private_key" {
+  value = var.FLAG_PYVENV == 2 ? data.external.validate_env[0].result["private_key"] : null
+}
