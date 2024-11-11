22388a89e9dd377591a07509623017e32182f32f
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-11 07:08:16 +0000

    [WIP] Spacelift

--- terraform/config/output.tf
+++ terraform/config/output.tf
@@ -1,15 +1,23 @@
 output "_validate_results" {
-    value = data.external.validate_env[count.index].result["valid"] != "false" ? "All validation checks have passed" : "Some validation checks failed"
+  value = [for i in range(length(data.external.validate_env)) :
+    data.external.validate_env[i].result["valid"] != "false" ? "All validation checks have passed" : "Some validation checks failed"
+  ]
 }
 
 output "validate_config_path" {
-    value = data.external.validate_env[count.index].result["config_path"]
+  value = [for i in range(length(data.external.validate_env)) :
+    data.external.validate_env[i].result["config_path"]
+  ]
 }
 
 output "validate_yaml" {
-    value = data.external.validate_env[count.index].result["yaml"]
+  value = [for i in range(length(data.external.validate_env)) :
+    data.external.validate_env[i].result["yaml"]
+  ]
 }
 
 output "validate_private_key" {
-    value = data.external.validate_env[count.index].result["private_key"]
+  value = [for i in range(length(data.external.validate_env)) :
+    data.external.validate_env[i].result["private_key"]
+  ]
 }
