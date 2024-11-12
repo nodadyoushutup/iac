b94141938e6fbdff7516fb3f4c2d3688d3c65fff
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-12 05:26:49 +0000

    [WIP] Spacelift

--- terraform/config/script/after_apply.py
+++ terraform/config/script/after_apply.py
@@ -1,7 +1,6 @@
 import subprocess
 import os
 import sys
-import json
 from datetime import datetime
 
 SPACECTL_PATH = "/mnt/workspace/source/terraform/config/bin/spacectl"
@@ -15,27 +14,6 @@ def verify_spacectl_path():
         sys.exit(1)
     if not os.access(SPACECTL_PATH, os.X_OK):
         print(f"{get_timestamp()} Error: spacectl at {SPACECTL_PATH} is not executable.")
-        sys.exit(1)
-
-def get_modules_without_version():
-    try:
-        # Run the spacectl list command to get module details in JSON format
-        result = subprocess.run(
-            [SPACECTL_PATH, "module", "list", "-o", "json"],
-            check=True,
-            capture_output=True,
-            text=True
-        )
-        modules_data = json.loads(result.stdout)
-        # Filter modules that do not have a valid current version
-        modules_without_version = [
-            module["id"] for module in modules_data
-            if "current" not in module or not module["current"].get("id")
-        ]
-        return modules_without_version
-    except subprocess.CalledProcessError as e:
-        print(f"{get_timestamp()} Error: Failed to retrieve module list.")
-        print(f"{get_timestamp()} Detailed error: {e.stderr.strip()}")
         sys.exit(1)
 
 def create_module_version(module):
@@ -57,19 +35,6 @@ def create_module_version(module):
 
 if __name__ == "__main__":
     verify_spacectl_path()
-    print(f"API TOKEN: {os.environ["SPACELIFT_API_TOKEN"]}")
-    result = subprocess.run(
-        [SPACECTL_PATH, "module", "list"],
-        check=True,
-        capture_output=True,
-        text=True
-    )
-    print(result)
-    # Retrieve modules without a current version
-    # modules = get_modules_without_version()
-    # if not modules:
-    #     print(f"{get_timestamp()} All modules already have versions. No action needed.")
-    # else:
-    #     # Loop through each module without a version and attempt to create one
-    #     for module in modules:
-    #         create_module_version(module)
+    modules = ["terraform-spacelift-stack"]
+    for module in modules:
+        create_module_version(module)
