9390c74a6982ac96a93f7760e207340935d7ffea
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-12 04:36:16 +0000

    [WIP] Spacelift

--- terraform/config/script/after_apply.py
+++ terraform/config/script/after_apply.py
@@ -1,16 +1,20 @@
 import subprocess
 import os
 import sys
+from datetime import datetime
 
 SPACECTL_PATH = "/mnt/workspace/source/terraform/config/bin/spacectl"
+
+def get_timestamp():
+    return datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
 def verify_spacectl_path():
     # Check if spacectl exists and is executable
     if not os.path.isfile(SPACECTL_PATH):
-        print(f"Error: spacectl not found at {SPACECTL_PATH}.")
+        print(f"{get_timestamp()} Error: spacectl not found at {SPACECTL_PATH}.")
         sys.exit(1)
     if not os.access(SPACECTL_PATH, os.X_OK):
-        print(f"Error: spacectl at {SPACECTL_PATH} is not executable.")
+        print(f"{get_timestamp()} Error: spacectl at {SPACECTL_PATH} is not executable.")
         sys.exit(1)
 
 def create_module_version(module):
@@ -20,10 +24,10 @@ def create_module_version(module):
             [SPACECTL_PATH, "module", "create-version", "--id", module],
             check=True
         )
-        print(f"Version created successfully for module {module}.")
+        print(f"{get_timestamp()} Version created successfully for module {module}.")
     except subprocess.CalledProcessError:
         # If the command fails, handle the error
-        print(f"Error encountered while creating version for module {module}. Ignoring and continuing.")
+        print(f"{get_timestamp()} Error encountered while creating version for module {module}. Ignoring and continuing.")
 
 if __name__ == "__main__":
     # Verify spacectl path and permissions
