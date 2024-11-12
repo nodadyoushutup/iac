eded01432324495325dad06c4730c784e20946a2
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-12 02:33:09 +0000

    [WIP] Spacelift

--- terraform/module/variable.tf
+++ terraform/module/variable.tf
@@ -1,10 +1,10 @@
-variable "BRANCH" {
-    type = string
-    description = "Branch"
-    default = "main"
+variable "GIT_BRANCH" {
+  type = string
+  description = "Branch"
+  default = "main"
 }
 
-variable "REPOSITORY" {
+variable "GIT_REPOSITORY" {
   type = string
   description = "Repository"
   default = "iac"
