480f9401c95aa1c7ba3cbf436414e949f3fe150b
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 01:31:13 +0000

    [WIP] Spacelift

--- terraform/config/context_attachment.tf
+++ terraform/config/context_attachment.tf
@@ -23,3 +23,13 @@ resource "spacelift_context_attachment" "spacectl_module" {
     priority   = 0
 }
 
+resource "spacelift_context_attachment" "config_module" {
+    count = var.FLAG_CONFIG >=1 && var.GIT_BRANCH != null && var.GIT_REPOSITORY != null ? 1 : 0
+    depends_on = [
+        spacelift_context.config,
+        spacelift_stack.module
+    ]
+    context_id = spacelift_context.config.id
+    stack_id   = spacelift_stack.module[0].id
+    priority   = 0
+}
--- terraform/module/main.tf
+++ terraform/module/main.tf
@@ -1,21 +1,21 @@
-# resource "spacelift_module" "spacelift_stack" {
-#     # REQUIRED
-#     branch = coalesce(try(local.config.spacelift.stack.branch, null), "main")
-#     repository = coalesce(try(local.config.spacelift.stack.repository, null), "iac")
+resource "spacelift_module" "spacelift_stack" {
+    # REQUIRED
+    branch = coalesce(var.GIT_BRANCH, "main")
+    repository = coalesce(var.GIT_REPOSITORY, "iac")
 
-#     # OPTIONAL
-#     administrative = false
-#     description = "Spacelift stack"
-#     enable_local_preview = false
-#     labels = ["spacelift"]
-#     name = "stack"
-#     project_root = "terraform/module/spacelift/stack"
-#     public = true
-#     shared_accounts = null
-#     space_id = "root"
-#     terraform_provider = "spacelift"
-#     workflow_tool = "TERRAFORM_FOSS"
-# }
+    # OPTIONAL
+    administrative = false
+    description = "Spacelift stack"
+    enable_local_preview = false
+    labels = ["spacelift"]
+    name = "stack"
+    project_root = "terraform/module/spacelift/stack"
+    public = true
+    shared_accounts = null
+    space_id = "root"
+    terraform_provider = "spacelift"
+    workflow_tool = "TERRAFORM_FOSS"
+}
 
 # resource "spacelift_context_attachment" "spacelift_stack_config" {
 #     depends_on = [spacelift_module.spacelift_stack]
