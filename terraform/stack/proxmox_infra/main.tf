2d832097e2d7335c14c5ec5dac7ec6fb6f41b262
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 21:00:36 +0000

    [WIP] Spacelift

--- terraform/stack/proxmox_infra/main.tf
+++ terraform/stack/proxmox_infra/main.tf
@@ -1,8 +1,31 @@
-resource "proxmox_virtual_environment_vm" "test_vm" {
-  stop_on_destroy = true
-  node_name = "pve"
-  agent {
-    enabled = false
-  }
+resource "proxmox_virtual_environment_download_file" "ubuntu_bionic_18_04" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "pve"
+  url = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
+  overwrite_unmanaged = true
 }
 
+resource "proxmox_virtual_environment_download_file" "ubuntu_focal_20_04" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "pve"
+  url = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
+  overwrite_unmanaged = true
+}
+
+resource "proxmox_virtual_environment_download_file" "ubuntu_jammy_22_04" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "pve"
+  url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+  overwrite_unmanaged = true
+}
+
+resource "proxmox_virtual_environment_download_file" "ubuntu_noble_22_04" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "pve"
+  url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+  overwrite_unmanaged = true
+}
