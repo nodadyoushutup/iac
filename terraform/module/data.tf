6b602455dfd74f5ad52108e5f20beaf841db2931
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 03:04:21 +0000

    [WIP] Spacelift

--- terraform/module/data.tf
+++ terraform/module/data.tf
@@ -1,3 +1,15 @@
 data "spacelift_context" "config" {
   context_id = "config"
 }
+
+data "spacelift_environment_variable" "GIT_BRANCH" {
+  depends_on = [data.spacelift_context.config]
+  context_id = data.spacelift_context.config
+  name = "GIT_BRANCH"
+}
+
+data "spacelift_environment_variable" "GIT_REPOSITORY" {
+  depends_on = [data.spacelift_context.config]
+  context_id = data.spacelift_context.config
+  name = "GIT_REPOSITORY"
+}
