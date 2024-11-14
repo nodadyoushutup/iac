9b1cf32668fb447c22d77cbd63226d0c976f098e
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 05:29:11 +0000

    [WIP] Spacelift

--- terraform/stack/spacelift_config/context_attachment.tf
+++ terraform/stack/spacelift_config/context_attachment.tf
@@ -1,35 +1,35 @@
-resource "spacelift_context_attachment" "config_config" {
+resource "spacelift_context_attachment" "config_spacelift_config" {
     depends_on = [spacelift_context.config]
     context_id = spacelift_context.config.id
     stack_id   = data.spacelift_stack.spacelift_config.id
     priority   = 0
 }
 
-resource "spacelift_context_attachment" "pyvenv_config" {
+resource "spacelift_context_attachment" "pyvenv_spacelift_config" {
     depends_on = [spacelift_context.config]
     context_id = spacelift_context.pyvenv.id
     stack_id   = data.spacelift_stack.spacelift_config.id
     priority   = 0
 }
 
-resource "spacelift_context_attachment" "spacectl_module" {
+resource "spacelift_context_attachment" "spacectl_spacelift_init" {
     count = var.FLAG_CONFIG >=1 && var.GIT_BRANCH != null && var.GIT_REPOSITORY != null ? 1 : 0
     depends_on = [
         spacelift_context.spacectl,
-        spacelift_stack.module
+        spacelift_stack.spacelift_stack
     ]
     context_id = spacelift_context.spacectl.id
-    stack_id   = spacelift_stack.module[0].id
+    stack_id   = spacelift_stack.spacelift_module[0].id
     priority   = 0
 }
 
-resource "spacelift_context_attachment" "config_module" {
+resource "spacelift_context_attachment" "config_spacelift_module" {
     count = var.FLAG_CONFIG >=1 && var.GIT_BRANCH != null && var.GIT_REPOSITORY != null ? 1 : 0
     depends_on = [
         spacelift_context.config,
-        spacelift_stack.module
+        spacelift_stack.spacelift_module
     ]
     context_id = spacelift_context.config.id
-    stack_id   = spacelift_stack.module[0].id
+    stack_id   = spacelift_stack.spacelift_module[0].id
     priority   = 0
 }
