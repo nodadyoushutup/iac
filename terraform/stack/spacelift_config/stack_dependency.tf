562783cad3098cf69bf8c3294c30a984f4b0c931
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 05:04:00 +0000

    [WIP] Spacelift

--- terraform/stack/spacelift_config/stack_dependency.tf
+++
@@ -1,6 +0,0 @@
-resource "spacelift_stack_dependency" "module_config" {
-  count = var.FLAG_CONFIG >=1 && var.GIT_BRANCH != null && var.GIT_REPOSITORY != null ? 1 : 0
-  depends_on = [spacelift_stack.module]
-  stack_id = spacelift_stack.module[0].id
-  depends_on_stack_id = data.spacelift_stack.config.id
-}
