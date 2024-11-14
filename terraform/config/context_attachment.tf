d0e18a280bff89979655a4ca124905e409a9b419
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 01:14:43 +0000

    [WIP] Spacelift

--- terraform/config/context_attachment.tf
+++ terraform/config/context_attachment.tf
@@ -5,10 +5,11 @@ resource "spacelift_context_attachment" "pyvenv_config" {
     priority   = 0
 }
 
-resource "spacelift_context_attachment" "spacectl_config" {
+resource "spacelift_context_attachment" "spacectl_module" {
+    count = var.FLAG_CONFIG >=1 && var.GIT_BRANCH != null && var.GIT_REPOSITORY != null ? 1 : 0
     depends_on = [spacelift_context.config]
     context_id = spacelift_context.spacectl.id
-    stack_id   = data.spacelift_stack.config.id
+    stack_id   = data.spacelift_stack.module.id
     priority   = 0
 }
 
--- terraform/config/stack.tf
+++ terraform/config/stack.tf
@@ -1,18 +1,19 @@
 
-# resource "spacelift_stack" "module" {
-#     count = var.BRANCH != null && var.REPOSITORY != null ? 1 : 0
-#     depends_on = [
-#         spacelift_environment_variable.branch,
-#         spacelift_environment_variable.repository
-#    ]
-#     administrative = true
-#     autodeploy = true
-#     branch = var.BRANCH
-#     description = "Modules"
-#     name = "module"
-#     project_root = "terraform/module"
-#     repository = var.REPOSITORY
-#     terraform_version = "1.5.7"
-#     labels = ["module"]
-# }
+resource "spacelift_stack" "module" {
+    count = var.FLAG_CONFIG >=1 && var.GIT_BRANCH != null && var.GIT_REPOSITORY != null ? 1 : 0
+    depends_on = [
+        spacelift_environment_variable.git_branch,
+        spacelift_environment_variable.git_repository,
+        spacelift_environment_variable.flag_config
+   ]
+    administrative = true
+    autodeploy = true
+    branch = var.BRANCH
+    description = "Modules"
+    name = "module"
+    project_root = "terraform/module"
+    repository = var.REPOSITORY
+    terraform_version = "1.5.7"
+    labels = ["module"]
+}
 
