84f6b5f265b67a5a1344adf5e13621eee85830fb
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 21:13:02 +0000

    [WIP] Spacelift

--- terraform/stack/proxmox_infra/main.tf
+++ terraform/stack/proxmox_infra/main.tf
@@ -34,6 +34,7 @@ resource "proxmox_virtual_environment_download_file" "centos_8_4" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "pve"
+  file_name = "CentOS-8-GenericCloud-8.4.img"
   url = "https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2"
   overwrite_unmanaged = true
 }
