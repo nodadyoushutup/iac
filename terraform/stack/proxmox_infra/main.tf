4a7ec24461ab511dff03a1f874b11674cc7f3001
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 21:20:14 +0000

    [WIP] Spacelift

--- terraform/stack/proxmox_infra/main.tf
+++ terraform/stack/proxmox_infra/main.tf
@@ -38,3 +38,16 @@ resource "proxmox_virtual_environment_download_file" "centos_8_4" {
   url = "https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2"
   overwrite_unmanaged = true
 }
+
+resource "proxmox_virtual_environment_vm" "test_vm" {
+  stop_on_destroy = true
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_download_file.centos_8_4.id
+    interface    = "scsi0"
+  }
+  node_name = "pve"
+  agent {
+    enabled = false
+  }
+}
