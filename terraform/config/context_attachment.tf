2fdd3f814ca2a7a119c51d4a998a9d50ad7d2d53
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 01:25:10 +0000

    [WIP] Spacelift

--- terraform/config/context_attachment.tf
+++ terraform/config/context_attachment.tf
@@ -14,7 +14,10 @@ resource "spacelift_context_attachment" "pyvenv_config" {
 
 resource "spacelift_context_attachment" "spacectl_module" {
     count = var.FLAG_CONFIG >=1 && var.GIT_BRANCH != null && var.GIT_REPOSITORY != null ? 1 : 0
-    depends_on = [spacelift_context.module]
+    depends_on = [
+        spacelift_context.spacectl,
+        spacelift_stack.module
+    ]
     context_id = spacelift_context.spacectl.id
     stack_id   = spacelift_stack.module.id
     priority   = 0
