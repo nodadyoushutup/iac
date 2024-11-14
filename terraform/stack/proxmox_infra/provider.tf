eb59084b02c1c0fdc6f794245494cd8b4182bad5
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 20:37:26 +0000

    [WIP] Proxmox

--- terraform/stack/proxmox_infra/provider.tf
+++ terraform/stack/proxmox_infra/provider.tf
@@ -10,7 +10,7 @@ provider "proxmox" {
   endpoint = "${local.config.proxmox.endpoint.protocol}://${local.config.proxmox.endpoint.ip_address}:${local.config.proxmox.endpoint.port}"
   insecure  = local.config.proxmox.endpoint.insecure
   password = local.config.proxmox.auth.password
-  username = local.config.proxmox.auth.username
+  username = "${local.config.proxmox.auth.username}@${local.config.proxmox.auth.realm}"
   ssh {
     agent = local.config.proxmox.ssh.agent.enabled
     agent_socket = local.config.proxmox.ssh.agent.socket
