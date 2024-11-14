3e83d95e3fdd4be287a8e2991669ca24b794acbe
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-14 05:33:40 +0000

    [WIP] Spacelift

--- terraform/stack/spacelift_config/context_attachment.tf
+++ terraform/stack/spacelift_config/context_attachment.tf
@@ -16,7 +16,7 @@ resource "spacelift_context_attachment" "spacectl_spacelift_init" {
     count = var.FLAG_CONFIG >=1 && var.GIT_BRANCH != null && var.GIT_REPOSITORY != null ? 1 : 0
     depends_on = [
         spacelift_context.spacectl,
-        spacelift_stack.spacelift_infra
+        spacelift_stack.spacelift_init
     ]
     context_id = spacelift_context.spacectl.id
     stack_id   = spacelift_stack.spacelift_init[0].id
