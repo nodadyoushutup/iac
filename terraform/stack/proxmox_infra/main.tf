5b4925830f083bcdf0d976577de82177f621d7a9
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-15 22:09:13 +0000

    [WIP] Talos

--- terraform/stack/proxmox_infra/main.tf
+++ terraform/stack/proxmox_infra/main.tf
@@ -1,3 +1,11 @@
+# resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+#   content_type = "iso"
+#   datastore_id = "local"
+#   node_name    = "pve"
+
+#   url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+# }
+
 # data "local_file" "ssh_public_key" {
 #   filename = "./id_rsa.pub"
 # }
@@ -50,124 +58,5 @@
 #   }
 # }
 
-resource "proxmox_virtual_environment_vm" "talos_cp_0" {
-  name      = "talos-cp-0"
-  node_name = "pve"
 
-  agent {
-    enabled = true
-  }
-
-  cpu {
-    cores = 2
-    type = "x86-64-v2-AES"
-  }
-
-  memory {
-    dedicated = 2048
-  }
-
-  tpm_state {
-    datastore_id = "local-lvm"
-    version = "v2.0"
-  }
-
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = proxmox_virtual_environment_download_file.talos_cloud_image_ip_210.id
-    interface    = "scsi0"
-    discard      = "on"
-    size         = 20
-  }
-
-  initialization {
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-
-  network_device {
-    bridge = "vmbr0"
-    mac_address = "0a:00:00:00:01:01"
-  }
-
-}
-
-resource "proxmox_virtual_environment_vm" "talos_w_0" {
-  name      = "talos-w-0"
-  node_name = "pve"
-
-  agent {
-    enabled = true
-  }
-
-  cpu {
-    cores = 2
-    type = "x86-64-v2-AES"
-  }
-
-  memory {
-    dedicated = 2048
-  }
-
-  tpm_state {
-    datastore_id = "local-lvm"
-    version = "v2.0"
-  }
-
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = proxmox_virtual_environment_download_file.talos_cloud_image.id
-    interface    = "scsi0"
-    discard      = "on"
-    size         = 20
-  }
-
-  initialization {
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-
-  network_device {
-    bridge = "vmbr0"
-  }
-
-}
-
-resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve"
-
-  url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
-}
-
-resource "proxmox_virtual_environment_download_file" "talos_cloud_image_ip_210" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve"
-  file_name = "talos_cloud_image_ip_210.img"
-  url = "https://factory.talos.dev/image/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515/v1.8.3/metal-amd64.qcow2"
-}
-
-resource "proxmox_virtual_environment_download_file" "talos_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve"
-  file_name = "talos_cloud_image.img"
-  url = "https://factory.talos.dev/image/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515/v1.8.3/metal-amd64.qcow2"
-}
-
-output "talos_cp_0_ipv4_address" {
-  value = proxmox_virtual_environment_vm.talos_cp_0.ipv4_addresses
-}
-
-output "talos_w_0_ipv4_address" {
-  value = proxmox_virtual_environment_vm.talos_w_0.ipv4_addresses
-}
 
--- terraform/stack/proxmox_infra/talos.tf
+++ terraform/stack/proxmox_infra/talos.tf
@@ -1,1 +1,144 @@
-# linux /boot/vmlinuz init_on_alloc=1 slab_nomerge pti=on panic=0 consoleblank=0 printk.devkmsg=on earlyprintk=ttyS0 console=tty0 console=ttyS0 talos.platform=metal ip=192.168.0.100::192.168.0.1:255.255.255.0::eth0:off
+resource "proxmox_virtual_environment_download_file" "talos_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "pve"
+  file_name = "talos_cloud_image.img"
+  url = "https://factory.talos.dev/image/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515/v1.8.3/metal-amd64.qcow2"
+}
+
+resource "proxmox_virtual_environment_vm" "talos_cp_0" {
+    depends_on = [ proxmox_virtual_environment_download_file.talos_cloud_image ]
+    name      = "talos-cp-0"
+    node_name = "pve"
+    vm_id = "200"
+
+    agent {
+        enabled = true
+    }
+
+    cpu {
+        cores = 2
+        type = "x86-64-v2-AES"
+    }
+
+    memory {
+        dedicated = 2048
+    }
+
+    tpm_state {
+        datastore_id = "local-lvm"
+        version = "v2.0"
+    }
+
+    disk {
+        datastore_id = "local-lvm"
+        file_id      = proxmox_virtual_environment_download_file.talos_cloud_image.id
+        interface    = "scsi0"
+        discard      = "on"
+        size         = 20
+    }
+
+    initialization {
+        ip_config {
+            ipv4 {
+                address = "dhcp"
+            }
+        }
+    }
+
+    network_device {
+        bridge = "vmbr0"
+        mac_address = "0a:00:00:00:02:00"
+    }
+
+}
+
+resource "proxmox_virtual_environment_vm" "talos_cp_1" {
+    name      = "talos-cp-1"
+    node_name = "pve"
+    vm_id = "201"
+
+    agent {
+        enabled = true
+    }
+
+    cpu {
+        cores = 2
+        type = "x86-64-v2-AES"
+    }
+
+    memory {
+        dedicated = 2048
+    }
+
+    tpm_state {
+        datastore_id = "local-lvm"
+        version = "v2.0"
+    }
+
+    disk {
+        datastore_id = "local-lvm"
+        file_id      = proxmox_virtual_environment_download_file.talos_cloud_image.id
+        interface    = "scsi0"
+        discard      = "on"
+        size         = 20
+    }
+
+    initialization {
+        ip_config {
+            ipv4 {
+                address = "dhcp"
+            }
+        }
+    }
+
+    network_device {
+        bridge = "vmbr0"
+        mac_address = "0a:00:00:00:02:01"
+    }
+}
+
+resource "proxmox_virtual_environment_vm" "talos_cp_2" {
+    name      = "talos-cp-2"
+    node_name = "pve"
+    vm_id = "202"
+
+    agent {
+        enabled = true
+    }
+
+    cpu {
+        cores = 2
+        type = "x86-64-v2-AES"
+    }
+
+    memory {
+        dedicated = 2048
+    }
+
+    tpm_state {
+        datastore_id = "local-lvm"
+        version = "v2.0"
+    }
+
+    disk {
+        datastore_id = "local-lvm"
+        file_id      = proxmox_virtual_environment_download_file.talos_cloud_image.id
+        interface    = "scsi0"
+        discard      = "on"
+        size         = 20
+    }
+
+    initialization {
+        ip_config {
+            ipv4 {
+                address = "dhcp"
+            }
+        }
+    }
+
+    network_device {
+        bridge = "vmbr0"
+        mac_address = "0a:00:00:00:02:02"
+    }
+}
