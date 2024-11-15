f52d8a8656a19f378a84e575e365d2ab2aa991cf
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-15 03:24:13 +0000

    [WIP] Proxmox

--- terraform/stack/proxmox_infra/main.tf
+++ terraform/stack/proxmox_infra/main.tf
@@ -1,12 +1,62 @@
-resource "proxmox_virtual_environment_vm" "test_vm" {
-  depends_on = [ proxmox_virtual_environment_download_file.centos_9 ]
-  stop_on_destroy = true
+data "local_file" "ssh_public_key" {
+  filename = "./id_rsa.pub"
+}
+
+resource "proxmox_virtual_environment_file" "cloud_config" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = "pve"
+
+  source_raw {
+    data = <<-EOF
+    #cloud-config
+    users:
+      - default
+      - name: ubuntu
+        groups:
+          - sudo
+        shell: /bin/bash
+        ssh_authorized_keys:
+          - ${trimspace(data.local_file.ssh_public_key.content)}
+        sudo: ALL=(ALL) NOPASSWD:ALL
+    runcmd:
+        - apt update
+        - apt install -y qemu-guest-agent net-tools
+        - timedatectl set-timezone America/Toronto
+        - systemctl enable qemu-guest-agent
+        - systemctl start qemu-guest-agent
+        - echo "done" > /tmp/cloud-config.done
+    EOF
+
+    file_name = "cloud-config.yaml"
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+  name      = "test-ubuntu"
+  node_name = "pve"
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
   disk {
     datastore_id = "local-lvm"
-    file_id = proxmox_virtual_environment_download_file.centos_9.id
-    interface = "scsi0"
-    size = 32
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
   }
+
   initialization {
     ip_config {
       ipv4 {
@@ -14,14 +64,23 @@ resource "proxmox_virtual_environment_vm" "test_vm" {
       }
     }
 
-    user_account {
-      keys     = []
-      password = "centos"
-      username = "centos"
-    }
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
   }
-  node_name = "pve"
-  agent {
-    enabled = false
+
+  network_device {
+    bridge = "vmbr0"
   }
+
+}
+
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "pve"
+
+  url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
+
+output "vm_ipv4_address" {
+  value = proxmox_virtual_environment_vm.ubuntu_vm.ipv4_addresses[1][0]
 }
