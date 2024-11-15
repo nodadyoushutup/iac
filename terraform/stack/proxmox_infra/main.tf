f22a79b0bda26c8efa49d570da0d5ab4af03cb6d
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-15 21:43:48 +0000

    [WIP] Talos

--- terraform/stack/proxmox_infra/main.tf
+++ terraform/stack/proxmox_infra/main.tf
@@ -152,7 +152,7 @@ resource "proxmox_virtual_environment_download_file" "talos_cloud_image_ip_210" 
   datastore_id = "local"
   node_name    = "pve"
   file_name = "talos_cloud_image_ip_210.img"
-  url = "https://factory.talos.dev/image/1ea23916cd08c79a19a004078f2449a181fbdb16df684122b5f9c73cb3e53e13/v1.8.3/metal-amd64.qcow2"
+  url = "https://factory.talos.dev/image/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515/v1.8.3/metal-amd64.qcow2"
 }
 
 resource "proxmox_virtual_environment_download_file" "talos_cloud_image" {
