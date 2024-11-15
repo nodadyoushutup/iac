27ec6752ce8cf90012e20abcec78cdc80c07e63b
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-15 22:52:40 +0000

    [WIP] Talos

--- terraform/stack/proxmox_infra/talos.tf
+++ terraform/stack/proxmox_infra/talos.tf
@@ -132,13 +132,15 @@ resource "proxmox_virtual_environment_vm" "talos_cp_2" {
     initialization {
         ip_config {
             ipv4 {
-                address = "dhcp"
+                # address = "dhcp"
+                address = "192.168.1.250/24"
+                gateway = "192.168.1.1"
             }
         }
     }
 
     network_device {
         bridge = "vmbr0"
-        mac_address = "0a:00:00:00:02:02"
+        # mac_address = "0a:00:00:00:02:02"
     }
 }
