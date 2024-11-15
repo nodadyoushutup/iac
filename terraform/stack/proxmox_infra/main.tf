870fc683d891484afbbe4a9a10785f3876558ebd
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-15 20:02:52 +0000

    [WIP] Proxmox

--- terraform/stack/proxmox_infra/main.tf
+++ terraform/stack/proxmox_infra/main.tf
@@ -107,5 +107,5 @@ resource "proxmox_virtual_environment_download_file" "talos_cloud_image" {
 
 
 output "vm_ipv4_address" {
-  value = proxmox_virtual_environment_vm.ubuntu_vm.ipv4_addresses[1][0]
+  value = proxmox_virtual_environment_vm.ubuntu_vm.ipv4_addresses
 }
