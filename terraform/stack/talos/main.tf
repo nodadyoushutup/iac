53b68fe405d78a7dfa559311a5197318bfaef814
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-16 03:36:49 +0000

    [WIP] Talos

--- terraform/stack/talos/main.tf
+++ terraform/stack/talos/main.tf
@@ -1,14 +1,14 @@
-# resource "talos_machine_secrets" "this" {
-#     talos_version = "1.8.3"
-# }
+resource "talos_machine_secrets" "this" {
+    talos_version = "1.8.3"
+}
 
-# data "talos_machine_configuration" "controlplane" {
-#     depends_on = [ talos_machine_secrets.this ]
-#     cluster_name     = local.config.talos.cluster_name
-#     cluster_endpoint = "https://${local.config.talos.control_plane[0].ip_address}:${local.config.talos.control_plane[0].port}"
-#     machine_type     = "controlplane"
-#     machine_secrets  = talos_machine_secrets.this.machine_secrets
-# }
+data "talos_machine_configuration" "controlplane" {
+    depends_on = [ talos_machine_secrets.this ]
+    cluster_name     = local.config.talos.cluster_name
+    cluster_endpoint = "https://talos.cp.0.nodadyoushutup.com"
+    machine_type     = "controlplane"
+    machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
 
 # data "talos_machine_configuration" "worker" {
 #     depends_on = [ talos_machine_secrets.this ]
