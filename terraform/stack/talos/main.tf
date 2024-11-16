b06b8543c5956659d2da99f82da26f5dd07cd0b4
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-16 04:46:27 +0000

    [WIP] Talos

--- terraform/stack/talos/main.tf
+++ terraform/stack/talos/main.tf
@@ -1,31 +1,17 @@
-# resource "talos_machine_secrets" "this" {
+# resource "talos_machine_secrets" "secrets" {
 #     talos_version = "1.8.3"
 # }
 
-# data "talos_machine_configuration" "controlplane" {
-#     depends_on = [ talos_machine_secrets.this ]
+# data "talos_machine_configuration" "talos_cp_0" {
 #     cluster_name     = local.config.talos.cluster_name
 #     cluster_endpoint = "https://${local.config.talos.control_plane[0].ip_address}:${local.config.talos.control_plane[0].port}"
 #     machine_type     = "controlplane"
-#     machine_secrets  = talos_machine_secrets.this.machine_secrets
+#     machine_secrets  = talos_machine_secrets.secrets.machine_secrets
 # }
 
-# data "talos_machine_configuration" "worker" {
-#     depends_on = [ talos_machine_secrets.this ]
-#     cluster_name     = local.config.talos.cluster_name
-#     cluster_endpoint = "https://${local.config.talos.control_plane[0].ip_address}:${local.config.talos.control_plane[0].port}"
-#     machine_type     = "worker"
-#     machine_secrets  = talos_machine_secrets.this.machine_secrets
-# }
-
-# data "talos_client_configuration" "this" {
-#     depends_on = [ 
-#         talos_machine_secrets.this, 
-#         data.talos_machine_configuration.controlplane, 
-#         data.talos_machine_configuration.worker 
-#     ]
+# data "talos_client_configuration" "secrets" {
 #     cluster_name         = local.config.talos.cluster_name
-#     client_configuration = talos_machine_secrets.this.client_configuration
+#     client_configuration = talos_machine_secrets.secrets.client_configuration
 #     endpoints            = [
 #         for control_plane in local.config.talos.control_plane :
 #         "${control_plane.ip_address}:${control_plane.port}"
@@ -33,41 +19,71 @@
 # }
 
 # resource "talos_machine_configuration_apply" "controlplane" {
-#     depends_on = [ 
-#         talos_machine_secrets.this,
-#         data.talos_machine_configuration.controlplane 
-#     ]
-#     client_configuration        = talos_machine_secrets.this.client_configuration
+#     client_configuration        = talos_machine_secrets.secrets.client_configuration
 #     machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
-#     for_each                    = { for idx, control_plane in local.config.talos.control_plane : idx => control_plane }
-#     node                        = "${each.value.ip_address}:${each.value.port}"
+#     node                        = "${local.config.talos.control_plane[0].ip_address}:${local.config.talos.control_plane[0].port}"
+#     # for_each                    = { for idx, control_plane in local.config.talos.control_plane : idx => control_plane }
+#     # node                        = "${each.value.ip_address}:${each.value.port}"
 # }
 
-# resource "talos_machine_configuration_apply" "worker" {
-#     depends_on = [ 
-#         talos_machine_secrets.this,
-#         data.talos_machine_configuration.worker 
-#     ]
-#     client_configuration        = talos_machine_secrets.this.client_configuration
-#     machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
-#     for_each                    = { for idx, worker in local.config.talos.worker : idx => worker }
-#     node                        = "${each.value.ip_address}:${each.value.port}"
-# }
+# # data "talos_machine_configuration" "worker" {
+# #     depends_on = [ talos_machine_secrets.this ]
+# #     cluster_name     = local.config.talos.cluster_name
+# #     cluster_endpoint = "https://${local.config.talos.control_plane[0].ip_address}:${local.config.talos.control_plane[0].port}"
+# #     machine_type     = "worker"
+# #     machine_secrets  = talos_machine_secrets.this.machine_secrets
+# # }
 
-# resource "talos_machine_bootstrap" "this" {
-#     depends_on = [ 
-#         talos_machine_secrets.this,
-#         talos_machine_configuration_apply.controlplane 
-#     ]
-#     client_configuration = talos_machine_secrets.this.client_configuration
-#     node                 = "${local.config.talos.control_plane[0].ip_address}:${local.config.talos.control_plane[0].port}"
-# }
-
-# # resource "talos_cluster_kubeconfig" "this" {
+# # data "talos_client_configuration" "this" {
 # #     depends_on = [ 
-# #         talos_machine_secrets.this.client_configuration,
-# #         talos_machine_bootstrap.this
+# #         talos_machine_secrets.this, 
+# #         data.talos_machine_configuration.controlplane, 
+# #         data.talos_machine_configuration.worker 
+# #     ]
+# #     cluster_name         = local.config.talos.cluster_name
+# #     client_configuration = talos_machine_secrets.this.client_configuration
+# #     endpoints            = [
+# #         for control_plane in local.config.talos.control_plane :
+# #         "${control_plane.ip_address}:${control_plane.port}"
+# #     ]
+# # }
+
+# # resource "talos_machine_configuration_apply" "controlplane" {
+# #     depends_on = [ 
+# #         talos_machine_secrets.this,
+# #         data.talos_machine_configuration.controlplane 
+# #     ]
+# #     client_configuration        = talos_machine_secrets.this.client_configuration
+# #     machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
+# #     for_each                    = { for idx, control_plane in local.config.talos.control_plane : idx => control_plane }
+# #     node                        = "${each.value.ip_address}:${each.value.port}"
+# # }
+
+# # resource "talos_machine_configuration_apply" "worker" {
+# #     depends_on = [ 
+# #         talos_machine_secrets.this,
+# #         data.talos_machine_configuration.worker 
+# #     ]
+# #     client_configuration        = talos_machine_secrets.this.client_configuration
+# #     machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
+# #     for_each                    = { for idx, worker in local.config.talos.worker : idx => worker }
+# #     node                        = "${each.value.ip_address}:${each.value.port}"
+# # }
+
+# # resource "talos_machine_bootstrap" "this" {
+# #     depends_on = [ 
+# #         talos_machine_secrets.this,
+# #         talos_machine_configuration_apply.controlplane 
 # #     ]
 # #     client_configuration = talos_machine_secrets.this.client_configuration
 # #     node                 = "${local.config.talos.control_plane[0].ip_address}:${local.config.talos.control_plane[0].port}"
 # # }
+
+# # # resource "talos_cluster_kubeconfig" "this" {
+# # #     depends_on = [ 
+# # #         talos_machine_secrets.this.client_configuration,
+# # #         talos_machine_bootstrap.this
+# # #     ]
+# # #     client_configuration = talos_machine_secrets.this.client_configuration
+# # #     node                 = "${local.config.talos.control_plane[0].ip_address}:${local.config.talos.control_plane[0].port}"
+# # # }
