8a65d2451a25eb28da3a84d7a1d1c6fc1955b430
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-16 02:02:13 +0000

    [WIP] Talos

--- terraform/stack/talos/variable.tf
+++ terraform/stack/talos/variable.tf
@@ -20,50 +20,50 @@ variable "cluster_endpoint" {
   type        = string
 }
 
-variable "node_data" {
-  description = "A map of node data"
-  type = object({
-    controlplanes = map(object({
-      install_disk = string
-      hostname     = optional(string)
-    }))
-    workers = map(object({
-      install_disk = string
-      hostname     = optional(string)
-    }))
-  })
-  default = {
-    controlplanes = {
-      "${local.config.talos.ip_address}:${local.config.talos.control_plane[0].port}" = {
-        install_disk = "/dev/sda"
-        hostname = "talos-cp-0"
-      },
-      "${local.config.talos.ip_address}:${local.config.talos.control_plane[1].port}" = {
-        install_disk = "/dev/sda"
-        hostname = "talos-cp-1"
-      },
-      "${local.config.talos.ip_address}:${local.config.talos.control_plane[2].port}" = {
-        install_disk = "/dev/sda"
-        hostname = "talos-cp-1"
-      }
-    }
-    workers = {
-      "${local.config.talos.ip_address}:${local.config.talos.worker[0].port}" = {
-        install_disk = "/dev/sda"
-        hostname = "talos-wk-0"
-      },
-      "${local.config.talos.ip_address}:${local.config.talos.worker[1].port}" = {
-        install_disk = "/dev/sda"
-        hostname = "talos-wk-1"
-      }
-      "${local.config.talos.ip_address}:${local.config.talos.worker[2].port}" = {
-        install_disk = "/dev/sda"
-        hostname = "talos-wk-2"
-      }
-      "${local.config.talos.ip_address}:${local.config.talos.worker[3].port}" = {
-        install_disk = "/dev/sda"
-        hostname = "talos-wk-3"
-      }
-    }
-  }
-}
+# variable "node_data" {
+#   description = "A map of node data"
+#   type = object({
+#     controlplanes = map(object({
+#       install_disk = string
+#       hostname     = optional(string)
+#     }))
+#     workers = map(object({
+#       install_disk = string
+#       hostname     = optional(string)
+#     }))
+#   })
+#   default = {
+#     controlplanes = {
+#       "${local.config.talos.ip_address}:${local.config.talos.control_plane[0].port}" = {
+#         install_disk = "/dev/sda"
+#         hostname = "talos-cp-0"
+#       },
+#       "${local.config.talos.ip_address}:${local.config.talos.control_plane[1].port}" = {
+#         install_disk = "/dev/sda"
+#         hostname = "talos-cp-1"
+#       },
+#       "${local.config.talos.ip_address}:${local.config.talos.control_plane[2].port}" = {
+#         install_disk = "/dev/sda"
+#         hostname = "talos-cp-1"
+#       }
+#     }
+#     workers = {
+#       "${local.config.talos.ip_address}:${local.config.talos.worker[0].port}" = {
+#         install_disk = "/dev/sda"
+#         hostname = "talos-wk-0"
+#       },
+#       "${local.config.talos.ip_address}:${local.config.talos.worker[1].port}" = {
+#         install_disk = "/dev/sda"
+#         hostname = "talos-wk-1"
+#       }
+#       "${local.config.talos.ip_address}:${local.config.talos.worker[2].port}" = {
+#         install_disk = "/dev/sda"
+#         hostname = "talos-wk-2"
+#       }
+#       "${local.config.talos.ip_address}:${local.config.talos.worker[3].port}" = {
+#         install_disk = "/dev/sda"
+#         hostname = "talos-wk-3"
+#       }
+#     }
+#   }
+# }
