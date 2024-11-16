8e9c99c7aa9f2d694eef3b78523839c21f05a4a7
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-16 00:34:30 +0000

    [WIP] Talos

--- terraform/stack/talos/main.tf
+++ terraform/stack/talos/main.tf
@@ -22,3 +22,31 @@ data "talos_machine_configuration" "talos_cp_2" {
   machine_type     = "controlplane"
   machine_secrets  = talos_machine_secrets.this.machine_secrets
 }
+
+data "talos_machine_configuration" "talos_wk_0" {
+  cluster_name     = local.config.talos.cluster_name
+  cluster_endpoint = "https://${local.config.talos.worker[0].ip_address}:${local.config.talos.worker[0].port}"
+  machine_type     = "controlplane"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+data "talos_machine_configuration" "talos_wk_1" {
+  cluster_name     = local.config.talos.cluster_name
+  cluster_endpoint = "https://${local.config.talos.worker[1].ip_address}:${local.config.talos.worker[1].port}"
+  machine_type     = "controlplane"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+data "talos_machine_configuration" "talos_wk_2" {
+  cluster_name     = local.config.talos.cluster_name
+  cluster_endpoint = "https://${local.config.talos.worker[2].ip_address}:${local.config.talos.worker[2].port}"
+  machine_type     = "controlplane"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+data "talos_machine_configuration" "talos_wk_3" {
+  cluster_name     = local.config.talos.cluster_name
+  cluster_endpoint = "https://${local.config.talos.worker[2].ip_address}:${local.config.talos.worker[2].port}"
+  machine_type     = "controlplane"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
