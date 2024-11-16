2ced78aab295ab385c7303e95a6fbf1e8e359a96
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-16 00:21:12 +0000

    [WIP] Talos

---
+++ terraform/stack/talos/main.tf
@@ -0,0 +1,10 @@
+resource "talos_machine_secrets" "machine_secrets" {
+    talos_version = "1.8.3"
+}
+
+data "talos_machine_configuration" "controlplane" {
+  cluster_name     = var.cluster_name
+  cluster_endpoint = var.cluster_endpoint
+  machine_type     = "controlplane"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
