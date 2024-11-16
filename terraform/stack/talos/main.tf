c14c48fe78bbd19f1f2e6f69289bfa6de0e2084b
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-16 00:32:20 +0000

    [WIP] Talos

--- terraform/stack/talos/main.tf
+++ terraform/stack/talos/main.tf
@@ -4,7 +4,7 @@ resource "talos_machine_secrets" "this" {
 
 data "talos_machine_configuration" "controlplane" {
   cluster_name     = local.config.talos.cluster_name
-  cluster_endpoint = "${local.config.talos.control_plane[0].protocol}://${local.config.talos.control_plane[0].ip_address}:${local.config.talos.control_plane[0].port}"
+  cluster_endpoint = "https://${local.config.talos.control_plane[0].ip_address}:${local.config.talos.control_plane[0].port}"
   machine_type     = "controlplane"
   machine_secrets  = talos_machine_secrets.this.machine_secrets
 }
