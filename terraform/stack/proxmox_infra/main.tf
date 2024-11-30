652436a9fcc37f4592ffa96635bd3bae42234e74
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-30 21:55:21 +0000

    [WIP] Docker

--- terraform/stack/proxmox_infra/main.tf
+++ terraform/stack/proxmox_infra/main.tf
@@ -1,3 +1,50 @@
+resource "proxmox_virtual_environment_vm" "docker" {
+    depends_on = [ proxmox_virtual_environment_download_file.cloud_image ]
+    name      = "docker"
+    node_name = "pve"
+    vm_id = "1102"
+
+    agent {
+        enabled = true
+    }
+
+    cpu {
+        cores = 2
+        type = "x86-64-v2-AES"
+    }
+
+    memory {
+        dedicated = 4096
+    }
+
+    tpm_state {
+        datastore_id = "virtualization"
+        version = "v2.0"
+    }
+
+    disk {
+        datastore_id = "virtualization"
+        file_id      = proxmox_virtual_environment_download_file.cloud_image.id
+        interface    = "scsi0"
+        discard      = "on"
+        size         = 100
+    }
+
+    initialization {
+        ip_config {
+            ipv4 {
+                address = "dhcp"
+            }
+        }
+    }
+
+    network_device {
+        bridge = "vmbr0"
+        mac_address = "0a:00:00:00:11:02"
+    }
+
+}
+
 resource "proxmox_virtual_environment_vm" "development" {
     depends_on = [ proxmox_virtual_environment_download_file.cloud_image ]
     name      = "development"
@@ -27,7 +74,7 @@ resource "proxmox_virtual_environment_vm" "development" {
         file_id      = proxmox_virtual_environment_download_file.cloud_image.id
         interface    = "scsi0"
         discard      = "on"
-        size         = 20
+        size         = 100
     }
 
     initialization {
