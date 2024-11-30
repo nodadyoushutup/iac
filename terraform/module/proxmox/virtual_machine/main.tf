85b9d4ee1346df32773b2b19947f0e5c0a4874d7
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-30 22:31:47 +0000

    [WIP] VM

--- terraform/module/proxmox/virtual_machine/main.tf
+++ terraform/module/proxmox/virtual_machine/main.tf
@@ -24,7 +24,7 @@ resource "proxmox_virtual_environment_vm" "virtual_machine" {
 
     disk {
         datastore_id = "virtualization"
-        file_id      = data.proxmox_virtual_environment_download_file.cloud_image.id
+        file_id      = "local:iso/cloud_image_x86_64_jammy.img"
         interface    = "scsi0"
         discard      = "on"
         size         = 100
