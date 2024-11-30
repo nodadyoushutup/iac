37f47f973bad5d2cd5d07acd7593235ed6245488
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-30 22:52:45 +0000

    [WIP] VM

--- terraform/module/proxmox/virtual_machine/main.tf
+++ terraform/module/proxmox/virtual_machine/main.tf
@@ -1,49 +1,49 @@
 resource "proxmox_virtual_environment_vm" "virtual_machine" {
-    name      = "vm"
-    node_name = "pve"
-    vm_id = "1103"
+    # name      = "vm"
+    # node_name = "pve"
+    # vm_id = "1103"
 
-    agent {
-        enabled = true
-    }
+    # agent {
+    #     enabled = true
+    # }
 
-    cpu {
-        cores = 2
-        type = "x86-64-v2-AES"
-    }
+    # cpu {
+    #     cores = 2
+    #     type = "x86-64-v2-AES"
+    # }
 
-    memory {
-        dedicated = 4096
-    }
+    # memory {
+    #     dedicated = 4096
+    # }
 
-    tpm_state {
-        datastore_id = "virtualization"
-        version = "v2.0"
-    }
+    # tpm_state {
+    #     datastore_id = "virtualization"
+    #     version = "v2.0"
+    # }
 
-    disk {
-        datastore_id = "virtualization"
-        file_id      = "local:iso/cloud_image_x86_64_jammy.img"
-        interface    = "scsi0"
-        discard      = "on"
-        size         = 100
-    }
+    # disk {
+    #     datastore_id = "virtualization"
+    #     file_id      = "local:iso/cloud_image_x86_64_jammy.img"
+    #     interface    = "scsi0"
+    #     discard      = "on"
+    #     size         = 100
+    # }
 
-    initialization {
-        ip_config {
-            ipv4 {
-                address = "dhcp"
-            }
-            ipv6 {
-                address = "dhcp"
-            }
-        }
-    }
+    # initialization {
+    #     ip_config {
+    #         ipv4 {
+    #             address = "dhcp"
+    #         }
+    #         ipv6 {
+    #             address = "dhcp"
+    #         }
+    #     }
+    # }
 
-    network_device {
-        bridge = "vmbr0"
-        mac_address = "0a:00:00:00:11:02"
-    }
+    # network_device {
+    #     bridge = "vmbr0"
+    #     mac_address = "0a:00:00:00:11:02"
+    # }
 
 }
 
