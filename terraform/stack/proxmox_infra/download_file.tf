990b7b1d25b07b14f089a83e919c6e2190ae54cf
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-30 21:36:07 +0000

    [WIP] Development

--- terraform/stack/proxmox_infra/download_file.tf
+++ terraform/stack/proxmox_infra/download_file.tf
@@ -3,7 +3,6 @@ resource "proxmox_virtual_environment_download_file" "cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "pve"
-  file_name = "cloud_image_x86_64_jammy.img"
   url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.0.2/cloud_image_x86_64_jammy.img"
 }
 
