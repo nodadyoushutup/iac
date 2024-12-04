2b1e209362a0931baf26073a04f068bdcfb675a2
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-04 21:16:22 +0000

    [WIP] VM

--- terraform/stack/proxmox_infra/main.tf
+++ terraform/stack/proxmox_infra/main.tf
@@ -35,10 +35,10 @@ resource "proxmox_virtual_environment_file" "cloud_config" {
           %{ endfor }
         sudo: ALL=(ALL) NOPASSWD:ALL
     write_files:
-        - path: /tmp/cloud-config.done
-          content: |
-            Cloud configuration is done.
-          permissions: '0644'      
+      - path: /tmp/cloud-config.done
+        content: |
+          Cloud configuration is done.
+        permissions: '0644'      
     runcmd:
       %{ for cmd in data.spacelift_context.spacectl.after_run }
       - ${cmd}
