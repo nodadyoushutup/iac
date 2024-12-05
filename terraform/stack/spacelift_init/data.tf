1c6513f80b7dd6fb6cd04ea8cdaab53cc5f57d82
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 00:45:17 +0000

    [WIP] Spacelift

--- terraform/stack/spacelift_init/data.tf
+++ terraform/stack/spacelift_init/data.tf
@@ -1,19 +1,19 @@
-data "spacelift_space" "root" {
-  space_id = "root"
-}
+# data "spacelift_space" "root" {
+#   space_id = "root"
+# }
 
-data "spacelift_context" "config" {
-  context_id = "config"
-}
+# data "spacelift_context" "config" {
+#   context_id = "config"
+# }
 
-data "spacelift_environment_variable" "git_branch" {
-  depends_on = [data.spacelift_context.config]
-  context_id = data.spacelift_context.config.id
-  name = "TF_VAR_GIT_BRANCH"
-}
+# data "spacelift_environment_variable" "git_branch" {
+#   depends_on = [data.spacelift_context.config]
+#   context_id = data.spacelift_context.config.id
+#   name = "TF_VAR_GIT_BRANCH"
+# }
 
-data "spacelift_environment_variable" "git_repository" {
-  depends_on = [data.spacelift_context.config]
-  context_id = data.spacelift_context.config.id
-  name = "TF_VAR_GIT_REPOSITORY"
-}
+# data "spacelift_environment_variable" "git_repository" {
+#   depends_on = [data.spacelift_context.config]
+#   context_id = data.spacelift_context.config.id
+#   name = "TF_VAR_GIT_REPOSITORY"
+# }
--- terraform/stack/spacelift_init/variable.tf
+++ terraform/stack/spacelift_init/variable.tf
@@ -1,11 +1,11 @@
-variable "GIT_BRANCH" {
-  type = string
-  description = "Branch"
-  default = "main"
-}
+# variable "GIT_BRANCH" {
+#   type = string
+#   description = "Branch"
+#   default = "main"
+# }
 
-variable "GIT_REPOSITORY" {
-  type = string
-  description = "Repository"
-  default = "iac"
-}
+# variable "GIT_REPOSITORY" {
+#   type = string
+#   description = "Repository"
+#   default = "iac"
+# }
