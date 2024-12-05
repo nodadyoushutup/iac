42d4fe4b991ee5058d3d1150c3b80c1b9f5df7c4
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 00:45:37 +0000

    [WIP] Spacelift

--- terraform/stack/spacelift_infra/variable.tf
+++ terraform/stack/spacelift_infra/variable.tf
@@ -1,11 +1,11 @@
-variable "GIT_BRANCH" {
-    type = string
-    description = "Environment branch"
-    default = null
-}
+# variable "GIT_BRANCH" {
+#     type = string
+#     description = "Environment branch"
+#     default = null
+# }
 
-variable "GIT_REPOSITORY" {
-    type = string
-    description = "Environment repository"
-    default = null
-}
+# variable "GIT_REPOSITORY" {
+#     type = string
+#     description = "Environment repository"
+#     default = null
+# }
