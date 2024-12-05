0cd0bc671c3db95726f515f5d41535348c9dba03
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 00:37:27 +0000

    [WIP] VM

--- terraform/module/proxmox/virtual_machine/provider.tf
+++ terraform/module/proxmox/virtual_machine/provider.tf
@@ -1,22 +1,22 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "bpg/proxmox"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     proxmox = {
+#       source = "bpg/proxmox"
+#     }
+#   }
+# }
 
-provider "proxmox" {
-  endpoint = "${local.config.proxmox.endpoint.protocol}://${local.config.proxmox.endpoint.ip_address}:${local.config.proxmox.endpoint.port}"
-  insecure  = local.config.proxmox.endpoint.insecure
-  username = "${local.config.proxmox.auth.username}@${local.config.proxmox.auth.realm}"
-  password = local.config.proxmox.auth.password
-  ssh {
-    private_key = file(local.config.proxmox.ssh.private_key)
-    node {
-      name = local.config.proxmox.ssh.node.name
-      address = local.config.proxmox.ssh.node.address
-      port = local.config.proxmox.ssh.node.port
-    }
-  }
-}
+# provider "proxmox" {
+#   endpoint = "${local.config.proxmox.endpoint.protocol}://${local.config.proxmox.endpoint.ip_address}:${local.config.proxmox.endpoint.port}"
+#   insecure  = local.config.proxmox.endpoint.insecure
+#   username = "${local.config.proxmox.auth.username}@${local.config.proxmox.auth.realm}"
+#   password = local.config.proxmox.auth.password
+#   ssh {
+#     private_key = file(local.config.proxmox.ssh.private_key)
+#     node {
+#       name = local.config.proxmox.ssh.node.name
+#       address = local.config.proxmox.ssh.node.address
+#       port = local.config.proxmox.ssh.node.port
+#     }
+#   }
+# }
