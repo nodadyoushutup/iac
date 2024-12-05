a965d1864fa76961cc6e56218fef8170f3d1104d
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 03:49:07 +0000

    [WIP] Spacelift

--- terraform/stack/spacelift/context.tf
+++ terraform/stack/spacelift/context.tf
@@ -22,10 +22,10 @@ resource "spacelift_context" "config" {
     name = "config"
     description = "Configuration"
     space_id = "root"
-    # before_init = [
-    #     "envsubst < /mnt/workspace/source/terraform/stack/config/config.yaml > /mnt/workspace/config.yaml",
-    #     "cat /mnt/workspace/config.yaml"
-    # ]
+    before_init = [
+        "envsubst < /mnt/workspace/source/terraform/stack/config/config.yaml > /mnt/workspace/config.yaml",
+        "cat /mnt/workspace/config.yaml"
+    ]
 }
 
 # resource "spacelift_context" "github" {
