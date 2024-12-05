a9c69a9056f189c1af60cc88fa24374146df77cf
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 00:25:43 +0000

    [WIP] VM

--- terraform/module/proxmox/virtual_machine/local.tf
+++ terraform/module/proxmox/virtual_machine/local.tf
@@ -1,5 +1,5 @@
-locals {
-  config_path = try(var.PATH_CONFIG != null && var.PATH_CONFIG != "" ? var.PATH_CONFIG : "/mnt/workspace/config.yaml")
-  config = try(yamldecode(file(local.config_path)), {})
-}
+# locals {
+#   config_path = try(var.PATH_CONFIG != null && var.PATH_CONFIG != "" ? var.PATH_CONFIG : "/mnt/workspace/config.yaml")
+#   config = try(yamldecode(file(local.config_path)), {})
+# }
 
