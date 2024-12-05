fc9dac5eb9dd150a54ef57f29d32f0aff4ca95c1
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 04:47:34 +0000

    [WIP] Spacelift

--- terraform/stack/spacelift/context.tf
+++ terraform/stack/spacelift/context.tf
@@ -1,23 +1,3 @@
-# resource "spacelift_context" "pyvenv" {
-#     name = "pyvenv"
-#     description = "Runner Python virtual environment hooks"
-#     space_id = data.spacelift_space.root.id
-#     before_init = [
-#         "python3 -m venv venv",
-#         "source venv/bin/activate",
-#         "pip install --upgrade pip && pip install pyyaml paramiko",
-#     ]
-# }
-
-# resource "spacelift_context" "spacectl" {
-#     name = "spacectl"
-#     description = "Runner Spacectl CLI"
-#     space_id = data.spacelift_space.root.id
-#     after_apply = [
-#         "python3 ${path.module}/script/after_apply.py"
-#     ]
-# }
-
 resource "spacelift_context" "config" {
     name = "config"
     description = "Configuration"
@@ -27,15 +7,3 @@ resource "spacelift_context" "config" {
         "cat /mnt/workspace/config.yaml"
     ]
 }
-
-# resource "spacelift_context" "github" {
-#     name = "github"
-#     description = "Github configuration"
-#     space_id = "root"
-# }
-
-# resource "spacelift_context" "proxmox" {
-#     name = "proxmox"
-#     description = "Proxmox configuration"
-#     space_id = "root"
-# }
