b3e1aeabd285cb8bb9e898c22adad32a80f728e7
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 00:44:51 +0000

    [WIP] VM

--- terraform/stack/spacelift_infra/main.tf
+++ terraform/stack/spacelift_infra/main.tf
@@ -1,17 +1,17 @@
-module "proxmox_infra" {
-    source  = "spacelift.io/nodadyoushutup/stack/spacelift"
-    branch = coalesce(var.GIT_BRANCH, "main")
-    name = "proxmox_infra"
-    project_root = "terraform/stack/proxmox_infra"
-    repository = coalesce(var.GIT_REPOSITORY, "iac")
-    terraform_version = "1.5.7"
-}
+# module "proxmox_infra" {
+#     source  = "spacelift.io/nodadyoushutup/stack/spacelift"
+#     branch = coalesce(var.GIT_BRANCH, "main")
+#     name = "proxmox_infra"
+#     project_root = "terraform/stack/proxmox_infra"
+#     repository = coalesce(var.GIT_REPOSITORY, "iac")
+#     terraform_version = "1.5.7"
+# }
 
-module "talos" {
-    source  = "spacelift.io/nodadyoushutup/stack/spacelift"
-    branch = coalesce(var.GIT_BRANCH, "main")
-    name = "talos"
-    project_root = "terraform/stack/talos"
-    repository = coalesce(var.GIT_REPOSITORY, "iac")
-    terraform_version = "1.5.7"
-}
+# module "talos" {
+#     source  = "spacelift.io/nodadyoushutup/stack/spacelift"
+#     branch = coalesce(var.GIT_BRANCH, "main")
+#     name = "talos"
+#     project_root = "terraform/stack/talos"
+#     repository = coalesce(var.GIT_REPOSITORY, "iac")
+#     terraform_version = "1.5.7"
+# }
