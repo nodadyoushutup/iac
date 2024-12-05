64162d5cc116af5f77eca26d9ef5ec0c7ada3091
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 02:15:50 +0000

    [WIP] Spacelift

--- terraform/stack/spacelift/proxmox.tf
+++ terraform/stack/spacelift/proxmox.tf
@@ -1,6 +1,6 @@
 resource "proxmox_virtual_environment_download_file" "cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve"
-  url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.1/cloud_image_x86_64_jammy.img"
+    content_type = "iso"
+    datastore_id = "local"
+    node_name    = "pve"
+    url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.1/cloud_image_x86_64_jammy.img"
 }
