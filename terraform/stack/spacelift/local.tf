a613be61b9697080ddb532a460e3a693ba5e5ab2
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 05:40:35 +0000

    [WIP] Spacelift

--- terraform/stack/spacelift/local.tf
+++ terraform/stack/spacelift/local.tf
@@ -1,6 +1,6 @@
 locals {
     config_path = var.CONFIG_PATH
-    config = try(yamldecode(file(local.config_path)))
+    config = try(yamldecode(file(local.config_path)), "/mnt/workspace/source/terraform/stack/spacelift/config/config.yaml")
     base64 = {
         private_key = try(
             filebase64(var.DEFAULT_PRIVATE_KEY), 
