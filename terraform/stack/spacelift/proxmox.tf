5a233fe67d5b5182fcc929072a470c1337247897
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 03:20:51 +0000

    [WIP] Spacelift

--- terraform/stack/spacelift/proxmox.tf
+++ terraform/stack/spacelift/proxmox.tf
@@ -5,35 +5,35 @@
 #     url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.1/cloud_image_x86_64_jammy.img"
 # }
 
-resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
-  node_name = "pve"
+# resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+#   node_name = "pve"
 
-  agent {
-    enabled = false
-  }
-  stop_on_destroy = true
+#   agent {
+#     enabled = false
+#   }
+#   stop_on_destroy = true
 
-  cpu {
-    cores        = 2
-    type         = "x86-64-v2-AES"
-  }
+#   cpu {
+#     cores        = 2
+#     type         = "x86-64-v2-AES"
+#   }
 
-  memory {
-    dedicated = 2048
-    floating  = 0
-  }
+#   memory {
+#     dedicated = 2048
+#     floating  = 0
+#   }
 
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = "local:iso/cloud_image_x86_64_jammy.img"
-    interface    = "scsi0"
-  }
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = "local:iso/cloud_image_x86_64_jammy.img"
+#     interface    = "scsi0"
+#   }
 
-  network_device {
-    bridge = "vmbr0"
-  }
+#   network_device {
+#     bridge = "vmbr0"
+#   }
 
-  operating_system {
-    type = "l26"
-  }
-}
+#   operating_system {
+#     type = "l26"
+#   }
+# }
