aabbe21b18fb8977dcbc4fe66af7486e607a71df
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-05 06:42:54 +0000

    [WIP] Proxmox

--- terraform/stack/spacelift/context_attachment.tf
+++ terraform/stack/spacelift/context_attachment.tf
@@ -5,13 +5,13 @@ resource "spacelift_context_attachment" "config_spacelift" {
     priority   = 0
 }
 
-# resource "spacelift_context_attachment" "spacectl_spacelift_init" {
-#     count = var.FLAG_CONFIG >=1 && var.GIT_BRANCH != null && var.GIT_REPOSITORY != null ? 1 : 0
-#     depends_on = [
-#         spacelift_context.spacectl,
-#         spacelift_stack.spacelift_init
-#     ]
-#     context_id = spacelift_context.spacectl.id
-#     stack_id   = spacelift_stack.spacelift_init[0].id
-#     priority   = 0
-# }
+resource "spacelift_context_attachment" "config_proxmox" {
+    count = var.FLAG_VALID_CONFIG >=1 ? 1 : 0
+    depends_on = [
+        spacelift_context.config,
+        spacelift_stack.proxmox
+    ]
+    context_id = spacelift_context.spacectl.id
+    stack_id   = spacelift_stack.proxmox[0].id
+    priority   = 0
+}
