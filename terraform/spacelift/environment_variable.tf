2d9252d925aca8d1de789114803ed8fc72a117fa
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-06 20:08:56 +0000

    [WIP] Spacelift

--- terraform/spacelift/environment_variable.tf
+++ terraform/spacelift/environment_variable.tf
@@ -3,7 +3,7 @@ resource "spacelift_environment_variable" "FLAG_VALID_CONFIG" {
     depends_on = [spacelift_context.config]
     context_id  = spacelift_context.config.id
     name        = "TF_VAR_FLAG_VALID_CONFIG" 
-    value       = data.external.validate[0].result["valid"] == "true" ? var.FLAG_VALID_CONFIG + 1 : 0
+    value       = data.external.validate.result["valid"] == "true" ? var.FLAG_VALID_CONFIG + 1 : 0
     write_only  = false 
     description = "Valid configuration"
 }
