df8106dc6957ff6149df02cadc9052fa616ad5ef
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-06 21:08:24 +0000

    [WIP] Proxmox

--- terraform/proxmox/local.tf
+++ terraform/proxmox/local.tf
@@ -1,13 +1,9 @@
 locals {
   config = {
-        path = {
-            config = var.CONFIG_PATH_CONFIG
-            public_key = var.CONFIG_PATH_PUBLIC_KEY
-        }
-        data = try(yamldecode(file("${path.module}/config.sub.yaml")), {})
+    path = {
+      config = var.CONFIG_PATH_CONFIG
+      public_key = var.CONFIG_PATH_PUBLIC_KEY
     }
-}
-
-output "debug" {
-  value = local.config
+    data = try(yamldecode(file("${path.module}/config.sub.yaml")), {})
+  }
 }
