dbc072ee6f08da5723aa428bc092ce6db0d8dabd
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-06 19:39:08 +0000

    [WIP] Spacelift

--- terraform/spacelift/context.tf
+++ terraform/spacelift/context.tf
@@ -3,7 +3,6 @@ resource "spacelift_context" "config" {
     description = "Configuration"
     space_id = "root"
     before_init = [
-        # "envsubst < ${local.config_path} > ./config.sub.yaml"
-        "echo Hello"
+        "envsubst < ${local.config_path} > ./config.sub.yaml"
     ]
 }
