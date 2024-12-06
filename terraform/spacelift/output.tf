c809135d6c11f5f770a876177cb0f600f7cbcd9e
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-06 20:07:17 +0000

    [WIP] Spacelift

--- terraform/spacelift/output.tf
+++ terraform/spacelift/output.tf
@@ -5,20 +5,20 @@ output "_validate_results" {
 
 output "validate_debug" {
   # value = var.FLAG_DEPLOY >= 1 ? data.external.validate[0].result["debug"] : null
-  value = var.FLAG_DEPLOY >= 1 ? data.external.validate[0].result["debug"]
+  value = data.external.validate[0].result["debug"]
 }
 
 output "validate_config_path" {
   # value = var.FLAG_DEPLOY >= 1 ? data.external.validate[0].result["config_path"] : null
-  value = var.FLAG_DEPLOY >= 1 ? data.external.validate[0].result["config_path"]
+  value = data.external.validate[0].result["config_path"]
 }
 
 output "validate_yaml" {
   # value = var.FLAG_DEPLOY >= 1 ? data.external.validate[0].result["yaml"] : null
-  value = var.FLAG_DEPLOY >= 1 ? data.external.validate[0].result["yaml"]
+  value = data.external.validate[0].result["yaml"]
 }
 
 output "validate_private_key" {
   # value = var.FLAG_DEPLOY >= 1 ? data.external.validate[0].result["private_key"] : null
-  value = var.FLAG_DEPLOY >= 1 ? data.external.validate[0].result["private_key"]
+  value = data.external.validate[0].result["private_key"]
 }
