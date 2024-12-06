1c8e6ca0537d0405c8a06716644841c7ff83573b
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-06 19:55:05 +0000

    [WIP] Spacelift

--- terraform/spacelift/stack.tf
+++ terraform/spacelift/stack.tf
@@ -7,7 +7,7 @@ resource "spacelift_stack" "proxmox" {
     name = "proxmox"
     project_root = "terraform/proxmox"
     repository = local.github.repository
-    runner_image = local.github.runner_image
+    runner_image = local.spacelift.runner_image
     terraform_version = "1.5.7"
     labels = ["proxmox"]
     additional_project_globs = [ 
