e396539fb0df46664129d64618c4d88a234f2a21
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-06 20:21:17 +0000

    [WIP] Spacelift

--- terraform/spacelift/context_attachment.tf
+++ terraform/spacelift/context_attachment.tf
@@ -1,12 +1,14 @@
 resource "spacelift_context_attachment" "config_spacelift" {
-    depends_on = [spacelift_context.config]
+    depends_on = [
+        spacelift_context.config,
+        data.spacelift_stack.spacelift    
+    ]
     context_id = spacelift_context.config.id
     stack_id   = data.spacelift_stack.spacelift.id
     priority   = 0
 }
 
 resource "spacelift_context_attachment" "config_proxmox" {
-    count = var.FLAG_VALID_CONFIG >=1 ? 1 : 0
     depends_on = [
         spacelift_context.config,
         spacelift_stack.proxmox
