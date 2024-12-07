0c1533e34d4b87d07a3a7106986c73ce79541849
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-06 21:25:32 -0500

    [WIP] Proxmox

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -8,7 +8,7 @@ resource "proxmox_virtual_environment_file" "cloud_config" {
         data = templatefile("${path.module}/file/cloud-config.yaml.tpl", {
             username = local.config.data.default.username
             ssh_public_key = [for key in data.local_file.ssh_public_key : trimspace(key.content)]
-            truenas = local.config.data.truenas
+            # truenas = local.config.data.truenas
             # iscsi = file("${path.module}/file/iscsi.sh")
         })
     }
