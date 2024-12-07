a97bba068239b573ee8d46cb0a8fa98f104cbcd5
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-07 00:18:01 -0500

    [WIP] Spacelift

--- terraform/spacelift/context.tf
+++ terraform/spacelift/context.tf
@@ -5,6 +5,5 @@ resource "spacelift_context" "config" {
     before_init = [
         "envsubst < ${local.config.path} > ./config.sub.yaml",
         "chmod 600 ${local.default.private_key}",
-        # "ssh-keyscan -p ${local.config.data.development.port.external} ${local.config.data.default.ip_address} >> ~/.ssh/known_hosts"
     ]
 }
