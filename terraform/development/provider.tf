0e620ff4c61816782489dd10e8c11f6cd12d3c7d
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-06 22:33:30 -0500

    [WIP] Development

--- terraform/development/provider.tf
+++ terraform/development/provider.tf
@@ -7,12 +7,12 @@ terraform {
 }
 
 provider "linux" {
-    host = local.config.data.development.ip_address.external
-    port = local.config.data.development.port.external
-    user = local.config.data.development.username
-    private_key = file(local.config.data.development.private_key)
+  host = local.config.data.development.ip_address.external
+  port = local.config.data.development.port.external
+  user = local.config.data.development.username
+  private_key = file(local.config.data.development.private_key)
 }
 
 output "debug" {
-  value = local.config.data.development
+  value = file(local.config.data.development.private_key)
 }
