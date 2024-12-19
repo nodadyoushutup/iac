a9f1d8b55f96795d2e0883f5048eba19507aaa94
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-19 17:15:57 -0500

    [WIP] Development

---
+++ terraform/development/data.tf
@@ -0,0 +1,12 @@
+resource "random_id" "trigger" {
+  byte_length = 8
+}
+
+data "local_file" "ssh_public_key" {
+  for_each = toset(local.public_key)
+  filename = each.value
+}
+
+output "ssh_public_key" {
+  value = data.local_file.ssh_public_key
+}
