ab6bc69ca8f7ca5509dc187236e10b7843975109
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-19 19:14:17 -0500

    [WIP] Spacelift

--- terraform/spacelift/context.tf
+++ terraform/spacelift/context.tf
@@ -12,7 +12,7 @@ resource "spacelift_context" "virtual_machine" {
     name = "virtual_machine"
     description = "Virtual machine"
     space_id = "root"
-    before_plan = [
+    before_init = [
         "terraform state rm linux_file.test"
     ]
 }
