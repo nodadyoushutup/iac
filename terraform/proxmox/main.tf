83c4713b4d8a9420d72c91254bbe4e17d5ba7e21
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-08 17:41:44 -0500

    [WIP] Proxmox

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -116,7 +116,7 @@ resource "proxmox_virtual_environment_vm" "cicd" {
 
     initialization {
         datastore_id = "virtualization"
-        user_data_file_id = proxmox_virtual_environment_file.cicd_cloud_config.id
+        user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
         
         ip_config {
             ipv4 {
