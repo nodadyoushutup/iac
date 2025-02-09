b0b205d03f59be94db128bff45473a91537ecd17
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-09 02:04:56 -0500

    [WIP] Proxmox

--- terraform/proxmox/variable.tf
+++ terraform/proxmox/variable.tf
@@ -1,54 +1,54 @@
-variable "SSH_PRIVATE_KEY" {
-  type = string
-  default = null
-}
+# variable "SSH_PRIVATE_KEY" {
+#   type = string
+#   default = null
+# }
 
-variable "PROXMOX_VE_ENDPOINT" {
-  type = string
-  default = null
-}
+# variable "PROXMOX_VE_ENDPOINT" {
+#   type = string
+#   default = null
+# }
 
-variable "PROXMOX_VE_PASSWORD" {
-  type = string
-  default = null
-}
+# variable "PROXMOX_VE_PASSWORD" {
+#   type = string
+#   default = null
+# }
 
-variable "PROXMOX_VE_USERNAME" {
-  type = string
-  default = null
-}
+# variable "PROXMOX_VE_USERNAME" {
+#   type = string
+#   default = null
+# }
 
-variable "PROXMOX_VE_SSH_USERNAME" {
-  type = string
-  default = null
-}
+# variable "PROXMOX_VE_SSH_USERNAME" {
+#   type = string
+#   default = null
+# }
 
-variable "PROXMOX_VE_SSH_NODE_ADDRESS" {
-  type = string
-  default = null
-}
+# variable "PROXMOX_VE_SSH_NODE_ADDRESS" {
+#   type = string
+#   default = null
+# }
 
-variable "PROXMOX_VE_SSH_NODE_NAME" {
-  type = string
-  default = null
-}
+# variable "PROXMOX_VE_SSH_NODE_NAME" {
+#   type = string
+#   default = null
+# }
 
-variable "VIRTUAL_MACHINE_USERNAME" {
-  type = string
-  default = null
-}
+# variable "VIRTUAL_MACHINE_USERNAME" {
+#   type = string
+#   default = null
+# }
 
-variable "NAS_LOCAL_IP" {
-  type = string
-  default = null
-}
+# variable "NAS_LOCAL_IP" {
+#   type = string
+#   default = null
+# }
 
-variable "NAS_NFS_MEDIA" {
-  type = string
-  default = null
-}
+# variable "NAS_NFS_MEDIA" {
+#   type = string
+#   default = null
+# }
 
-variable "GITHUB_USERNAME" {
-  type = string
-  default = null
-}
+# variable "GITHUB_USERNAME" {
+#   type = string
+#   default = null
+# }
