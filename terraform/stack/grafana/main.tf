94504bb5473c738db1174518ab6a73be41bd0ad4
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-09 17:23:47 -0500

    [WIP] Grafana

--- terraform/stack/grafana/main.tf
+++ terraform/stack/grafana/main.tf
@@ -6,22 +6,22 @@ resource "docker_volume" "grafana" {
     name = "grafana"
 }
 
-# resource "docker_container" "grafana" {
-#     depends_on = [
-#         docker_image.grafana
-#     ]
-#     name  = "grafana"
-#     image = docker_image.grafana.image_id
-#     restart = "unless-stopped"
-#     privileged = true
+resource "docker_container" "grafana" {
+    depends_on = [
+        docker_image.grafana
+    ]
+    name  = "grafana"
+    image = docker_image.grafana.image_id
+    restart = "unless-stopped"
+    privileged = true
     
-#     ports {
-#         internal = "3000"
-#         external = "3000"
-#     }
+    ports {
+        internal = "3000"
+        external = "3000"
+    }
 
-#     volumes {
-#         host_path = "/home/${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}/prometheus.yml"
-#         container_path = "/etc/prometheus/prometheus.yml"
-#     }
-# }
+    volumes {
+        volume_name = docker_volume.grafana.name
+        container_path = "/var/lib/grafana"
+    }
+}
