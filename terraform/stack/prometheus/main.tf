1329e8e78703cb71913bbdea19a512ab4ad2fa44
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-09 16:25:50 -0500

    [WIP] Prometheus

--- terraform/stack/prometheus/main.tf
+++ terraform/stack/prometheus/main.tf
@@ -1,5 +1,26 @@
+resource "null_resource" "exec_prometheus" {
+  triggers = {
+    always_run = timestamp()
+  }
+
+  connection {
+    type = local.exec.connection.docker.type
+    user = local.exec.connection.docker.user
+    private_key = local.exec.connection.docker.private_key
+    host = local.exec.connection.docker.host
+    port = local.exec.connection.docker.port
+  }
+
+  provisioner "remote-exec" {
+    inline = concat(
+      local.exec.inline.prometheus
+    )
+  }
+}
+
 resource "docker_image" "prometheus" {
-    name = "prom/prometheus:v3.1.0"
+  depends_on = [null_resource.exec_prometheus]
+  name = "prom/prometheus:v3.1.0"
 }
 
 resource "docker_container" "prometheus" {
@@ -16,8 +37,8 @@ resource "docker_container" "prometheus" {
   }
 
   volumes {
-    host_path = "/var/run/docker.sock"
-    container_path = "/var/run/docker.sock"
+    host_path = "./prometheus.yml"
+    container_path = "/etc/prometheus/prometheus.yml"
   }
 
 #   healthcheck {
