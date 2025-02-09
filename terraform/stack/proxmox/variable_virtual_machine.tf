a6924c6cce2e0a3bdb15e8ce8e787ce78b5ed309
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-09 13:11:23 -0500

    [WIP] Proxmox

--- terraform/stack/proxmox/variable_virtual_machine.tf
+++ terraform/stack/proxmox/variable_virtual_machine.tf
@@ -42,7 +42,7 @@ variable "VIRTUAL_MACHINE_DOCKER_IP_ADDRESS" {
 }
 
 variable "VIRTUAL_MACHINE_DOCKER_MAC_ADDRESS" {
-  type = number
+  type = string
   default = null
 }
 
@@ -73,7 +73,7 @@ variable "VIRTUAL_MACHINE_DEVELOPMENT_IP_ADDRESS" {
 }
 
 variable "VIRTUAL_MACHINE_DEVELOPMENT_MAC_ADDRESS" {
-  type = number
+  type = string
   default = null
 }
 
