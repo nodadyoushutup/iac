98e75ebfd1e024f498222396339839cfb6eb8de9
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-09 19:30:33 -0500

    [WIP] Grafana

--- terraform/stack/grafana/provider.tf
+++ terraform/stack/grafana/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_providers {
+        grafana = {
+            source  = "grafana/grafana"
+            version = "3.18.3"
+        }
+    }
+}
+
 provider "grafana" {
   url  = "http://${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:3000"
   auth = "grafana:grafana"
