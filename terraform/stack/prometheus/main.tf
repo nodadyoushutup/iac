4f6ed13d07f386e20f1046f1e4aba1995950cdd8
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-09 21:22:06 -0500

    [WIP] Prometheus

--- terraform/stack/prometheus/main.tf
+++ terraform/stack/prometheus/main.tf
@@ -23,28 +23,28 @@ resource "docker_image" "prometheus" {
   name = "prom/prometheus:v3.1.0"
 }
 
-resource "docker_container" "prometheus" {
-  depends_on = [docker_image.prometheus]
-  name  = "prometheus"
-  image = docker_image.prometheus.image_id
-  restart = "unless-stopped"
-  privileged = true
-  wait = true
-  network_mode = "bridge"
+# resource "docker_container" "prometheus" {
+#   depends_on = [docker_image.prometheus]
+#   name  = "prometheus"
+#   image = docker_image.prometheus.image_id
+#   restart = "unless-stopped"
+#   privileged = true
+#   wait = true
+#   network_mode = "bridge"
   
-  ports {
-    internal = "9090"
-    external = "9090"
-  }
+#   ports {
+#     internal = "9090"
+#     external = "9090"
+#   }
 
-  volumes {
-    host_path = "/home/${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}/prometheus.yml"
-    container_path = "/etc/prometheus/prometheus.yml"
-  }
+#   volumes {
+#     host_path = "/home/${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}/prometheus.yml"
+#     container_path = "/etc/prometheus/prometheus.yml"
+#   }
 
-  healthcheck {
-    test = ["CMD", "curl", "-f", "http://${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:9090/-/healthy"]
-    interval = "5s"
-    retries = 12
-  }
-}
+#   healthcheck {
+#     test = ["CMD", "curl", "-f", "http://${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:9090/-/healthy"]
+#     interval = "5s"
+#     retries = 12
+#   }
+# }
