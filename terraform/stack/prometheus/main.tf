dfdeb86bd45a7541d478efffc9b007c36a3f1890
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-09 21:24:55 -0500

    [WIP] Prometheus

--- terraform/stack/prometheus/main.tf
+++ terraform/stack/prometheus/main.tf
@@ -43,7 +43,7 @@ resource "docker_container" "prometheus" {
   }
 
   healthcheck {
-    test = ["CMD", "curl", "-f", "http://${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:9090/-/healthy"]
+    test = ["CMD", "curl", "-f", "http://${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:9090"]
     interval = "5s"
     retries = 12
   }
