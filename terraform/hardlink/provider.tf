5b34c4d634500903c089d892315b51bf6acdc5d5
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-09 19:59:23 -0500

    [WIP] Grafana

--- terraform/hardlink/provider.tf
+++ terraform/hardlink/provider.tf
@@ -1,5 +1,8 @@
 terraform {
     required_providers {
+        proxmox = {
+            source = "bpg/proxmox"
+        }
         docker = {
             source  = "kreuzwerker/docker"
             version = "3.0.2"
@@ -9,6 +12,24 @@ terraform {
             version = "3.18.3"
         }
     }
+}
+
+provider "proxmox" {
+  endpoint = var.PROXMOX_VE_ENDPOINT
+  password = var.PROXMOX_VE_PASSWORD
+  username = var.PROXMOX_VE_USERNAME
+  random_vm_ids = true
+  insecure = true
+  ssh {
+    agent = true
+    # agent_socket = 1022
+    username = var.PROXMOX_VE_SSH_USERNAME
+    private_key = file(var.SSH_PRIVATE_KEY)
+    node {
+      name    = var.PROXMOX_VE_SSH_NODE_NAME
+      address = var.PROXMOX_VE_SSH_NODE_ADDRESS
+    }
+  }
 }
 
 provider "docker" {
