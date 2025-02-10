b96eba24e6e29a82f5b2904b837677c4f4f4d718
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-09 19:32:09 -0500

    [WIP] Grafana

--- terraform/stack/grafana/provider_docker.tf
+++ terraform/stack/grafana/provider_docker.tf
@@ -4,10 +4,19 @@ terraform {
             source  = "kreuzwerker/docker"
             version = "3.0.2"
         }
+        grafana = {
+            source  = "grafana/grafana"
+            version = "3.18.3"
+        }
     }
 }
 
 provider "docker" {
     host = "ssh://${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}@${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:22"
     ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-o", "IdentityFile=${var.SSH_PRIVATE_KEY}"]
+}
+
+provider "grafana" {
+  url  = "http://${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:3000"
+  auth = "grafana:grafana"
 }
