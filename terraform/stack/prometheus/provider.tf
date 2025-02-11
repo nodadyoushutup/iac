87eeb727d3d057ef04ef0ae1651902cac0a8b2ff
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-10 22:51:32 -0500

    Prometheus

--- terraform/stack/prometheus/provider.tf
+++ terraform/stack/prometheus/provider.tf
@@ -1,44 +1,17 @@
 terraform {
     required_providers {
-        proxmox = {
-            source = "bpg/proxmox"
-        }
         docker = {
             source  = "kreuzwerker/docker"
             version = "3.0.2"
         }
-        grafana = {
-            source  = "grafana/grafana"
-            version = "3.18.3"
-        }
     }
-}
 
-provider "proxmox" {
-  endpoint = var.PROXMOX_VE_ENDPOINT
-  password = var.PROXMOX_VE_PASSWORD
-  username = var.PROXMOX_VE_USERNAME
-  random_vm_ids = true
-  insecure = true
-  ssh {
-    agent = true
-    # agent_socket = 1022
-    username = var.PROXMOX_VE_SSH_USERNAME
-    private_key = file(var.SSH_PRIVATE_KEY)
-    node {
-      name    = var.PROXMOX_VE_SSH_NODE_NAME
-      address = var.PROXMOX_VE_SSH_NODE_ADDRESS
+    backend "s3" {
+        key = "prometheus.tfstate"
     }
-  }
 }
 
 provider "docker" {
     host = "ssh://${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}@${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:22"
     ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-o", "IdentityFile=${var.SSH_PRIVATE_KEY}"]
-}
-
-provider "grafana" {
-    url  = "http://${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:3000"
-    auth = "grafana:grafana"
-    insecure_skip_verify = true
 }
---
+++ terraform/stack/prometheus/variable.tf
@@ -0,0 +1,19 @@
+variable "SSH_PRIVATE_KEY" {
+    type = string
+    default = null
+}
+
+variable "VIRTUAL_MACHINE_GLOBAL_USERNAME" {
+    type = string
+    default = null
+}
+
+variable "VIRTUAL_MACHINE_DOCKER_IP_ADDRESS" {
+    type = string
+    default = null
+}
+
+variable "VIRTUAL_MACHINE_DEVELOPMENT_IP_ADDRESS" {
+    type = string
+    default = null
+}
