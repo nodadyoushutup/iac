52a8de234054f38a67788b425988acc2ffc8112c
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-10 22:18:25 -0500

    [WIP] Proxmox

--- terraform/stack/proxmox/provider.tf
+++ terraform/stack/proxmox/provider.tf
@@ -3,17 +3,11 @@ terraform {
         proxmox = {
             source = "bpg/proxmox"
         }
-        docker = {
-            source  = "kreuzwerker/docker"
-            version = "3.0.2"
-        }
-        grafana = {
-            source  = "grafana/grafana"
-            version = "3.18.3"
-        }
     }
 
-    backend "s3" {}
+    backend "s3" {
+        key = "proxmox.tfstate"
+    }
 }
 
 provider "proxmox" {
@@ -32,15 +26,4 @@ provider "proxmox" {
       address = var.PROXMOX_VE_SSH_NODE_ADDRESS
     }
   }
-}
-
-provider "docker" {
-    host = "ssh://${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}@${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:22"
-    ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-o", "IdentityFile=${var.SSH_PRIVATE_KEY}"]
-}
-
-provider "grafana" {
-    url  = "http://${var.VIRTUAL_MACHINE_DOCKER_IP_ADDRESS}:3000"
-    auth = "grafana:grafana"
-    insecure_skip_verify = true
 }
