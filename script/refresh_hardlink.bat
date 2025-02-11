9f8c8b41ff1dc8ce6b4e693b5ae120af1feb7566
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-10 23:19:27 -0500

    [WIP] Hardlink

---
+++ script/refresh_hardlink.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Determine the directory where this script resides
+set "SCRIPT_DIR=%~dp0"
+
+:: Set the terraform stack directory relative to the script directory
+set "STACK_DIR=%SCRIPT_DIR%..\terraform\stack"
+
+:: Check if the stack directory exists
+if not exist "%STACK_DIR%" (
+    echo Stack directory does not exist: %STACK_DIR%
+    exit /b 1
+)
+
+:: Iterate over each first-level directory in the stack directory
+for /d %%D in ("%STACK_DIR%\*") do (
+    echo Refreshing hard links in directory: %%~fD
+    call "%SCRIPT_DIR%create_hardlink.bat" "%%~fD"
+    if errorlevel 1 (
+        echo Error processing directory: %%~fD
+    )
+)
+
+echo All hard links refreshed successfully.
+exit /b 0
