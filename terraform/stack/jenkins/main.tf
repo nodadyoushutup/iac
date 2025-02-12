dbfd0b0ef642904710af3fb2824e44391aa385c6
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-12 18:10:41 -0500

    [WIP] Jenkins

--- terraform/stack/jenkins/main.tf
+++ terraform/stack/jenkins/main.tf
@@ -1,18 +1,3 @@
-locals {
-  template = {
-    pipeline = {
-      proxmox = templatefile(
-        "${path.module}/template/job.xml.tpl", 
-        {
-          SUBDIR = "terraform/stack/proxmox"
-          GIT_REPOSITORY_BRANCH = var.GIT_REPOSITORY_BRANCH
-          GIT_REPOSITORY_URL = var.GIT_REPOSITORY_URL
-        }
-      )
-    }
-  }
-}
-
 resource "jenkins_job" "proxmox" {
   name = "proxmox"
   template = local.template.pipeline.proxmox
