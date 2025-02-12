9abb7cbebe67cad2302894815e3cff30923e3108
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-12 18:15:26 -0500

    [WIP] Jenkins

--- terraform/stack/jenkins/local.tf
+++ terraform/stack/jenkins/local.tf
@@ -1,6 +1,38 @@
 locals {
   template = {
     pipeline = {
+      cadvisor = templatefile(
+        "${path.module}/template/job.xml.tpl", 
+        {
+          SUBDIR = "terraform/stack/cadvisor"
+          GIT_REPOSITORY_BRANCH = var.GIT_REPOSITORY_BRANCH
+          GIT_REPOSITORY_URL = var.GIT_REPOSITORY_URL
+        }
+      )
+      dozzle = templatefile(
+        "${path.module}/template/job.xml.tpl", 
+        {
+          SUBDIR = "terraform/stack/dozzle"
+          GIT_REPOSITORY_BRANCH = var.GIT_REPOSITORY_BRANCH
+          GIT_REPOSITORY_URL = var.GIT_REPOSITORY_URL
+        }
+      )
+      grafana = templatefile(
+        "${path.module}/template/job.xml.tpl", 
+        {
+          SUBDIR = "terraform/stack/grafana"
+          GIT_REPOSITORY_BRANCH = var.GIT_REPOSITORY_BRANCH
+          GIT_REPOSITORY_URL = var.GIT_REPOSITORY_URL
+        }
+      )
+      prometheus = templatefile(
+        "${path.module}/template/job.xml.tpl", 
+        {
+          SUBDIR = "terraform/stack/prometheus"
+          GIT_REPOSITORY_BRANCH = var.GIT_REPOSITORY_BRANCH
+          GIT_REPOSITORY_URL = var.GIT_REPOSITORY_URL
+        }
+      )
       proxmox = templatefile(
         "${path.module}/template/job.xml.tpl", 
         {
--- terraform/stack/jenkins/main.tf
+++ terraform/stack/jenkins/main.tf
@@ -1,3 +1,23 @@
+resource "jenkins_job" "cadvisor" {
+  name = "cadvisor"
+  template = local.template.pipeline.cadvisor
+}
+
+resource "jenkins_job" "dozzle" {
+  name = "dozzle"
+  template = local.template.pipeline.dozzle
+}
+
+resource "jenkins_job" "grafana" {
+  name = "grafana"
+  template = local.template.pipeline.grafana
+}
+
+resource "jenkins_job" "prometheus" {
+  name = "prometheus"
+  template = local.template.pipeline.prometheus
+}
+
 resource "jenkins_job" "proxmox" {
   name = "proxmox"
   template = local.template.pipeline.proxmox
