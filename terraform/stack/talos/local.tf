967c39d5e62fe0c940549446d756a6af23554458
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-13 00:40:20 -0500

    [WIP] Talos

--- terraform/stack/talos/local.tf
+++ terraform/stack/talos/local.tf
@@ -1,41 +1,41 @@
-# locals {
-#     exec = {
-#         connection = {
-#             development = {
-#                 type = "ssh"
-#                 user = var.VIRTUAL_MACHINE_GLOBAL_USERNAME
-#                 private_key = file(var.SSH_PRIVATE_KEY)
-#                 host = var.VIRTUAL_MACHINE_DEVELOPMENT_IP_ADDRESS
-#                 port = 22
-#             }
-#         }
-#         inline = {
-#             kubeconfig = [
-#                 "cat <<EOF > /tmp/config",
-#                 "${talos_cluster_kubeconfig.talos.kubeconfig_raw}",
-#                 "EOF",
-#                 "mkdir -p /home/${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}/.kube/",
-#                 "cp -p /tmp/config /home/${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}/.kube/config",
-#                 "chown ${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}:${var.VIRTUAL_MACHINE_GLOBAL_USERNAME} /home/${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}/.kube/config",
-#                 "rm -rf /tmp/config",
-#             ]
-#         }   
-#     }
+locals {
+    exec = {
+        connection = {
+            development = {
+                type = "ssh"
+                user = var.VIRTUAL_MACHINE_GLOBAL_USERNAME
+                private_key = file(var.SSH_PRIVATE_KEY)
+                host = var.VIRTUAL_MACHINE_DEVELOPMENT_IP_ADDRESS
+                port = 22
+            }
+        }
+        inline = {
+            kubeconfig = [
+                "cat <<EOF > /tmp/config",
+                "${talos_cluster_kubeconfig.talos.kubeconfig_raw}",
+                "EOF",
+                "mkdir -p /home/${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}/.kube/",
+                "cp -p /tmp/config /home/${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}/.kube/config",
+                "chown ${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}:${var.VIRTUAL_MACHINE_GLOBAL_USERNAME} /home/${var.VIRTUAL_MACHINE_GLOBAL_USERNAME}/.kube/config",
+                "rm -rf /tmp/config",
+            ]
+        }   
+    }
 
-#     node_data = {
-#         controlplanes = {
-#             for idx, cp in var.talos.controlplane : cp.ip_address => {
-#                 ip_address = cp.ip_address
-#                 hostname = format("talos-cp-%d", idx)
-#                 install_disk = "/dev/sda"
-#             }
-#         }
-#         workers = {
-#             for idx, wk in var.talos.worker : wk.ip_address => {
-#                 ip_address  = wk.ip_address
-#                 hostname = format("talos-wk-%d", idx)
-#                 install_disk = "/dev/sda"
-#             }
-#         }
-#     }
-# }
+    node_data = {
+        controlplanes = {
+            for idx, cp in var.talos.controlplane : cp.ip_address => {
+                ip_address = cp.ip_address
+                hostname = format("talos-cp-%d", idx)
+                install_disk = "/dev/sda"
+            }
+        }
+        workers = {
+            for idx, wk in var.talos.worker : wk.ip_address => {
+                ip_address  = wk.ip_address
+                hostname = format("talos-wk-%d", idx)
+                install_disk = "/dev/sda"
+            }
+        }
+    }
+}
--- terraform/stack/talos/main.tf
+++ terraform/stack/talos/main.tf
@@ -1,92 +1,92 @@
-# resource "talos_machine_secrets" "talos" {}
+resource "talos_machine_secrets" "talos" {}
 
-# data "talos_machine_configuration" "controlplane" {
-#   cluster_name     = var.cluster_name
-#   cluster_endpoint = var.cluster_endpoint
-#   machine_type     = "controlplane"
-#   machine_secrets  = talos_machine_secrets.talos.machine_secrets
-# }
+data "talos_machine_configuration" "controlplane" {
+  cluster_name     = var.cluster_name
+  cluster_endpoint = var.cluster_endpoint
+  machine_type     = "controlplane"
+  machine_secrets  = talos_machine_secrets.talos.machine_secrets
+}
 
-# data "talos_machine_configuration" "worker" {
-#   cluster_name     = var.cluster_name
-#   cluster_endpoint = var.cluster_endpoint
-#   machine_type     = "worker"
-#   machine_secrets  = talos_machine_secrets.talos.machine_secrets
-# }
+data "talos_machine_configuration" "worker" {
+  cluster_name     = var.cluster_name
+  cluster_endpoint = var.cluster_endpoint
+  machine_type     = "worker"
+  machine_secrets  = talos_machine_secrets.talos.machine_secrets
+}
 
-# data "talos_client_configuration" "talos" {
-#   cluster_name         = var.cluster_name
-#   client_configuration = talos_machine_secrets.talos.client_configuration
-#   endpoints            = [for k, v in local.node_data.controlplanes : k]
-# }
+data "talos_client_configuration" "talos" {
+  cluster_name         = var.cluster_name
+  client_configuration = talos_machine_secrets.talos.client_configuration
+  endpoints            = [for k, v in local.node_data.controlplanes : k]
+}
 
-# resource "talos_machine_configuration_apply" "controlplane" {
-#   client_configuration        = talos_machine_secrets.talos.client_configuration
-#   machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
-#   for_each                    = local.node_data.controlplanes
-#   node                        = each.key
-#   config_patches = [
-#     templatefile("${path.module}/templates/install-disk-and-hostname.yaml.tmpl", {
-#       hostname     = each.value.hostname == null ? format("%s-cp-%s", var.cluster_name, index(keys(local.node_data.controlplanes), each.key)) : each.value.hostname
-#       install_disk = each.value.install_disk
-#     }),
-#     file("${path.module}/files/cp-scheduling.yaml"),
-#   ]
-# }
+resource "talos_machine_configuration_apply" "controlplane" {
+  client_configuration        = talos_machine_secrets.talos.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
+  for_each                    = local.node_data.controlplanes
+  node                        = each.key
+  config_patches = [
+    templatefile("${path.module}/templates/install-disk-and-hostname.yaml.tmpl", {
+      hostname     = each.value.hostname == null ? format("%s-cp-%s", var.cluster_name, index(keys(local.node_data.controlplanes), each.key)) : each.value.hostname
+      install_disk = each.value.install_disk
+    }),
+    file("${path.module}/files/cp-scheduling.yaml"),
+  ]
+}
 
-# resource "talos_machine_configuration_apply" "worker" {
-#   client_configuration        = talos_machine_secrets.talos.client_configuration
-#   machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
-#   for_each                    = local.node_data.workers
-#   node                        = each.key
-#   config_patches = [
-#     templatefile("${path.module}/templates/install-disk-and-hostname.yaml.tmpl", {
-#       hostname     = each.value.hostname == null ? format("%s-worker-%s", var.cluster_name, index(keys(local.node_data.workers), each.key)) : each.value.hostname
-#       install_disk = each.value.install_disk
-#     })
-#   ]
-# }
+resource "talos_machine_configuration_apply" "worker" {
+  client_configuration        = talos_machine_secrets.talos.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
+  for_each                    = local.node_data.workers
+  node                        = each.key
+  config_patches = [
+    templatefile("${path.module}/templates/install-disk-and-hostname.yaml.tmpl", {
+      hostname     = each.value.hostname == null ? format("%s-worker-%s", var.cluster_name, index(keys(local.node_data.workers), each.key)) : each.value.hostname
+      install_disk = each.value.install_disk
+    })
+  ]
+}
 
-# resource "talos_machine_bootstrap" "talos" {
-#   depends_on = [talos_machine_configuration_apply.controlplane]
+resource "talos_machine_bootstrap" "talos" {
+  depends_on = [talos_machine_configuration_apply.controlplane]
 
-#   client_configuration = talos_machine_secrets.talos.client_configuration
-#   node                 = [for k, v in local.node_data.controlplanes : k][0]
-# }
+  client_configuration = talos_machine_secrets.talos.client_configuration
+  node                 = [for k, v in local.node_data.controlplanes : k][0]
+}
 
-# resource "talos_cluster_kubeconfig" "talos" {
-#   depends_on           = [talos_machine_bootstrap.talos]
-#   client_configuration = talos_machine_secrets.talos.client_configuration
-#   node                 = [for k, v in local.node_data.controlplanes : k][0]
-# }
+resource "talos_cluster_kubeconfig" "talos" {
+  depends_on           = [talos_machine_bootstrap.talos]
+  client_configuration = talos_machine_secrets.talos.client_configuration
+  node                 = [for k, v in local.node_data.controlplanes : k][0]
+}
 
-# resource "null_resource" "exec_development" {
-#   depends_on = [talos_cluster_kubeconfig.talos]
-#   triggers = {
-#     always_run = timestamp()
-#   }
+resource "null_resource" "exec_development" {
+  depends_on = [talos_cluster_kubeconfig.talos]
+  triggers = {
+    always_run = timestamp()
+  }
 
-#   connection {
-#     type = local.exec.connection.development.type
-#     user = local.exec.connection.development.user
-#     private_key = local.exec.connection.development.private_key
-#     host = local.exec.connection.development.host
-#     port = local.exec.connection.development.port
-#   }
+  connection {
+    type = local.exec.connection.development.type
+    user = local.exec.connection.development.user
+    private_key = local.exec.connection.development.private_key
+    host = local.exec.connection.development.host
+    port = local.exec.connection.development.port
+  }
 
-#   provisioner "remote-exec" {
-#     inline = concat(
-#         local.exec.inline.kubeconfig,
-#     )
-#   }
-# }
+  provisioner "remote-exec" {
+    inline = concat(
+        local.exec.inline.kubeconfig,
+    )
+  }
+}
 
-# data "talos_cluster_kubeconfig" "talos" {
-#     depends_on = [talos_machine_bootstrap.talos]
-#     client_configuration = talos_machine_secrets.talos.client_configuration
-#     node = "192.168.1.200"
-# }
+data "talos_cluster_kubeconfig" "talos" {
+    depends_on = [talos_machine_bootstrap.talos]
+    client_configuration = talos_machine_secrets.talos.client_configuration
+    node = "192.168.1.200"
+}
 
-# output "kubeconfig" {
-#     value = talos_cluster_kubeconfig.talos.kubernetes_client_configuration.ca_certificate
-# }
+output "kubeconfig" {
+    value = talos_cluster_kubeconfig.talos.kubernetes_client_configuration.ca_certificate
+}
