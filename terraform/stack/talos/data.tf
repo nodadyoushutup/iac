e79b43b675df2343a94c8983c758bedff623f785
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-12 22:40:40 -0500

    [WIP] Talos

--- terraform/stack/talos/data.tf
+++ terraform/stack/talos/data.tf
@@ -1,3 +1,15 @@
-data "proxmox_virtual_environment_vms" "talos" {
-  tags      = ["ubuntu"]
+data "proxmox_virtual_environment_vms" "talos_controlplane" {
+    tags = ["talos-controlplane"]
+}
+
+data "proxmox_virtual_environment_vms" "talos_worker" {
+    tags = ["talos-worker"]
+}
+
+output "talos_controlplane" {
+    value = data.proxmox_virtual_environment_vms.talos_controlplane
+}
+
+output "talos_worker" {
+    value = data.proxmox_virtual_environment_vms.talos_worker
 }
