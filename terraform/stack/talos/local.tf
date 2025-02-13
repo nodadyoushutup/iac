c0a7c2d5aed274980d8b10ce2a5336aa7b4e9948
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-12 23:17:50 -0500

    [WIP] Talos

--- terraform/stack/talos/local.tf
+++ terraform/stack/talos/local.tf
@@ -22,45 +22,16 @@ locals {
         }   
     }
 
-    talos = {
-        controlplane = [
-            {
-                ip_address = "192.168.1.200"
-                mac_address = "00:11:22:33:44:55"
-                vm_id = "1200"
-            },
-            {
-                ip_address = "192.168.1.201"
-                mac_address = "00:11:22:33:44:66"
-                vm_id = "1201"
-            },
-            {
-                ip_address = "192.168.1.202"
-                mac_address = "00:11:22:33:44:77"
-                vm_id = "1202"
-            },
-        ]
-
-        worker = [
-            {
-                ip_address = "192.168.1.203"
-                mac_address = "00:11:22:33:44:88"
-                vm_id = "1203"
-            }
-        ]
-    }
-    
-
     node_data = {
         controlplanes = {
-            for idx, cp in local.talos.controlplane : cp.ip_address => {
+            for idx, cp in var.talos.controlplane : cp.ip_address => {
                 ip_address = cp.ip_address
                 hostname = format("talos-cp-%d", idx + 1)
                 install_disk = "/dev/sda"
             }
         }
         workers = {
-            for idx, wk in local.talos.worker : wk.ip_address => {
+            for idx, wk in var.talos.worker : wk.ip_address => {
                 ip_address  = wk.ip_address
                 hostname = format("talos-wk-%d", idx + 1)
                 install_disk = "/dev/sda"
