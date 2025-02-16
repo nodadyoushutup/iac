427532d682b48155f2bfd13a7edb86d665ded1de
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-16 16:20:00 -0500

    [WIP] VM

--- terraform/module/proxmox/virtual_machine/main.tf
+++ terraform/module/proxmox/virtual_machine/main.tf
@@ -18,7 +18,7 @@ resource "proxmox_virtual_environment_file" "cloud" {
         data = templatefile(
             "${path.module}/template/cloud_config.yaml.tpl",
             {
-                hostname = var.cloud_config.hostname ? var.cloud_config.hostname : var.name
+                hostname = var.cloud_config.hostname != null ? var.cloud_config.hostname : var.name
                 username = var.cloud_config.auth.username
                 ssh_public_key = var.cloud_config.auth.ssh_public_key != [] ? var.cloud_config.auth.ssh_public_key : []
                 ssh_import = var.cloud_config.auth.github != null ? "su - ${var.cloud_config.auth.username} -c 'ssh-import-id gh:${var.cloud_config.auth.github}'" : "echo 'No SSH import'"
