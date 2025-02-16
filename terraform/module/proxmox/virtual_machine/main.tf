adb46c22d0cc74e2196bbc3e64a3d3fa612fd864
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-16 13:23:52 -0500

    [WIP] VM

--- terraform/module/proxmox/virtual_machine/main.tf
+++ terraform/module/proxmox/virtual_machine/main.tf
@@ -4,13 +4,9 @@ resource "proxmox_virtual_environment_download_file" "cloud" {
     node_name = var.image.node_name
     overwrite = true
     overwrite_unmanaged = true
-    file_name = "${var.name}-cloud-image-x86-64-jammy-0.1.13.img"
+    file_name = "${var.name}-cloud-image-x86-64-jammy-${var.image.version}.img"
     # url = "https://github.com/nodadyoushutup/cloud-image/releases/download/${var.image.version}}/cloud-image-x86-64-jammy-${var.image.version}}.img"
-    url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.13/cloud-image-x86-64-jammy-0.1.13.img"
-}
-
-output "url" {
-  value = "https://github.com/nodadyoushutup/cloud-image/releases/download/${var.image.version}}/cloud-image-x86-64-jammy-${var.image.version}}.img"
+    url = "https://github.com/nodadyoushutup/cloud-image/releases/download/${var.image.version}/cloud-image-x86-64-jammy-${var.image.version}.img"
 }
 
 resource "proxmox_virtual_environment_file" "cloud" {
