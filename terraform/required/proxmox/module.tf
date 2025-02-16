f79a1fd846a23e2a52eda1efa157eedc6ca45196
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-16 17:47:24 -0500

    [WIP] VM

--- terraform/required/proxmox/module.tf
+++ terraform/required/proxmox/module.tf
@@ -6,7 +6,7 @@ module "debug_vm_test" {
   ]
 
   image = {
-    file_name = "nocloud-amd64.raw"
+    file_name = "nocloud-amd64.img"
     # url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.13/cloud-image-x86-64-jammy-0.1.13.img"
     url = "https://github.com/nodadyoushutup/iac/releases/download/v1.9.4/nocloud-amd64.raw"
   }
