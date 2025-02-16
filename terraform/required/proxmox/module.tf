df6e1d5bead5e823516b8172f9f24555b3336fd9
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-16 15:58:05 -0500

    [WIP] VM

--- terraform/required/proxmox/module.tf
+++ terraform/required/proxmox/module.tf
@@ -9,9 +9,6 @@ module "debug_vm_test" {
     auth = {
       github = "nodadyoushutup"
       username = "nodadyoushutup"
-      ssh_public_key = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDQy28xL2wHdR2CMcvl+CY9X6uiC2C2ImuEJAcRZXAwjp2GYBhNZQ51dlu9upAo2WNjvJL/XJY8D8RK/LxLilUHikfK+krZyoMLth6u3dEXw9FLMr2Zs8fAcvmh6ZrpveN8k7T5xU0l3Y/isL/5Ud8Bk89rsvMM2zpTVtAiSJ6b2wqF1X4MonhpHiH3AXbmaPIBOMlzZ6/0xohd4DRB4s8ljdlcq4uznN7/M6h1cpIb4J3jpOADgrQ5vR8Qpc/Nsz1o9eF0+BSRYV3guyA5/CNuZZYTkcNWPONfKre/kAnA7PI9Z1U/epFGF0selhiPNpTpuDZu1nfe6RPLWsaVOJpVugapXslXUuSIMuMnnwSbGv7NmSpSy7ViJb3qI/2KoIVgOa8vV87yOJQKWTmehFgZG1tCX75QJoqdaYKkVsOaMqmmDsICgYXhCi53p8uPc8eZD5pbzjVMzLS6bMbdZA3hW+NVecgc/gH1ezO7MyL7LBGJYrIm5yW0/dVKEtu0EXU= nodadyoushutup@ubuntu"
-      ]
     }
   }
 
