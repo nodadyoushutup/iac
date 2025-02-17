52aed1a2cc1e574d25538bf655135636b767dd70
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-16 21:06:15 -0500

    [WIP] VM

--- terraform/required/proxmox/local.tf
+++ terraform/required/proxmox/local.tf
@@ -1,39 +1,39 @@
-locals {
-    template = {
-        gitconfig = templatefile(
-            "${path.module}/../proxmox/template/.gitconfig.tpl", 
-            {
-                git_gitconfig_name = var.git.gitconfig.name,
-                git_gitconfig_email = var.git.gitconfig.email
-            }
-        )
-        private_key = templatefile(
-            "${path.module}/../proxmox/template/id_rsa.tpl", 
-            {
-                id_rsa = file(var.SSH_PRIVATE_KEY)
-            }
-        )
-    }
+# locals {
+#     template = {
+#         gitconfig = templatefile(
+#             "${path.module}/../proxmox/template/.gitconfig.tpl", 
+#             {
+#                 git_gitconfig_name = var.git.gitconfig.name,
+#                 git_gitconfig_email = var.git.gitconfig.email
+#             }
+#         )
+#         private_key = templatefile(
+#             "${path.module}/../proxmox/template/id_rsa.tpl", 
+#             {
+#                 id_rsa = file(var.SSH_PRIVATE_KEY)
+#             }
+#         )
+#     }
 
-    exec = {
-        inline = {
-            gitconfig = [
-                "cat <<EOF > /tmp/.gitconfig",
-                "${local.template.gitconfig}",
-                "EOF",
-                "cp -p /tmp/.gitconfig /home/${var.machine.global.username}/.gitconfig",
-                "chown ${var.machine.global.username}:${var.machine.global.username} /home/${var.machine.global.username}/.gitconfig",
-                "rm -rf /tmp/.gitconfig",
-            ]
-            private_key = [
-                "cat <<EOF > /tmp/id_rsa",
-                "${local.template.private_key}",
-                "EOF",
-                "cp -p /tmp/id_rsa /home/${var.machine.global.username}/.ssh/id_rsa",
-                "chown ${var.machine.global.username}:${var.machine.global.username} /home/${var.machine.global.username}/.ssh/id_rsa",
-                "chmod 600 /home/${var.machine.global.username}/.ssh/id_rsa",
-                "rm -rf /tmp/id_rsa",
-            ]
-        }   
-    }
-}
+#     exec = {
+#         inline = {
+#             gitconfig = [
+#                 "cat <<EOF > /tmp/.gitconfig",
+#                 "${local.template.gitconfig}",
+#                 "EOF",
+#                 "cp -p /tmp/.gitconfig /home/${var.machine.global.username}/.gitconfig",
+#                 "chown ${var.machine.global.username}:${var.machine.global.username} /home/${var.machine.global.username}/.gitconfig",
+#                 "rm -rf /tmp/.gitconfig",
+#             ]
+#             private_key = [
+#                 "cat <<EOF > /tmp/id_rsa",
+#                 "${local.template.private_key}",
+#                 "EOF",
+#                 "cp -p /tmp/id_rsa /home/${var.machine.global.username}/.ssh/id_rsa",
+#                 "chown ${var.machine.global.username}:${var.machine.global.username} /home/${var.machine.global.username}/.ssh/id_rsa",
+#                 "chmod 600 /home/${var.machine.global.username}/.ssh/id_rsa",
+#                 "rm -rf /tmp/id_rsa",
+#             ]
+#         }   
+#     }
+# }
