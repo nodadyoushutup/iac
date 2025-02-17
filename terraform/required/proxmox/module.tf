a5bdb302106ca6066d28f37d09a9da3a290351c3
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-16 19:19:01 -0500

    [WIP] VM

--- terraform/required/proxmox/module.tf
+++ terraform/required/proxmox/module.tf
@@ -1,9 +1,5 @@
 module "debug_vm_test" {
   source = "../../module/proxmox/virtual_machine"
-  depends_on = [
-    proxmox_virtual_environment_download_file.cloud,
-    proxmox_virtual_environment_file.cloud_config
-  ]
 
   image = {
     file_name = "test.img"
