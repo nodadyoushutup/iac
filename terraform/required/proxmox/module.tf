3d538cf39a7b4ac4557ff8fb2db43716802acec4
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-16 19:04:06 -0500

    [WIP] VM

--- terraform/required/proxmox/module.tf
+++ terraform/required/proxmox/module.tf
@@ -6,7 +6,7 @@ module "debug_vm_test" {
   ]
 
   image = {
-    file = "test.img"
+    file_name = "test.img"
     url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.13/cloud-image-x86-64-jammy-0.1.13.img"
     # url = "https://github.com/nodadyoushutup/iac/releases/download/talos-0.1.3/talos-image-amd64-0.1.3.img"
   }
