61d12686af005fdc85f2acc76baf4007c877fea6
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-16 20:27:10 -0500

    [WIP] VM

--- terraform/required/proxmox/module.tf
+++ terraform/required/proxmox/module.tf
@@ -1,22 +1,42 @@
 locals {
-  machines = [
+  controlplane = [
     {
-      name           = "talos-cp-0"
+      name = "talos-cp-0"
+      image = {
+        url = "https://github.com/nodadyoushutup/iac/releases/download/talos-0.1.3/talos-image-amd64-0.1.3.img"
+      }
       initialization = {
         ip_config = {
           ipv4 = {
-            address = "192.168.1.190"
+            address = "192.168.1.200"
             gateway = "192.168.1.1"
           }
         }
       }
     },
     {
-      name           = "talos-cp-1"
+      name = "talos-cp-1"
+      image = {
+        url = "https://github.com/nodadyoushutup/iac/releases/download/talos-0.1.3/talos-image-amd64-0.1.3.img"
+      }
       initialization = {
         ip_config = {
           ipv4 = {
-            address = "192.168.1.191"
+            address = "192.168.1.201"
+            gateway = "192.168.1.1"
+          }
+        }
+      }
+    },
+    {
+      name = "talos-cp-2"
+      image = {
+        url = "https://github.com/nodadyoushutup/iac/releases/download/talos-0.1.3/talos-image-amd64-0.1.3.img"
+      }
+      initialization = {
+        ip_config = {
+          ipv4 = {
+            address = "192.168.1.202"
             gateway = "192.168.1.1"
           }
         }
@@ -29,25 +49,9 @@ module "debug_vm_test" {
   source = "../../module/proxmox/virtual_machine"
 
   # Turn the list into a map keyed by name
-  for_each = { for machine in local.machines : machine.name => machine }
+  for_each = { for machine in local.controlplane : machine.name => machine }
 
-  # Example image configuration
-  image = {
-    # url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.13/cloud-image-x86-64-jammy-0.1.13.img"
-    url = "https://github.com/nodadyoushutup/iac/releases/download/talos-0.1.3/talos-image-amd64-0.1.3.img"
-  }
-
-  # Example cloud-config
-  cloud_config = {
-    auth = {
-      github   = "nodadyoushutup"
-      username = "nodadyoushutup"
-    }
-  }
-
-  # Pull in initialization (IP, gateway, etc.) from each machine
+  image = each.value.image
   initialization = each.value.initialization
-
-  # Give each VM its `name`
   name = each.value.name
 }
