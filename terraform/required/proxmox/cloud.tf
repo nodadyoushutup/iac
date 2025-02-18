465c4ab5f19b9c2d705f856ca900b531a2420e00
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-17 19:39:40 -0500

    [WIP] Proxmox

--- terraform/required/proxmox/cloud.tf
+++ terraform/required/proxmox/cloud.tf
@@ -3,10 +3,10 @@ module "cloud_required" {
   for_each = { for machine in var.machine.cloud.required : machine.name => machine }
 
   cloud_config = each.value.cloud_config.auth.github != null ? each.value.cloud_config : var.machine.global.cloud_config
-  image = try(each.value.image, false) ? each.value.image : {}
-  initialization = try(each.value.initialization, false) ? each.value.initialization : {}
+  image = try(each.value.image, null) != null ? each.value.image : {}
+  initialization = try(each.value.initialization, null) != null ? each.value.initialization : {}
   name = each.value.name
-  vm_id = try(each.value.vm_id, false) ? each.value.vm_id : null
+  vm_id = try(each.value.vm_id, null) != null ? each.value.vm_id : null
 }
 
 # module "cloud_custom" {
