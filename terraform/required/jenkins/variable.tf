669df78ad673fb405768a3f59cdb1b6dd52abb48
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-17 21:33:07 -0500

    [WIP] Jenkins

--- terraform/required/jenkins/variable.tf
+++ terraform/required/jenkins/variable.tf
@@ -324,25 +324,25 @@ variable "skip_requesting_account_id" {
     type = bool
 }
 
-# provider "aws" {
-#     region = var.region
-#     access_key = var.access_key
-#     secret_key = var.secret_key
-#     skip_credentials_validation = var.skip_credentials_validation
-#     skip_metadata_api_check = var.skip_metadata_api_check
-#     skip_region_validation = var.skip_region_validation
-#     skip_requesting_account_id = var.skip_requesting_account_id
+provider "aws" {
+    region = var.region
+    access_key = var.access_key
+    secret_key = var.secret_key
+    skip_credentials_validation = var.skip_credentials_validation
+    skip_metadata_api_check = var.skip_metadata_api_check
+    skip_region_validation = var.skip_region_validation
+    skip_requesting_account_id = var.skip_requesting_account_id
+    
+    endpoints {
+        s3 = var.endpoints.s3
+    }
+}
 
-#     endpoints {
-#         s3 = var.endpoints.s3
-#     }
-# }
+data "aws_s3_object" "secret" {
+    bucket = "terraform"
+    key = "secret.tfvars"
+}
 
-# data "aws_s3_object" "secret" {
-#     bucket = "terraform"
-#     key = "secret.tfvars"
-# }
-
-# output "secrets_content" {
-#     value = data.aws_s3_object.secret.body
-# }
+output "secrets_content" {
+    value = data.aws_s3_object.secret.body
+}
