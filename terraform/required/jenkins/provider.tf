33396bfdb994f5ccddddd4607cc07467f137def4
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-17 22:52:04 -0500

    [WIP] Jenkins

--- terraform/required/jenkins/provider.tf
+++ terraform/required/jenkins/provider.tf
@@ -11,6 +11,15 @@ terraform {
     }
 }
 
-provider "jenkins" {
-    server_url = jsondecode(data.aws_s3_object.config.body).machine.clickops.initialization.ip_config.ipv4.address
+locals {
+    # server_url = jsondecode(data.aws_s3_object.config.body).machine.clickops.initialization.ip_config.ipv4.address
+    config = jsondecode(data.aws_s3_object.config.body)
 }
+
+output "local" {
+    value = local.config
+}
+
+# provider "jenkins" {
+#     server_url = local.server_url
+# }
