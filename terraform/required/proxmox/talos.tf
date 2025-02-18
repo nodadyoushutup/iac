5931c21816feb37a25980fe25f28c3180ac342c2
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-17 22:27:31 -0500

    [WIP] Config

--- terraform/required/proxmox/talos.tf
+++ terraform/required/proxmox/talos.tf
@@ -1,19 +1,19 @@
-module "talos_controlplane" {
-  source = "../../module/proxmox/virtual_machine"
-  for_each = { for machine in local.config.machine.talos.controlplane : machine.name => machine }
+# module "talos_controlplane" {
+#   source = "../../module/proxmox/virtual_machine"
+#   for_each = { for machine in local.config.machine.talos.controlplane : machine.name => machine }
 
-  image = each.value.image
-  initialization = each.value.initialization
-  name = each.value.name
-  vm_id = each.value.vm_id
-}
+#   image = each.value.image
+#   initialization = each.value.initialization
+#   name = each.value.name
+#   vm_id = each.value.vm_id
+# }
 
-module "talos_worker" {
-  source = "../../module/proxmox/virtual_machine"
-  for_each = { for machine in local.config.machine.talos.worker : machine.name => machine }
+# module "talos_worker" {
+#   source = "../../module/proxmox/virtual_machine"
+#   for_each = { for machine in local.config.machine.talos.worker : machine.name => machine }
 
-  image = each.value.image
-  initialization = each.value.initialization
-  name = each.value.name
-  vm_id = each.value.vm_id
-}
+#   image = each.value.image
+#   initialization = each.value.initialization
+#   name = each.value.name
+#   vm_id = each.value.vm_id
+# }
