2940cd175ed18ac01409508e2b4a70f9c36d5705
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-17 21:09:59 -0500

    [WIP] Jenkins

--- terraform/required/dozzle/variable.tf
+++ terraform/required/dozzle/variable.tf
@@ -272,3 +272,54 @@ variable "machine" {
     })
 }
 
+variable "bucket" {
+    description = "MinIO bucket"
+    type = string
+}
+
+variable "endpoints" {
+    description = "MinIO endpoint"
+    type = object({
+        s3 = string
+    })
+}
+
+variable "access_key" {
+    description = "MinIO access key"
+    type = string
+}
+
+variable "secret_key" {
+    description = "MinIO access key"
+    type = string
+}
+
+variable "region" {
+    description = "MinIO access key"
+    type = string
+}
+
+variable "skip_credentials_validation" {
+    description = "MinIO access key"
+    type = bool
+}
+
+variable "skip_metadata_api_check" {
+    description = "MinIO access key"
+    type = bool
+}
+
+variable "skip_region_validation" {
+    description = "MinIO access key"
+    type = bool
+}
+
+variable "use_path_style" {
+    description = "MinIO access key"
+    type = bool
+}
+
+variable "skip_requesting_account_id" {
+    description = "MinIO access key"
+    type = bool
+}
