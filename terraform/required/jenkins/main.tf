c41fc71a563f498aadedf45822746deffed5adf5
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-17 23:10:40 -0500

    [WIP] Jenkins

--- terraform/required/jenkins/main.tf
+++ terraform/required/jenkins/main.tf
@@ -1,20 +1,20 @@
-# resource "jenkins_folder" "required" {
-#   name = "required"
-#   description = "Required terraform for base framework operations"
-# }
+resource "jenkins_folder" "required" {
+  name = "required"
+  description = "Required terraform for base framework operations"
+}
 
-# resource "jenkins_job" "required" {
-#   for_each = toset(local.directory.required)
+resource "jenkins_job" "required" {
+  for_each = toset(local.directory.required)
 
-#   folder = jenkins_folder.required.id
-#   name = each.value
-#   template = templatefile(
-#     "${path.module}/template/job.xml.tpl", 
-#     {
-#       subdir = "terraform/required/${each.value}"
-#       git_repository_branch = var.git.repository.branch
-#       git_repository_url = var.git.repository.url
-#       script_path = "terraform/required/jenkins/pipeline/terraform.jenkins"
-#     }
-#   )
-# }
+  folder = jenkins_folder.required.id
+  name = each.value
+  template = templatefile(
+    "${path.module}/template/job.xml.tpl", 
+    {
+      subdir = "terraform/required/${each.value}"
+      git_repository_branch = var.git.repository.branch
+      git_repository_url = var.git.repository.url
+      script_path = "terraform/required/jenkins/pipeline/terraform.jenkins"
+    }
+  )
+}
