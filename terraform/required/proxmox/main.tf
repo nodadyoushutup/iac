5dc8fb1614ad833cba81edaa1113dd96e2df72a6
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-19 00:29:03 -0500

    [WIP] Jenkins

---
+++ terraform/required/proxmox/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {
+    key = "proxmox.tfstate"
+  }
+}
+
+data "terraform_remote_state" "config" {
+  backend = "s3"
+
+  config = {
+    bucket                      = "config"
+    key                         = "config.tfstate"
+    region                      = "us-east-1"
+    access_key                  = var.access_key
+    secret_key                  = var.secret_key
+    endpoint                    = var.endpoints.s3
+    skip_credentials_validation = var.skip_credentials_validation
+    skip_metadata_api_check     = var.skip_metadata_api_check
+    skip_region_validation      = var.skip_region_validation
+    skip_requesting_account_id  = var.skip_requesting_account_id
+    use_path_style              = var.use_path_style
+  }
+}
+
+output "previous_config" {
+  value = data.terraform_remote_state.config.outputs.config
+}
