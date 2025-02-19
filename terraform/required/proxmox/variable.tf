b29f61583ac1421e2727be87c74b6839dd9de0f3
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-19 00:10:42 -0500

    [WIP] Jenkins

--- terraform/required/proxmox/variable.tf
+++ terraform/required/proxmox/variable.tf
@@ -255,54 +255,54 @@ variable "ssh_private_key" {
 #     })
 # }
 
-# variable "bucket" {
-#     description = "MinIO bucket"
-#     type = string
-# }
+variable "bucket" {
+    description = "MinIO bucket"
+    type = string
+}
 
-# variable "endpoints" {
-#     description = "MinIO endpoint"
-#     type = object({
-#         s3 = string
-#     })
-# }
+variable "endpoints" {
+    description = "MinIO endpoint"
+    type = object({
+        s3 = string
+    })
+}
 
-# variable "access_key" {
-#     description = "MinIO access key"
-#     type = string
-# }
+variable "access_key" {
+    description = "MinIO access key"
+    type = string
+}
 
-# variable "secret_key" {
-#     description = "MinIO access key"
-#     type = string
-# }
+variable "secret_key" {
+    description = "MinIO access key"
+    type = string
+}
 
-# variable "region" {
-#     description = "MinIO access key"
-#     type = string
-# }
+variable "region" {
+    description = "MinIO access key"
+    type = string
+}
 
-# variable "skip_credentials_validation" {
-#     description = "MinIO access key"
-#     type = bool
-# }
+variable "skip_credentials_validation" {
+    description = "MinIO access key"
+    type = bool
+}
 
-# variable "skip_metadata_api_check" {
-#     description = "MinIO access key"
-#     type = bool
-# }
+variable "skip_metadata_api_check" {
+    description = "MinIO access key"
+    type = bool
+}
 
-# variable "skip_region_validation" {
-#     description = "MinIO access key"
-#     type = bool
-# }
+variable "skip_region_validation" {
+    description = "MinIO access key"
+    type = bool
+}
 
-# variable "use_path_style" {
-#     description = "MinIO access key"
-#     type = bool
-# }
+variable "use_path_style" {
+    description = "MinIO access key"
+    type = bool
+}
 
-# variable "skip_requesting_account_id" {
-#     description = "MinIO access key"
-#     type = bool
-# }
+variable "skip_requesting_account_id" {
+    description = "MinIO access key"
+    type = bool
+}
