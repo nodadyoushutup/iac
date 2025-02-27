5bda795aadfe40a5cb392fcce927bdf23ca2028c
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-26 21:10:07 -0500

    IAC-11 [WIP] Password

--- terraform/config/script/sanitize.py
+++ terraform/config/script/sanitize.py
@@ -43,11 +43,9 @@ def main():
 
     # Sanitize the entire configuration.
     sanitized = sanitize(config, sensitive_keys, sensitive_strings)
-    result = {"data": sanitized}
 
     # Output the sanitized config.
-    # print(json.dumps({"data": json.dumps(sanitized)}))
-    print(json.dumps(result))
+    print(json.dumps({"data": json.dumps(sanitized)}))
 
 if __name__ == '__main__':
     main()
