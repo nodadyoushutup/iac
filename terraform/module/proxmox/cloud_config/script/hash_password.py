f7e976944cae7a956b07fa842387230645ea0f9d
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-26 20:39:06 -0500

    IAC-11 [WIP] Password

--- terraform/module/proxmox/cloud_config/script/hash_password.py
+++ terraform/module/proxmox/cloud_config/script/hash_password.py
@@ -3,12 +3,11 @@ import json
 from passlib.hash import sha512_crypt
 
 def main():
-    if len(sys.argv) < 2:
-        sys.exit(1)
-    
-    password = sys.argv[1]
-    
     try:
+        input_data = json.load(sys.stdin)
+        password = input_data.get("password", "")
+        if not password:
+            raise ValueError("Password not provided")
         hashed_pw = sha512_crypt.hash(password)
         output = {"data": hashed_pw}
     except Exception as e:
