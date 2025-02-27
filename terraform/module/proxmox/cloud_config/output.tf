0f9cf7c2438bf8580f387f385c75c83552feaade
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-26 19:33:35 -0500

    IAC-11 [WIP] Template update

---
+++ terraform/module/proxmox/cloud_config/output.tf
@@ -0,0 +1,35 @@
+output "name" {
+    value = local.name
+}
+
+output "node_name" {
+    value = local.node_name_computed
+}
+
+output "username" {
+    value = local.username_computed
+}
+
+output "address" {
+    value = local.address_computed
+}
+
+output "overwrite" {
+    value = local.overwrite_computed
+}
+
+output "github" {
+    value = local.github_computed
+}
+
+output "gateway" {
+    value = local.gateway_computed
+}
+
+output "type" {
+    value = local.type
+}
+
+output "template" {
+    value = local.template
+}
