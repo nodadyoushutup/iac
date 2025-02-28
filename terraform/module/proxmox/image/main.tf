9fc4f565b4a00fa625478043f7a67d8b64304ce2
nodadyoushutup <admin@nodadyoushutup.com>
2025-02-27 20:44:52 -0500

    IAC-10 [WIP] Image

--- terraform/module/proxmox/image/main.tf
+++ terraform/module/proxmox/image/main.tf
@@ -1,7 +1,15 @@
-
+resource "proxmox_virtual_environment_download_file" "image" {
+    content_type = "iso"
+    datastore_id = "local"
+    file_name = "cloud-image.img"
+    node_name = "pve"
+    url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.13/cloud-image-x86-64-jammy-0.1.13.img"
+    checksum = "58fafa456675eabcb995b5bca12af4edf96959966d0086c4979c5107f3fe11c3"
+    checksum_algorithm = "sha256"
+}
 
 
 
 output "debug" {
-    value = var.config
+    value = proxmox_virtual_environment_download_file.image.id
 }
