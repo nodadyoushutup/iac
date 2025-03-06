ba53f0fd051d109fce294ecdf6662e53e0b2fa9e
nodadyoushutup <admin@nodadyoushutup.com>
2025-03-05 19:12:56 -0500

    IAC-14 [WIP] VM

--- terraform/module/proxmox/virtual_machine/main.tf
+++ terraform/module/proxmox/virtual_machine/main.tf
@@ -53,15 +53,15 @@ resource "proxmox_virtual_environment_vm" "virtual_machine" {
     boot_order = local.virtual_machine.boot_order_computed
 
     cpu {
-        affinity = local.virtual_machine.cpu_computed.affinity_computed
-        cores = local.virtual_machine.cpu_computed.cores_computed
-        flags = local.virtual_machine.cpu_computed.flags_computed
-        hotplugged = local.virtual_machine.cpu_computed.hotplugged_computed
-        limit = local.virtual_machine.cpu_computed.limit_computed
-        numa = local.virtual_machine.cpu_computed.numa_computed
-        sockets = local.virtual_machine.cpu_computed.sockets_computed
-        type = local.virtual_machine.cpu_computed.type_computed
-        units = local.virtual_machine.cpu_computed.units_computed
+        affinity = local.virtual_machine.cpu_computed.affinity
+        cores = local.virtual_machine.cpu_computed.cores
+        flags = local.virtual_machine.cpu_computed.flags
+        hotplugged = local.virtual_machine.cpu_computed.hotplugged
+        limit = local.virtual_machine.cpu_computed.limit
+        numa = local.virtual_machine.cpu_computed.numa
+        sockets = local.virtual_machine.cpu_computed.sockets
+        type = local.virtual_machine.cpu_computed.type
+        units = local.virtual_machine.cpu_computed.units
     }
 
 
