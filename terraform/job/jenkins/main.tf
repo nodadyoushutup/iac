45325399998987d85f1139acbb7c81a18e66f411
nodadyoushutup <admin@nodadyoushutup.com>
2025-03-09 19:22:11 -0400

    IAC-14 [WIP] VM

--- terraform/job/jenkins/main.tf
+++ terraform/job/jenkins/main.tf
@@ -1,31 +1,32 @@
-resource "jenkins_folder" "framework" {
-  name = "framework"
-  description = "Required terraform"
-}
+# resource "jenkins_folder" "framework" {
+#   name = "framework"
+#   description = "Required terraform"
+# }
 
-resource "jenkins_folder" "custom" {
-  name = "custom"
-  description = "Custom terraform"
-}
+# resource "jenkins_folder" "custom" {
+#   name = "custom"
+#   description = "Custom terraform"
+# }
 
-module "job" {
-  depends_on = [
-    jenkins_folder.framework, 
-    jenkins_folder.custom
-  ]
+# module "job" {
+#   depends_on = [
+#     jenkins_folder.framework, 
+#     jenkins_folder.custom
+#   ]
   
-  for_each = try(local.config.jenkins.job, null) != null ? local.config.jenkins.job : {}
-  source = "../../module/jenkins/job"
+#   for_each = try(local.config.jenkins.job, null) != null ? local.config.jenkins.job : {}
+#   source = "../../module/jenkins/job"
 
-  config = local.config # Required
-  name = try(each.key, null)
-  folder = try(each.value.folder, null)
-  discard_build = try(each.value.discard_build, null)
-  path = try(each.value.path, null)
-  parameter = try(each.value.parameter, null)
-  git_repository = try(each.value.git_repository, null)
-}
+#   config = local.config # Required
+#   name = try(each.key, null)
+#   folder = try(each.value.folder, null)
+#   discard_build = try(each.value.discard_build, null)
+#   path = try(each.value.path, null)
+#   parameter = try(each.value.parameter, null)
+#   git_repository = try(each.value.git_repository, null)
+# }
 
 output "debug" {
-  value = module.job.debug
+  # value = module.job.debug
+  value = local.config
 }
