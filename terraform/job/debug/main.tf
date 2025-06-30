4b611c1daaa97ba8d1a5723b53bc313c30bae913
nodadyoushutup <admin@nodadyoushutup.com>
2025-06-30 21:50:36 +0000

    debug

--- terraform/job/debug/main.tf
+++ terraform/job/debug/main.tf
@@ -2,13 +2,14 @@ module "cloud_config" {
     source = "../../module/proxmox/cloud_config"
 
     config = local.config
-    name = "talos-cp-2"
+    name = "debug"
     datastore_id = "config"
     node_name = "pve"
     overwrite = true
     auth = {
         username = "nodadyoushutup"
         github = "nodadyoushutup"
+        password = "password"
     }
     ipv4 = {
         address = "192.168.1.102"
@@ -16,5 +17,5 @@ module "cloud_config" {
 }
 
 output "cloud_config" {
-  value = module.cloud_config
+  value = module.network_config
 }
