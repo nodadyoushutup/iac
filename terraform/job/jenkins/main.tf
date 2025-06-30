47d153a4b44a55bc2ff14d3dadedd03de18d6f4f
nodadyoushutup <admin@nodadyoushutup.com>
2025-06-30 18:46:46 +0000

    Jenkins

--- terraform/job/jenkins/main.tf
+++ terraform/job/jenkins/main.tf
@@ -1,18 +1,18 @@
-resource "jenkins_folder" "framework" {
-  name = "framework"
-  description = "Required terraform"
-}
+# resource "jenkins_folder" "framework" {
+#   name = "framework"
+#   description = "Required terraform"
+# }
 
-resource "jenkins_folder" "custom" {
-  name = "custom"
-  description = "Custom terraform"
-}
+# resource "jenkins_folder" "custom" {
+#   name = "custom"
+#   description = "Custom terraform"
+# }
 
 module "job" {
-  depends_on = [
-    jenkins_folder.framework, 
-    jenkins_folder.custom
-  ]
+  # depends_on = [
+  #   jenkins_folder.framework, 
+  #   jenkins_folder.custom
+  # ]
   
   for_each = try(local.config.jenkins.job, null) != null ? local.config.jenkins.job : {}
   source = "../../module/jenkins/job"
