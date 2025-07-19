2adaf566f055b9a4e1338b58eef9482ee48a6477
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-19 16:45:40 +0000

    outputs

--- terraform/module/proxmox/cloud_init_user/output.tf
+++ terraform/module/proxmox/cloud_init_user/output.tf
@@ -1,23 +1,23 @@
-output "name" {
-    value = local.name
-}
+# output "name" {
+#     value = local.name
+# }
 
-output "node_name" {
-    value = local.node_name_computed
-}
+# output "node_name" {
+#     value = local.node_name_computed
+# }
 
-output "overwrite" {
-    value = local.overwrite_computed
-}
+# output "overwrite" {
+#     value = local.overwrite_computed
+# }
 
-output "type" {
-    value = local.type
-}
+# output "type" {
+#     value = local.type
+# }
 
 output "template" {
     value = local.template
 }
 
-output "cloud_id" {
-    value = proxmox_virtual_environment_file.cloud.id
+output "pve_snippet" {
+    value = proxmox_virtual_environment_file.user
 }
