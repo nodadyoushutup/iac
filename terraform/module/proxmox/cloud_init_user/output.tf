8d696a6c22c82783e5816545f753feb536c13653
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-19 16:47:34 +0000

    output

--- terraform/module/proxmox/cloud_init_user/output.tf
+++ terraform/module/proxmox/cloud_init_user/output.tf
@@ -1,23 +1,3 @@
-# output "name" {
-#     value = local.name
-# }
-
-# output "node_name" {
-#     value = local.node_name_computed
-# }
-
-# output "overwrite" {
-#     value = local.overwrite_computed
-# }
-
-# output "type" {
-#     value = local.type
-# }
-
-output "template" {
-    value = local.template
-}
-
 output "pve_snippet" {
     value = proxmox_virtual_environment_file.user
 }
