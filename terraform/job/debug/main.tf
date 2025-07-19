681da2038c35497849d1ca3338b7df490a9e71ec
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-19 16:49:30 +0000

    remove network

--- terraform/job/debug/main.tf
+++ terraform/job/debug/main.tf
@@ -7,31 +7,6 @@ module "cloud_init_user" {
     datastore_id = "config"
     node_name = "pve"
     overwrite = true
-    
-    # NETWORK
-    network = {
-        version = 2
-        ethernets = {
-            eth0 = {
-                match = {
-                    name = "en*"
-                }
-                type = "physical"
-                set-name = "eth0"
-                dhcp4 = "no"
-                addresses = [
-                    "192.168.1.185/24"
-                ]
-                gateway4 = "192.168.1.1"
-                nameservers = {
-                    addresses = [
-                        "8.8.8.8",
-                        "8.8.4.4"
-                    ]
-                }
-            }
-        }
-    }
 
     # USER
     bootcmd = [
