344a9eb89bf27cd1be4d0362ad8f45b70cc41d78
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-20 18:09:36 +0000

    image

--- terraform/module/proxmox/image/local.tf
+++ terraform/module/proxmox/image/local.tf
@@ -4,7 +4,7 @@ locals { # Required
 
 locals { # Constant
     url = {
-        cloud = "https://cir.nodadyoushutup.com/public/jammy-cloud-image-amd64-0.1.93.img"
+        cloud = "http://192.168.1.150:5001/public/jammy-cloud-image-amd64-0.1.93.img"
         # cloud = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
         talos = "https://github.com/nodadyoushutup/talos-image/releases/download/0.1.0/talos-image-amd64-0.1.0.img"
     }
