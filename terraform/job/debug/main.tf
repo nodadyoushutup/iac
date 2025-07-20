cd13d5d845c98b7e0468830c33ef37f61032952c
NoDadYouShutUp <70164187+nodadyoushutup@users.noreply.github.com>
2025-07-20 13:58:23 -0400

    debug job uses virtual machine module

--- terraform/job/debug/main.tf
+++ terraform/job/debug/main.tf
@@ -1,24 +1,26 @@
-module "cloud_init" {
-    source = "../../module/proxmox/cloud_init"
+module "virtual_machine" {
+  source = "../../module/proxmox/virtual_machine"
 
-    # PROXMOX
+  config = local.config
+  vm_id  = 2000
+
+  cloud_init = {
     config       = local.config
     name         = "test"
     datastore_id = "config"
     node_name    = "pve"
     overwrite    = true
 
-    # NETWORK
     network = {
       ethernets = {
         eth0 = {
           match = {
             name = "en*"
           }
-          set-name = "eth0"
-          dhcp4 = false
+          set-name  = "eth0"
+          dhcp4     = false
           addresses = ["192.168.1.150/24"]
-          gateway4 = "192.168.1.1"
+          gateway4  = "192.168.1.1"
           nameservers = {
             addresses = ["8.8.8.8", "8.8.4.4"]
           }
@@ -26,43 +28,33 @@ module "cloud_init" {
       }
     }
 
-    # USER
-    bootcmd = [
-        "mkdir -p /mnt/epool/media"
-    ]
-    runcmd = [
-        "echo 'runcmd test'"
-    ]
-    mounts = [
-        ["192.168.1.100:/mnt/epool/media", "/mnt/epool/media", "nfs","defaults,_netdev", "0", "0"]
-    ]
-    users = [
-        {
-            name = "nodadyoushutup"
-            groups = ["sudo", "docker"]
-            ssh_import_id = ["gh:nodadyoushutup"]
-            shell = "/bin/bash"
-            sudo = "ALL=(ALL) NOPASSWD:ALL"
-            plain_text_passwd = "password"
-            lock_passwd = false,
-        }
-    ]
+    bootcmd = ["mkdir -p /mnt/epool/media"]
+    runcmd  = ["echo 'runcmd test'"]
+    mounts  = [["192.168.1.100:/mnt/epool/media", "/mnt/epool/media", "nfs", "defaults,_netdev", "0", "0"]]
+    users = [{
+      name              = "nodadyoushutup"
+      groups            = ["sudo", "docker"]
+      ssh_import_id     = ["gh:nodadyoushutup"]
+      shell             = "/bin/bash"
+      sudo              = "ALL=(ALL) NOPASSWD:ALL"
+      plain_text_passwd = "password"
+      lock_passwd       = false
+    }]
     groups = ["docker"]
     gitconfig = {
-        username = "nodadyoushutup"
-        email = "admin@nodadyoushutup.com"
-        github_pat = local.config.proxmox.global.machine.cloud_config.gitconfig.github_pat
+      username   = "nodadyoushutup"
+      email      = "admin@nodadyoushutup.com"
+      github_pat = local.config.proxmox.global.machine.cloud_config.gitconfig.github_pat
     }
-    write_files = [
-        {
-            path = "/etc/skel/.canary"
-            encoding = "b64"
-            content = "Y2FuYXJ5Cg=="
-            permissions = "0640"
-        }
-    ]
+    write_files = [{
+      path        = "/etc/skel/.canary"
+      encoding    = "b64"
+      content     = "Y2FuYXJ5Cg=="
+      permissions = "0640"
+    }]
+  }
 }
 
-output "cloud_init" {
-  value = module.cloud_init
+output "virtual_machine" {
+  value = module.virtual_machine.debug
 }
--- terraform/module/proxmox/virtual_machine/main.tf
+++ terraform/module/proxmox/virtual_machine/main.tf
@@ -1,198 +1,128 @@
 module "cloud_init" {
-    source = "../cloud_init"
+  source = "../cloud_init"
 
-    # CONFIG
-    config = var.config
+  config       = try(var.cloud_init.config, var.config)
+  name         = try(var.cloud_init.name, null)
+  datastore_id = try(var.cloud_init.datastore_id, null)
+  node_name    = try(var.cloud_init.node_name, null)
+  overwrite    = try(var.cloud_init.overwrite, null)
 
-    # PROXMOX
-    name = "test"
-    datastore_id = "config"
-    node_name = "pve"
-    overwrite = true
-
-    # NETWORK
-    network = {
-        ethernets = {
-            eth0 = {
-                match = {
-                    name = "en*"
-                }
-                set-name  = "eth0"
-                dhcp4     = false
-                addresses = ["192.168.1.150/24"]
-                gateway4  = "192.168.1.1"
-                nameservers = {
-                    addresses = ["8.8.8.8", "8.8.4.4"]
-                }
-            }
-        }
-    }
-    
-    # USER
-    bootcmd = [
-        "mkdir -p /mnt/epool/media"
-    ]
-    runcmd = [
-        "echo 'runcmd test'"
-    ]
-    mounts = [
-        ["192.168.1.100:/mnt/epool/media", "/mnt/epool/media", "nfs","defaults,_netdev", "0", "0"]
-    ]
-    users = [
-        {
-            name = "nodadyoushutup"
-            groups = ["sudo", "docker"]
-            ssh_import_id = ["gh:nodadyoushutup"]
-            shell = "/bin/bash"
-            sudo = "ALL=(ALL) NOPASSWD:ALL"
-            plain_text_passwd = "password"
-            lock_passwd = false,
-        }
-    ]
-    groups = ["docker"]
-    gitconfig = {
-        username = "nodadyoushutup"
-        email = "admin@nodadyoushutup.com"
-        github_pat = var.config.proxmox.global.machine.cloud_config.gitconfig.github_pat
-    }
-    write_files = [
-        {
-            path = "/etc/skel/.canary"
-            encoding = "b64"
-            content = "Y2FuYXJ5Cg=="
-            permissions = "0640"
-        }
-    ]
-    
+  network     = try(var.cloud_init.network, null)
+  bootcmd     = try(var.cloud_init.bootcmd, null)
+  runcmd      = try(var.cloud_init.runcmd, null)
+  mounts      = try(var.cloud_init.mounts, null)
+  users       = try(var.cloud_init.users, null)
+  groups      = try(var.cloud_init.groups, null)
+  gitconfig   = try(var.cloud_init.gitconfig, null)
+  write_files = try(var.cloud_init.write_files, null)
 }
 
 module "image" {
-    source = "../image"
+  source = "../image"
 
-    config = var.config
-    name = "test"
-    datastore_id = "config"
-    # file_name = "xyz-image.img"
-    # node_name = "pve"
-    # url = "https://github.com/nodadyoushutup/cloud-image/releases/download/0.1.13/jammy-cloud-image-amd64-0.1.13.img"
-    # overwrite = true
-    # overwrite_unmanaged = true
+  config       = var.config
+  name         = "test"
+  datastore_id = "config"
 }
 
 resource "proxmox_virtual_environment_vm" "virtual_machine" {
-    depends_on = [
-        module.cloud_init,
-        module.image
-    ]
+  depends_on = [
+    module.cloud_init,
+    module.image,
+  ]
 
-    agent {
-        enabled = local.agent_computed.enabled
-        # timeout = local.agent_computed.timeout
-        timeout = "5m"
-        trim = local.agent_computed.trim
-        type = local.agent_computed.type
-    }
+  agent {
+    enabled = local.agent_computed.enabled
+    timeout = "5m"
+    trim    = local.agent_computed.trim
+    type    = local.agent_computed.type
+  }
 
-    audio_device {
-        device = local.audio_device_computed.device
-        driver = local.audio_device_computed.driver
-        enabled = local.audio_device_computed.enabled
-    }
+  audio_device {
+    device  = local.audio_device_computed.device
+    driver  = local.audio_device_computed.driver
+    enabled = local.audio_device_computed.enabled
+  }
 
-    # bios = local.bios_computed
-    bios = "seabios"
+  bios       = "seabios"
+  boot_order = local.boot_order_computed
 
-    boot_order = local.boot_order_computed
+  cpu {
+    affinity   = local.cpu_computed.affinity
+    cores      = local.cpu_computed.cores
+    flags      = local.cpu_computed.flags
+    hotplugged = local.cpu_computed.hotplugged
+    limit      = local.cpu_computed.limit
+    numa       = local.cpu_computed.numa
+    sockets    = local.cpu_computed.sockets
+    type       = local.cpu_computed.type
+    units      = local.cpu_computed.units
+  }
 
-    cpu {
-        affinity = local.cpu_computed.affinity
-        cores = local.cpu_computed.cores
-        flags = local.cpu_computed.flags
-        hotplugged = local.cpu_computed.hotplugged
-        limit = local.cpu_computed.limit
-        numa = local.cpu_computed.numa
-        sockets = local.cpu_computed.sockets
-        type = local.cpu_computed.type
-        units = local.cpu_computed.units
-    }
+  description = local.description_computed
 
-    description = local.description_computed
+  disk {
+    aio          = local.disk_computed.aio
+    backup       = local.disk_computed.backup
+    cache        = local.disk_computed.cache
+    datastore_id = local.disk_computed.datastore_id
+    discard      = local.disk_computed.discard
+    file_format  = local.disk_computed.file_format
+    file_id      = local.disk_computed.file_id
+    interface    = local.disk_computed.interface
+    iothread     = local.disk_computed.iothread
+    replicate    = local.disk_computed.replicate
+    serial       = local.disk_computed.serial
+    size         = local.disk_computed.size
+    ssd          = local.disk_computed.ssd
+  }
 
-    disk {
-        aio = local.disk_computed.aio
-        backup = local.disk_computed.backup
-        cache = local.disk_computed.cache
-        datastore_id = local.disk_computed.datastore_id
-        discard = local.disk_computed.discard
-        file_format = local.disk_computed.file_format
-        file_id = local.disk_computed.file_id
-        interface = local.disk_computed.interface
-        iothread = local.disk_computed.iothread
-        replicate = local.disk_computed.replicate
-        serial = local.disk_computed.serial
-        size = local.disk_computed.size
-        ssd = local.disk_computed.ssd
-    }
+  initialization {
+    user_data_file_id    = module.cloud_init.cloud_id
+    network_data_file_id = module.cloud_init.network_id
+  }
 
-#     efi_disk { #TODO
-#         datastore_id = local.efi_disk.datastore_id
-#         file_format = local.efi_disk.file_format
-#         type = local.efi_disk.type
-#         pre_enrolled_keys = local.efi_disk.pre_enrolled_keys
-#     }
+  machine = local.machine_computed
 
-    initialization {
-        user_data_file_id = module.cloud_init.cloud_id
-        network_data_file_id = module.cloud_init.network_id
-    }
-                    
-    machine = local.machine_computed
+  memory {
+    dedicated = local.memory_computed.dedicated
+    floating  = local.memory_computed.floating
+    shared    = local.memory_computed.shared
+  }
 
-    memory {
-        dedicated = local.memory_computed.dedicated
-        floating = local.memory_computed.floating
-        shared = local.memory_computed.shared
-    }
+  network_device {
+    bridge       = local.network_device_computed.bridge
+    disconnected = local.network_device_computed.disconnected
+    enabled      = local.network_device_computed.enabled
+    firewall     = local.network_device_computed.firewall
+    mac_address  = local.network_device_computed.mac_address
+    model        = local.network_device_computed.model
+  }
 
-    network_device {
-        bridge = local.network_device_computed.bridge
-        disconnected = local.network_device_computed.disconnected
-        enabled = local.network_device_computed.enabled
-        firewall = local.network_device_computed.firewall
-        mac_address = local.network_device_computed.mac_address
-        model = local.network_device_computed.model
-    }
+  node_name = local.node_name_computed
+  on_boot   = local.on_boot_computed
 
-#     name = local.name
+  operating_system {
+    type = local.operating_system_computed.type
+  }
 
-    node_name = local.node_name_computed
+  started = local.started_computed
 
-    on_boot = local.on_boot_computed
+  startup {
+    order      = local.startup_computed.order
+    up_delay   = local.startup_computed.up_delay
+    down_delay = local.startup_computed.down_delay
+  }
 
-    operating_system { #TODO
-        type = local.operating_system_computed.type
-    }
+  tags            = local.tags_computed
+  stop_on_destroy = local.stop_on_destroy_computed
 
-    started = local.started_computed
+  vga {
+    memory    = local.vga_computed.memory
+    type      = local.vga_computed.type
+    clipboard = local.vga_computed.clipboard
+  }
 
-    startup {
-        order = local.startup_computed.order
-        up_delay = local.startup_computed.up_delay
-        down_delay = local.startup_computed.down_delay
-    }
-
-    tags = local.tags_computed
-
-    stop_on_destroy = local.stop_on_destroy_computed
-
-    vga {
-        memory = local.vga_computed.memory
-        type = local.vga_computed.type
-        clipboard = local.vga_computed.clipboard
-    }
-
-    vm_id = local.vm_id_computed
-
-    name = "test"
-    
+  vm_id = local.vm_id_computed
+  name  = "test"
 }
