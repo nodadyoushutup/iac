adcd6378085327228738abdf45816bfda59f5c9c
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-20 21:32:39 +0000

    global updates

--- terraform/module/proxmox/cloud_init_network/global.tf
+++ terraform/module/proxmox/cloud_init_network/global.tf
@@ -1,14 +1,14 @@
 locals { # Global
     # PROXMOX
-    datastore_id_global = try(var.config.proxmox.global.machine.cloud_config.datastore_id, null)
-    node_name_global = try(var.config.proxmox.global.machine.cloud_config.node_name, null)
-    overwrite_global = try(var.config.proxmox.global.machine.cloud_config.overwrite, null)
+    datastore_id_global = try(var.config.proxmox.global.machine.cloud_init.datastore_id, null)
+    node_name_global = try(var.config.proxmox.global.machine.cloud_init.node_name, null)
+    overwrite_global = try(var.config.proxmox.global.machine.cloud_init.overwrite, null)
 
     # NETWORK
 
-    ethernets_global = try(var.config.proxmox.global.machine.cloud_config.ethernets, null)
-    bonds_global     = try(var.config.proxmox.global.machine.cloud_config.bonds, null)
-    bridges_global   = try(var.config.proxmox.global.machine.cloud_config.bridges, null)
-    vlans_global     = try(var.config.proxmox.global.machine.cloud_config.vlans, null)
+    ethernets_global = try(var.config.proxmox.global.machine.cloud_init.ethernets, null)
+    bonds_global     = try(var.config.proxmox.global.machine.cloud_init.bonds, null)
+    bridges_global   = try(var.config.proxmox.global.machine.cloud_init.bridges, null)
+    vlans_global     = try(var.config.proxmox.global.machine.cloud_init.vlans, null)
     
 }
--- terraform/module/proxmox/cloud_init_user/global.tf
+++ terraform/module/proxmox/cloud_init_user/global.tf
@@ -1,20 +1,20 @@
 locals { # Global
     # PROXMOX
-    datastore_id_global = try(var.config.proxmox.global.machine.cloud_config.datastore_id, null)
-    node_name_global = try(var.config.proxmox.global.machine.cloud_config.node_name, null)
-    overwrite_global = try(var.config.proxmox.global.machine.cloud_config.overwrite, null)
+    datastore_id_global = try(var.config.proxmox.global.machine.cloud_init.datastore_id, null)
+    node_name_global = try(var.config.proxmox.global.machine.cloud_init.node_name, null)
+    overwrite_global = try(var.config.proxmox.global.machine.cloud_init.overwrite, null)
 
     # USER
-    users_global = try(var.config.proxmox.global.machine.cloud_config.users, null)
-    mounts_global = try(var.config.proxmox.global.machine.cloud_config.mounts, null)
-    groups_global = try(var.config.proxmox.global.machine.cloud_config.groups, null)
-    write_files_global = try(var.config.proxmox.global.machine.cloud_config.write_files, null)
-    bootcmd_global = try(var.config.proxmox.global.machine.cloud_config.bootcmd, null)
-    runcmd_global = try(var.config.proxmox.global.machine.cloud_config.runcmd, null)
+    users_global = try(var.config.proxmox.global.machine.cloud_init.users, null)
+    mounts_global = try(var.config.proxmox.global.machine.cloud_init.mounts, null)
+    groups_global = try(var.config.proxmox.global.machine.cloud_init.groups, null)
+    write_files_global = try(var.config.proxmox.global.machine.cloud_init.write_files, null)
+    bootcmd_global = try(var.config.proxmox.global.machine.cloud_init.bootcmd, null)
+    runcmd_global = try(var.config.proxmox.global.machine.cloud_init.runcmd, null)
     gitconfig_global = {
-        username = try(var.config.proxmox.global.machine.cloud_config.gitconfig.username, null)
-        email = try(var.config.proxmox.global.machine.cloud_config.gitconfig.email, null)
-        github_pat = try(var.config.proxmox.global.machine.cloud_config.gitconfig.github_pat, null)
+        username = try(var.config.proxmox.global.machine.cloud_init.gitconfig.username, null)
+        email = try(var.config.proxmox.global.machine.cloud_init.gitconfig.email, null)
+        github_pat = try(var.config.proxmox.global.machine.cloud_init.gitconfig.github_pat, null)
     }
     
 }
