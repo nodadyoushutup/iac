1c7afb8cb2e656a2330c1d4a644727e3db5aa2cf
NoDadYouShutUp <70164187+nodadyoushutup@users.noreply.github.com>
2025-07-20 18:12:10 -0400

    Add typed global users support

--- terraform/module/proxmox/cloud_init_user/constant.tf
+++ terraform/module/proxmox/cloud_init_user/constant.tf
@@ -1,6 +1,34 @@
 locals { # Constant
-    source = {
-        talos    = "${path.module}/template/talos_config.yaml.tpl"
-        gitconfig = "${path.module}/template/gitconfig.tpl"
-    }
+  source = {
+    talos     = "${path.module}/template/talos_config.yaml.tpl"
+    gitconfig = "${path.module}/template/gitconfig.tpl"
+  }
+
+  user_default = {
+    name                = null
+    doas                = null
+    expiredate          = null
+    gecos               = null
+    groups              = null
+    homedir             = null
+    inactive            = null
+    lock_passwd         = null
+    no_create_home      = null
+    no_log_init         = null
+    no_user_group       = null
+    passwd              = null
+    hashed_passwd       = null
+    plain_text_passwd   = null
+    create_groups       = null
+    primary_group       = null
+    selinux_user        = null
+    shell               = null
+    snapuser            = null
+    ssh_authorized_keys = null
+    ssh_import_id       = null
+    ssh_redirect_user   = null
+    system              = null
+    sudo                = null
+    uid                 = null
+  }
 }
