991f6802f326c09ade57f8e18da084c2b379eb09
NoDadYouShutUp <70164187+nodadyoushutup@users.noreply.github.com>
2025-07-20 21:17:04 -0400

    Make VM block configuration optional

--- terraform/module/proxmox/virtual_machine/global.tf
+++ terraform/module/proxmox/virtual_machine/global.tf
@@ -1,91 +1,91 @@
 locals {
-    agent_global = {
-        enabled = try(var.config.proxmox.global.virtual_machine.agent.enabled, null)
-        timeout = try(var.config.proxmox.global.virtual_machine.agent.timeout, null)
-        trim = try(var.config.proxmox.global.virtual_machine.agent.trim, null)
-        type = try(var.config.proxmox.global.virtual_machine.agent.type, null)
-    }
+  agent_global = try(var.config.proxmox.global.virtual_machine.agent, null) != null ? {
+    enabled = try(var.config.proxmox.global.virtual_machine.agent.enabled, null)
+    timeout = try(var.config.proxmox.global.virtual_machine.agent.timeout, null)
+    trim    = try(var.config.proxmox.global.virtual_machine.agent.trim, null)
+    type    = try(var.config.proxmox.global.virtual_machine.agent.type, null)
+  } : null
 
-    audio_device_global = try(var.config.proxmox.global.virtual_machine.audio_device, null) != null ? {
-        device = try(var.config.proxmox.global.virtual_machine.audio_device.device, null)
-        driver = try(var.config.proxmox.global.virtual_machine.audio_device.driver, null)
-        enabled = try(var.config.proxmox.global.virtual_machine.audio_device.enabled, null)
-    } : null
+  audio_device_global = try(var.config.proxmox.global.virtual_machine.audio_device, null) != null ? {
+    device  = try(var.config.proxmox.global.virtual_machine.audio_device.device, null)
+    driver  = try(var.config.proxmox.global.virtual_machine.audio_device.driver, null)
+    enabled = try(var.config.proxmox.global.virtual_machine.audio_device.enabled, null)
+  } : null
 
-    bios_global = try(var.config.proxmox.global.virtual_machine.bios, null)
+  bios_global = try(var.config.proxmox.global.virtual_machine.bios, null)
 
-    boot_order_global = try(var.config.proxmox.global.virtual_machine.boot_order, null)
+  boot_order_global = try(var.config.proxmox.global.virtual_machine.boot_order, null)
 
-    cpu_global = {
-        affinity = try(var.config.proxmox.global.virtual_machine.cpu.affinity, null)
-        cores = try(var.config.proxmox.global.virtual_machine.cpu.cores, null)
-        flags = try(var.config.proxmox.global.virtual_machine.cpu.flags, null)
-        hotplugged = try(var.config.proxmox.global.virtual_machine.cpu.hotplugged, null)
-        limit = try(var.config.proxmox.global.virtual_machine.cpu.limit, null)
-        numa = try(var.config.proxmox.global.virtual_machine.cpu.numa, null)
-        sockets = try(var.config.proxmox.global.virtual_machine.cpu.sockets, null)
-        type = try(var.config.proxmox.global.virtual_machine.cpu.type, null)
-        units = try(var.config.proxmox.global.virtual_machine.cpu.units, null)
-    }
+  cpu_global = try(var.config.proxmox.global.virtual_machine.cpu, null) != null ? {
+    affinity   = try(var.config.proxmox.global.virtual_machine.cpu.affinity, null)
+    cores      = try(var.config.proxmox.global.virtual_machine.cpu.cores, null)
+    flags      = try(var.config.proxmox.global.virtual_machine.cpu.flags, null)
+    hotplugged = try(var.config.proxmox.global.virtual_machine.cpu.hotplugged, null)
+    limit      = try(var.config.proxmox.global.virtual_machine.cpu.limit, null)
+    numa       = try(var.config.proxmox.global.virtual_machine.cpu.numa, null)
+    sockets    = try(var.config.proxmox.global.virtual_machine.cpu.sockets, null)
+    type       = try(var.config.proxmox.global.virtual_machine.cpu.type, null)
+    units      = try(var.config.proxmox.global.virtual_machine.cpu.units, null)
+  } : null
 
-    description_global = try(var.config.proxmox.global.virtual_machine.description, null)
+  description_global = try(var.config.proxmox.global.virtual_machine.description, null)
 
-    disk_global = {
-        aio = try(var.config.proxmox.global.virtual_machine.disk.aio, null)
-        backup = try(var.config.proxmox.global.virtual_machine.disk.backup, null)
-        cache = try(var.config.proxmox.global.virtual_machine.disk.cache, null)
-        datastore_id = try(var.config.proxmox.global.virtual_machine.disk.datastore_id, null)
-        discard = try(var.config.proxmox.global.virtual_machine.disk.discard, null)
-        file_format = try(var.config.proxmox.global.virtual_machine.disk.file_format, null)
-        file_id = try(var.config.proxmox.global.virtual_machine.disk.file_id, null)
-        interface = try(var.config.proxmox.global.virtual_machine.disk.interface, null)
-        iothread = try(var.config.proxmox.global.virtual_machine.disk.iothread, null)
-        replicate = try(var.config.proxmox.global.virtual_machine.disk.replicate, null)
-        serial = try(var.config.proxmox.global.virtual_machine.disk.serial, null)
-        size = try(var.config.proxmox.global.virtual_machine.disk.size, null)
-        ssd = try(var.config.proxmox.global.virtual_machine.disk.ssd, null)
-    }
+  disk_global = try(var.config.proxmox.global.virtual_machine.disk, null) != null ? {
+    aio          = try(var.config.proxmox.global.virtual_machine.disk.aio, null)
+    backup       = try(var.config.proxmox.global.virtual_machine.disk.backup, null)
+    cache        = try(var.config.proxmox.global.virtual_machine.disk.cache, null)
+    datastore_id = try(var.config.proxmox.global.virtual_machine.disk.datastore_id, null)
+    discard      = try(var.config.proxmox.global.virtual_machine.disk.discard, null)
+    file_format  = try(var.config.proxmox.global.virtual_machine.disk.file_format, null)
+    file_id      = try(var.config.proxmox.global.virtual_machine.disk.file_id, null)
+    interface    = try(var.config.proxmox.global.virtual_machine.disk.interface, null)
+    iothread     = try(var.config.proxmox.global.virtual_machine.disk.iothread, null)
+    replicate    = try(var.config.proxmox.global.virtual_machine.disk.replicate, null)
+    serial       = try(var.config.proxmox.global.virtual_machine.disk.serial, null)
+    size         = try(var.config.proxmox.global.virtual_machine.disk.size, null)
+    ssd          = try(var.config.proxmox.global.virtual_machine.disk.ssd, null)
+  } : null
 
-    machine_global = try(var.config.proxmox.global.virtual_machine.machine, null)
+  machine_global = try(var.config.proxmox.global.virtual_machine.machine, null)
 
-    memory_global = {
-        dedicated = try(var.config.proxmox.global.virtual_machine.memory.dedicated, null)
-        floating = try(var.config.proxmox.global.virtual_machine.memory.floating, null)
-        shared = try(var.config.proxmox.global.virtual_machine.memory.shared, null)
-    }
+  memory_global = try(var.config.proxmox.global.virtual_machine.memory, null) != null ? {
+    dedicated = try(var.config.proxmox.global.virtual_machine.memory.dedicated, null)
+    floating  = try(var.config.proxmox.global.virtual_machine.memory.floating, null)
+    shared    = try(var.config.proxmox.global.virtual_machine.memory.shared, null)
+  } : null
 
-    network_device_global = {
-        bridge = try(var.config.proxmox.global.virtual_machine.network_device.bridge, null)
-        disconnected = try(var.config.proxmox.global.virtual_machine.network_device.disconnected, null)
-        enabled = try(var.config.proxmox.global.virtual_machine.network_device.enabled, null)
-        firewall = try(var.config.proxmox.global.virtual_machine.network_device.firewall, null)
-        mac_address = try(var.config.proxmox.global.virtual_machine.network_device.mac_address, null)
-        model = try(var.config.proxmox.global.virtual_machine.network_device.model, null)
-    }
+  network_device_global = try(var.config.proxmox.global.virtual_machine.network_device, null) != null ? {
+    bridge       = try(var.config.proxmox.global.virtual_machine.network_device.bridge, null)
+    disconnected = try(var.config.proxmox.global.virtual_machine.network_device.disconnected, null)
+    enabled      = try(var.config.proxmox.global.virtual_machine.network_device.enabled, null)
+    firewall     = try(var.config.proxmox.global.virtual_machine.network_device.firewall, null)
+    mac_address  = try(var.config.proxmox.global.virtual_machine.network_device.mac_address, null)
+    model        = try(var.config.proxmox.global.virtual_machine.network_device.model, null)
+  } : null
 
-    node_name_global = try(var.config.proxmox.global.virtual_machine.node_name, null)
+  node_name_global = try(var.config.proxmox.global.virtual_machine.node_name, null)
 
-    on_boot_global = try(var.config.proxmox.global.virtual_machine.on_boot, null)
+  on_boot_global = try(var.config.proxmox.global.virtual_machine.on_boot, null)
 
-    operating_system_global = {
-        type = try(var.config.proxmox.global.virtual_machine.operating_system.type, null)
-    }
+  operating_system_global = try(var.config.proxmox.global.virtual_machine.operating_system, null) != null ? {
+    type = try(var.config.proxmox.global.virtual_machine.operating_system.type, null)
+  } : null
 
-    started_global = try(var.config.proxmox.global.virtual_machine.started, null)
+  started_global = try(var.config.proxmox.global.virtual_machine.started, null)
 
-    startup_global = {
-        order = try(var.config.proxmox.global.virtual_machine.startup.order, null)
-        up_delay = try(var.config.proxmox.global.virtual_machine.startup.up_delay, null)
-        down_delay = try(var.config.proxmox.global.virtual_machine.startup.down_delay, null)
-    }
+  startup_global = try(var.config.proxmox.global.virtual_machine.startup, null) != null ? {
+    order      = try(var.config.proxmox.global.virtual_machine.startup.order, null)
+    up_delay   = try(var.config.proxmox.global.virtual_machine.startup.up_delay, null)
+    down_delay = try(var.config.proxmox.global.virtual_machine.startup.down_delay, null)
+  } : null
 
-    tags_global = try(var.config.proxmox.global.virtual_machine.tags, null)
+  tags_global = try(var.config.proxmox.global.virtual_machine.tags, null)
 
-    stop_on_destroy_global = try(var.config.proxmox.global.virtual_machine.stop_on_destroy, null)
+  stop_on_destroy_global = try(var.config.proxmox.global.virtual_machine.stop_on_destroy, null)
 
-    vga_global = {
-        memory = try(var.config.proxmox.global.virtual_machine.vga.memory, null)
-        type = try(var.config.proxmox.global.virtual_machine.vga.type, null)
-        clipboard = try(var.config.proxmox.global.virtual_machine.vga.clipboard, null)
-    }
+  vga_global = try(var.config.proxmox.global.virtual_machine.vga, null) != null ? {
+    memory    = try(var.config.proxmox.global.virtual_machine.vga.memory, null)
+    type      = try(var.config.proxmox.global.virtual_machine.vga.type, null)
+    clipboard = try(var.config.proxmox.global.virtual_machine.vga.clipboard, null)
+  } : null
 }
--- terraform/module/proxmox/virtual_machine/input.tf
+++ terraform/module/proxmox/virtual_machine/input.tf
@@ -1,93 +1,93 @@
 locals {
-    agent_input = {
-        enabled = try(var.agent.enabled, null)
-        timeout = try(var.agent.timeout, null)
-        trim = try(var.agent.trim, null)
-        type = try(var.agent.type, null)
-    }
+  agent_input = try(var.agent, null) != null ? {
+    enabled = try(var.agent.enabled, null)
+    timeout = try(var.agent.timeout, null)
+    trim    = try(var.agent.trim, null)
+    type    = try(var.agent.type, null)
+  } : null
 
-    audio_device_input = try(var.audio_device, null) != null ? {
-        device = try(var.audio_device.device, null)
-        driver = try(var.audio_device.driver, null)
-        enabled = try(var.audio_device.enabled, null)
-    } : null
+  audio_device_input = try(var.audio_device, null) != null ? {
+    device  = try(var.audio_device.device, null)
+    driver  = try(var.audio_device.driver, null)
+    enabled = try(var.audio_device.enabled, null)
+  } : null
 
-    bios_input = try(var.bios, null)
+  bios_input = try(var.bios, null)
 
-    boot_order_input = try(var.boot_order, null)
+  boot_order_input = try(var.boot_order, null)
 
-    cpu_input = {
-        affinity = try(var.cpu.affinity, null)
-        cores = try(var.cpu.cores, null)
-        flags = try(var.cpu.flags, null)
-        hotplugged = try(var.cpu.hotplugged, null)
-        limit = try(var.cpu.limit, null)
-        numa = try(var.cpu.numa, null)
-        sockets = try(var.cpu.sockets, null)
-        type = try(var.cpu.type, null)
-        units =try(var.cpu.units, null)
-    }
+  cpu_input = try(var.cpu, null) != null ? {
+    affinity   = try(var.cpu.affinity, null)
+    cores      = try(var.cpu.cores, null)
+    flags      = try(var.cpu.flags, null)
+    hotplugged = try(var.cpu.hotplugged, null)
+    limit      = try(var.cpu.limit, null)
+    numa       = try(var.cpu.numa, null)
+    sockets    = try(var.cpu.sockets, null)
+    type       = try(var.cpu.type, null)
+    units      = try(var.cpu.units, null)
+  } : null
 
-    description_input = try(var.description, null)
+  description_input = try(var.description, null)
 
-    disk_input = {
-        aio = try(var.disk.aio, null)
-        backup = try(var.disk.backup, null)
-        cache = try(var.disk.cache, null)
-        datastore_id = try(var.disk.datastore_id, null)
-        discard = try(var.disk.discard, null)
-        file_format = try(var.disk.file_format, null)
-        file_id = try(var.disk.file_id, null)
-        interface = try(var.disk.interface, null)
-        iothread = try(var.disk.iothread, null)
-        replicate = try(var.disk.replicate, null)
-        serial = try(var.disk.serial, null)
-        size = try(var.disk.size, null)
-        ssd = try(var.disk.ssd, null)
-    }
+  disk_input = try(var.disk, null) != null ? {
+    aio          = try(var.disk.aio, null)
+    backup       = try(var.disk.backup, null)
+    cache        = try(var.disk.cache, null)
+    datastore_id = try(var.disk.datastore_id, null)
+    discard      = try(var.disk.discard, null)
+    file_format  = try(var.disk.file_format, null)
+    file_id      = try(var.disk.file_id, null)
+    interface    = try(var.disk.interface, null)
+    iothread     = try(var.disk.iothread, null)
+    replicate    = try(var.disk.replicate, null)
+    serial       = try(var.disk.serial, null)
+    size         = try(var.disk.size, null)
+    ssd          = try(var.disk.ssd, null)
+  } : null
 
-    machine_input = try(var.machine, null)
+  machine_input = try(var.machine, null)
 
-    memory_input = {
-        dedicated = try(var.memory.dedicated, null)
-        floating = try(var.memory.floating, null)
-        shared = try(var.memory.shared, null)
-    }
+  memory_input = try(var.memory, null) != null ? {
+    dedicated = try(var.memory.dedicated, null)
+    floating  = try(var.memory.floating, null)
+    shared    = try(var.memory.shared, null)
+  } : null
 
-    network_device_input = {
-        bridge = try(var.network_device.bridge, null)
-        disconnected = try(var.network_device.disconnected, null)
-        enabled = try(var.network_device.enabled, null)
-        firewall = try(var.network_device.firewall, null)
-        mac_address = try(var.network_device.mac_address, null)
-        model = try(var.network_device.model, null)
-    }
+  network_device_input = try(var.network_device, null) != null ? {
+    bridge       = try(var.network_device.bridge, null)
+    disconnected = try(var.network_device.disconnected, null)
+    enabled      = try(var.network_device.enabled, null)
+    firewall     = try(var.network_device.firewall, null)
+    mac_address  = try(var.network_device.mac_address, null)
+    model        = try(var.network_device.model, null)
+  } : null
 
-    node_name_input = try(var.node_name, null)
+  node_name_input = try(var.node_name, null)
 
-    on_boot_input = try(var.on_boot, null)
+  on_boot_input = try(var.on_boot, null)
 
-    operating_system_input = {
-        type = try(var.operating_system.type, null)
-    }
+  operating_system_input = try(var.operating_system, null) != null ? {
+    type = try(var.operating_system.type, null)
+  } : null
 
-    started_input = try(var.started, null)
+  started_input = try(var.started, null)
 
-    startup_input = {
-        order = try(var.startup.order, null)
-        up_delay = try(var.startup.up_delay, null)
-        down_delay = try(var.startup.down_delay, null)
-    }
+  startup_input = try(var.startup, null) != null ? {
+    order      = try(var.startup.order, null)
+    up_delay   = try(var.startup.up_delay, null)
+    down_delay = try(var.startup.down_delay, null)
+  } : null
 
-    tags_input = try(var.tags, null)
+  tags_input = try(var.tags, null)
 
-    stop_on_destroy_input = try(var.stop_on_destroy, null)
+  stop_on_destroy_input = try(var.stop_on_destroy, null)
 
-    vga_input = {
-        memory = try(var.vga.memory, null)
-        type = try(var.vga.type, null)
-        clipboard = try(var.vga.clipboard, null)
-    }
+  vga_input = try(var.vga, null) != null ? {
+    memory    = try(var.vga.memory, null)
+    type      = try(var.vga.type, null)
+    clipboard = try(var.vga.clipboard, null)
+  } : null
 
-    vm_id_input = try(var.vm_id, null)
+  vm_id_input = try(var.vm_id, null)
 }
