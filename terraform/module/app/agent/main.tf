96dadbf94dadd14094152bcb8b86d2d95e1b5485
nodadyoushutup <admin@nodadyoushutup.com>
2025-09-28 16:22:12 -0400

    Agent version pin

--- terraform/module/app/agent/main.tf
+++ terraform/module/app/agent/main.tf
@@ -16,7 +16,7 @@ resource "docker_service" "agent" {
 
   task_spec {
     container_spec {
-      image = "ghcr.io/nodadyoushutup/jenkins-agent:0.0.2"
+      image = "ghcr.io/nodadyoushutup/jenkins-agent:0.0.3"
 
       env = {
         JENKINS_URL        = var.jenkins_url
