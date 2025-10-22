cbf122bb34e1a361b5e348715f5573bfb2a05cd3
nodadyoushutup <admin@nodadyoushutup.com>
2025-10-22 18:41:44 -0400

    pipeline update

--- terraform/module/config/main.tf
+++ terraform/module/config/main.tf
@@ -1,3 +1,17 @@
+locals {
+  jenkins_config_pipeline = trimspace(file("${path.root}/../pipeline/terraform.jenkins"))
+}
+
 resource "jenkins_folder" "folder_jenkins" {
-  name  = "jenkins"
+  name = "jenkins"
+}
+
+resource "jenkins_job" "jenkins_config" {
+  name   = "jenkins-config"
+  folder = jenkins_folder.folder_jenkins.id
+
+  template = templatefile("${path.module}/templates/jenkins-config-job.xml.tmpl", {
+    description = "Terraform-managed pipeline for Jenkins configuration"
+    pipeline    = local.jenkins_config_pipeline
+  })
 }
