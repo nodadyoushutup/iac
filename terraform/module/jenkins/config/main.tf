353b2fcd90abe06990cfeb8f23e2414fbc006d3c
nodadyoushutup <admin@nodadyoushutup.com>
2025-11-06 21:41:09 -0500

    jenkins dozzle

--- terraform/module/jenkins/config/main.tf
+++ terraform/module/jenkins/config/main.tf
@@ -1,5 +1,6 @@
 locals {
   jenkins_config_pipeline = trimspace(file("${path.root}/../../pipeline/jenkins.jenkins"))
+  dozzle_pipeline = trimspace(file("${path.root}/../../pipeline/dozzle.jenkins"))
 }
 
 resource "jenkins_folder" "folder_jenkins" {
@@ -10,8 +11,17 @@ resource "jenkins_job" "jenkins_config" {
   name   = "config"
   folder = jenkins_folder.folder_jenkins.id
 
-  template = templatefile("${path.module}/job/config.xml.tmpl", {
+  template = templatefile("${path.module}/job/jenkins.xml.tmpl", {
     description = "Jenkins configuration"
     pipeline    = local.jenkins_config_pipeline
+  })
+}
+
+resource "jenkins_job" "dozzle" {
+  name   = "dozzle"
+
+  template = templatefile("${path.module}/job/dozzle.xml.tmpl", {
+    description = "Dozzle configuration"
+    pipeline    = local.dozzle_pipeline
   })
 }
