a8862b75d3de1e7d7fa4c80119b0ed847e14a989
nodadyoushutup <admin@nodadyoushutup.com>
2025-11-09 16:59:40 -0500

    jenkin jobs

--- terraform/module/jenkins/config/main.tf
+++ terraform/module/jenkins/config/main.tf
@@ -1,23 +1,22 @@
 locals {
   repo_url = "https://github.com/nodadyoushutup/homelab"
 
-  multi_stage_services = {
-    jenkins = {
-      jobs = {
-        controller = {
-          description = "Jenkins controller deployment"
-          script_path = "pipeline/jenkins/controller.jenkins"
-        }
-        agent = {
-          description = "Jenkins agent deployment"
-          script_path = "pipeline/jenkins/agent.jenkins"
-        }
-        config = {
-          description = "Jenkins configuration apply"
-          script_path = "pipeline/jenkins/config.jenkins"
-        }
-      }
+  jenkins_jobs = {
+    controller = {
+      description = "Jenkins controller deployment"
+      script_path = "pipeline/jenkins/controller.jenkins"
     }
+    agent = {
+      description = "Jenkins agent deployment"
+      script_path = "pipeline/jenkins/agent.jenkins"
+    }
+    config = {
+      description = "Jenkins configuration apply"
+      script_path = "pipeline/jenkins/config.jenkins"
+    }
+  }
+
+  multi_stage_services = {
     grafana = {
       jobs = {
         app = {
@@ -63,8 +62,7 @@ locals {
     for service, cfg in local.multi_stage_services : {
       for job_name, job in cfg.jobs :
       "${service}-${job_name}" => {
-        folder      = service
-        name        = job_name
+        name        = "${service}-${job_name}"
         description = job.description
         script_path = job.script_path
       }
@@ -72,22 +70,27 @@ locals {
   ]...)
 }
 
-resource "jenkins_folder" "folder_jenkins" {
-  name = "homelab"
-}
-
-resource "jenkins_folder" "service" {
-  for_each = local.multi_stage_services
-
-  name   = each.key
-  folder = jenkins_folder.folder_jenkins.id
+resource "jenkins_folder" "jenkins_service" {
+  name = "jenkins"
 }
 
 resource "jenkins_job" "multi_stage" {
   for_each = local.multi_stage_jobs
 
-  name   = each.value.name
-  folder = jenkins_folder.service[each.value.folder].id
+  name = each.value.name
+
+  template = templatefile("${path.module}/job/bash_pipeline.xml.tmpl", {
+    description = each.value.description
+    script_path = each.value.script_path
+    repo_url    = local.repo_url
+  })
+}
+
+resource "jenkins_job" "jenkins" {
+  for_each = local.jenkins_jobs
+
+  name   = each.key
+  folder = jenkins_folder.jenkins_service.id
 
   template = templatefile("${path.module}/job/bash_pipeline.xml.tmpl", {
     description = each.value.description
