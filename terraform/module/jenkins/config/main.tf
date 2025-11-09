7ff1d9e0d7f23cfd2285cf9c02f040198e7eae5c
nodadyoushutup <admin@nodadyoushutup.com>
2025-11-08 19:30:45 -0500

    grafana jenkins job

--- terraform/module/jenkins/config/main.tf
+++ terraform/module/jenkins/config/main.tf
@@ -1,11 +1,3 @@
-locals {
-  jenkins_config_pipeline = trimspace(file("${path.root}/../../pipeline/jenkins.jenkins"))
-  dozzle_pipeline         = trimspace(file("${path.root}/../../pipeline/dozzle.jenkins"))
-  node_exporter_pipeline  = trimspace(file("${path.root}/../../pipeline/node_exporter.jenkins"))
-  prometheus_pipeline     = trimspace(file("${path.root}/../../pipeline/prometheus.jenkins"))
-  grafana_pipeline        = trimspace(file("${path.root}/../../pipeline/grafana.jenkins"))
-}
-
 resource "jenkins_folder" "folder_jenkins" {
   name = "jenkins"
 }
@@ -16,7 +8,6 @@ resource "jenkins_job" "jenkins_config" {
 
   template = templatefile("${path.module}/job/jenkins.xml.tmpl", {
     description = "Jenkins configuration"
-    pipeline    = local.jenkins_config_pipeline
   })
 }
 
@@ -25,7 +16,6 @@ resource "jenkins_job" "dozzle" {
 
   template = templatefile("${path.module}/job/dozzle.xml.tmpl", {
     description = "Dozzle configuration"
-    pipeline    = local.dozzle_pipeline
   })
 }
 
@@ -34,7 +24,6 @@ resource "jenkins_job" "node_exporter" {
 
   template = templatefile("${path.module}/job/node_exporter.xml.tmpl", {
     description = "Node Exporter configuration"
-    pipeline    = local.node_exporter_pipeline
   })
 }
 
@@ -43,7 +32,6 @@ resource "jenkins_job" "prometheus" {
 
   template = templatefile("${path.module}/job/prometheus.xml.tmpl", {
     description = "Prometheus configuration"
-    pipeline    = local.prometheus_pipeline
   })
 }
 
@@ -52,6 +40,5 @@ resource "jenkins_job" "grafana" {
 
   template = templatefile("${path.module}/job/grafana.xml.tmpl", {
     description = "Grafana application and configuration pipeline"
-    pipeline    = local.grafana_pipeline
   })
 }
