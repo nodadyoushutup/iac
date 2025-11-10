33c59e6fde0154fe22cf5987c518f0e5c9f61d34
nodadyoushutup <admin@nodadyoushutup.com>
2025-11-09 23:40:10 -0500

    npm image sha

--- terraform/module/nginx_proxy_manager/app/main.tf
+++ terraform/module/nginx_proxy_manager/app/main.tf
@@ -1,6 +1,6 @@
 locals {
   stack_name = "nginx-proxy-manager"
-  image      = "jc21/nginx-proxy-manager:2.12.6"
+  image      = "jc21/nginx-proxy-manager:2.12.6@sha256:6ab097814f54b1362d5fd3c5884a01ddd5878aaae9992ffd218439180f0f92f3"
   allowed_platforms = [
     {
       os           = "linux"
